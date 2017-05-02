11d7ea9a8fb9c71e11ea84e6ff1c4f16ae0134de
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-05-02 16:05:07 +0200

    PEX file completion

--- korge-intellij-plugin/src/com/soywiz/korge/intellij/KorgeFileTypeFactory.kt
+++ korge-intellij-plugin/src/com/soywiz/korge/intellij/KorgeFileTypeFactory.kt
@@ -1,5 +1,7 @@
 package com.soywiz.korge.intellij
 
+import com.intellij.ide.highlighter.XmlFileType
+import com.intellij.javaee.ExternalResourceManager
 import com.intellij.openapi.fileTypes.FileTypeConsumer
 import com.intellij.openapi.fileTypes.FileTypeFactory
 import com.intellij.openapi.fileTypes.UserBinaryFileType
@@ -8,10 +10,14 @@ class KorgeFileTypeFactory : FileTypeFactory() {
 	companion object {
 		val KORGE_ANI = UserBinaryFileType()
 		val KORGE_AUDIO = UserBinaryFileType()
+		val KORGE_TMX = XmlFileType.INSTANCE
+		val KORGE_PEX = XmlFileType.INSTANCE
 	}
 
 	override fun createFileTypes(fileTypeConsumer: FileTypeConsumer) {
 		fileTypeConsumer.consume(KORGE_ANI, "swf;ani")
 		fileTypeConsumer.consume(KORGE_AUDIO, "wav;mp3;ogg")
+		fileTypeConsumer.consume(KORGE_TMX, "tmx")
+		fileTypeConsumer.consume(KORGE_PEX, "pex")
 	}
 }
