214286dc7fc8b078adc65586f1dcb90c9c8696d5
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-10-01 01:43:59 +0200

    More work

--- korge-common/src/main/kotlin/com/soywiz/korge/scene/Scene.kt
+++ korge-common/src/main/kotlin/com/soywiz/korge/scene/Scene.kt
@@ -1,6 +1,5 @@
 package com.soywiz.korge.scene
 
-import com.soywiz.korge.bus.Bus
 import com.soywiz.korge.log.Logger
 import com.soywiz.korge.resources.ResourcesRoot
 import com.soywiz.korge.time.TimeSpan
@@ -11,16 +10,17 @@ import com.soywiz.korge.view.ViewsContainer
 import com.soywiz.korge.view.scaleView
 import com.soywiz.korio.async.CoroutineContextHolder
 import com.soywiz.korio.coroutine.CoroutineContext
-import com.soywiz.korio.inject.AsyncDependency
+import com.soywiz.korio.inject.AsyncInjector
 import com.soywiz.korio.inject.Inject
+import com.soywiz.korio.inject.InjectorAsyncDependency
 import com.soywiz.korio.util.Cancellable
 import com.soywiz.korma.geom.ISize
 
-abstract class Scene : AsyncDependency, ViewsContainer, CoroutineContextHolder {
-	@Inject override lateinit var views: Views
-	@Inject lateinit var sceneContainer: SceneContainer
-	@Inject lateinit var resourcesRoot: ResourcesRoot
-	@Inject protected lateinit var bus: Bus
+abstract class Scene : InjectorAsyncDependency, ViewsContainer, CoroutineContextHolder {
+	override lateinit var views: Views
+	lateinit var sceneContainer: SceneContainer
+	lateinit var resourcesRoot: ResourcesRoot
+	//protected lateinit var bus: Bus
 	lateinit internal var _sceneViewContainer: Container; private set
 	lateinit var sceneView: Container; private set
 	val root get() = _sceneViewContainer
@@ -29,7 +29,11 @@ abstract class Scene : AsyncDependency, ViewsContainer, CoroutineContextHolder {
 
 	open protected fun createSceneView(): Container = views.container()
 
-	suspend final override fun init() {
+	suspend override fun init(injector: AsyncInjector): Unit {
+		this.views = injector.get()
+		this.sceneContainer = injector.get()
+		this.resourcesRoot = injector.get()
+		//this.bus = injector.get()
 		_sceneViewContainer = views.container()
 		sceneView = createSceneView()
 		_sceneViewContainer += sceneView
@@ -66,7 +70,12 @@ class EmptyScene : Scene() {
 
 abstract class LogScene : Scene() {
 	val name: String = this::class.simpleName ?: "LogSceneUnknown"
-	@Inject lateinit var logger: Logger
+	lateinit var logger: Logger
+
+	suspend override fun init(injector: AsyncInjector) {
+		super.init(injector)
+		logger = injector.get()
+	}
 
 	suspend override fun sceneInit(sceneView: Container) {
 		logger.info("$name.sceneInit")
