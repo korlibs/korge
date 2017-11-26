fab07b487c64b47829acf71013eda63d5f41f31c
soywiz <soywiz@gmail.com>
2017-11-27 00:22:21 +0100

    Try to fix FlaTest

--- korge-ext-fla/common/src/test/kotlin/com/soywiz/korge/ext/fla/FlaTest.kt
+++ korge-ext-fla/common/src/test/kotlin/com/soywiz/korge/ext/fla/FlaTest.kt
@@ -5,9 +5,12 @@ import com.soywiz.korio.vfs.ResourcesVfs
 import org.junit.Test
 
 class FlaTest {
+	val TestAssertVfs = localCurrentDirVfs["src/test/resources"]
+	//val TestAssertVfs = ResourcesVfs
+
 	@Test
 	fun name() = syncTest {
-		val fla = Fla.read(ResourcesVfs["simple1.fla"])
+		val fla = Fla.read(TestAssertVfs["simple1.fla"])
 		//val fla = Fla.read(ResourcesVfs["simple1"])
 	}
 }
