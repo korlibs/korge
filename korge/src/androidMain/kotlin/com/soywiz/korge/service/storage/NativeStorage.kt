04a1580b27c4a3227b8f29e8fffb897e56744d7e
soywiz <soywiz@gmail.com>
2019-01-26 13:53:20 +0100

    WIP 1.0.0-beta-3

--- korge/src/androidMain/kotlin/com/soywiz/korge/service/storage/NativeStorage.kt
+++ korge/src/androidMain/kotlin/com/soywiz/korge/service/storage/NativeStorage.kt
@@ -1,19 +1,51 @@
 package com.soywiz.korge.service.storage
 
+import java.io.*
+import java.util.*
+
 actual object NativeStorage : IStorage {
+	val props = Properties()
+
+	init {
+		load()
+	}
+
+	private fun load() {
+		try {
+			FileInputStream(File("game.storage")).use { fis ->
+				props.load(fis)
+			}
+		} catch (e: IOException) {
+			e.printStackTrace()
+		}
+	}
+
+	private fun save() {
+		try {
+			FileOutputStream(File("game.storage")).use { fout ->
+				props.store(fout, "")
+			}
+		} catch (e: IOException) {
+			e.printStackTrace()
+		}
+	}
+
 	actual override fun set(key: String, value: String) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		props[key] = value
+		save()
 	}
 
 	actual override fun getOrNull(key: String): String? {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		return props[key]?.toString()
 	}
 
 	actual override fun remove(key: String) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		props.remove(key)
+		save()
 	}
 
 	actual override fun removeAll() {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		props.clear()
+		save()
 	}
 }
--- korge/src/commonMain/kotlin/com/soywiz/korge/KorgeVersion.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/KorgeVersion.kt
@@ -1,3 +1,3 @@
 package com.soywiz.korge
 
-val Korge.VERSION get() = "1.0.0-beta-2-eap-100"
+val Korge.VERSION get() = "1.0.0-beta-3"
