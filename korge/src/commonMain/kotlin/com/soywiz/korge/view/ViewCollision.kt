a6e801bb83dc38d1e74ed9fe5b2872f089d3ef17
soywiz <soywiz@gmail.com>
2019-04-28 06:34:13 +0200

    More work on collisions

--- korge/src/commonMain/kotlin/com/soywiz/korge/view/ViewCollision.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/ViewCollision.kt
@@ -1,6 +1,7 @@
 package com.soywiz.korge.view
 
 import com.soywiz.korge.internal.fastForEach
+import com.soywiz.korio.lang.Cancellable
 import com.soywiz.korio.lang.threadLocal
 import com.soywiz.korma.geom.Rectangle
 
@@ -21,4 +22,37 @@ fun View.collidesWith(otherList: List<View>, kind: CollisionKind = CollisionKind
 		if (this.collidesWith(other, kind)) return true
 	}
 	return false
+}
+
+/**
+ * Adds a component to [this] that checks each frame for collisions against descendant views of [root] that matches the [filter],
+ * when a collision is found [callback] is executed with this view as receiver, and the collision target as first parameter.
+ * if no [root] is provided, it computes the root of the view [this] each frame, you can specify a collision [kind]
+ */
+fun View.onCollision(filter: (View) -> Boolean = { true }, root: View? = null, kind: CollisionKind = CollisionKind.GLOBAL_RECT, callback: View.(View) -> Unit): Cancellable {
+	return addUpdatable {
+		(root ?: this.root).foreachDescendant {
+			if (this != it && filter(it) && this.collidesWith(it, kind)) {
+				callback(this, it)
+			}
+		}
+	}
+}
+
+/**
+ * Adds a component to [this] that checks collisions each frame of descendants views of [root] matching [filterSrc], matching against descendant views matching [filterDst].
+ * When a collision is found, [callback] is called. It returns a [Cancellable] to remove the component.
+ */
+fun View.onDescendantCollision(root: View = this, filterSrc: (View) -> Boolean = { true }, filterDst: (View) -> Boolean = { true }, kind: CollisionKind = CollisionKind.GLOBAL_RECT, callback: View.(View) -> Unit): Cancellable {
+	return addUpdatable {
+		root.foreachDescendant { src ->
+			if (filterSrc(src)) {
+				root.foreachDescendant { dst ->
+					if (src !== dst && filterDst(dst) && src.collidesWith(dst, kind)) {
+						callback(src, dst)
+					}
+				}
+			}
+		}
+	}
 }
---
+++ korge/src/commonTest/kotlin/com/soywiz/korge/view/ViewCollision.kt
@@ -0,0 +1,21 @@
+package com.soywiz.korge.view
+
+import com.soywiz.korim.color.Colors
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ViewCollision {
+	@Test
+	fun test() {
+		val container = Container().apply {
+			solidRect(16, 16, Colors.RED).position(0, 0).name("view1")
+			solidRect(16, 16, Colors.RED).position(14, 0).name("view2")
+		}
+		val log = arrayListOf<String>()
+		container["view1"]!!.onCollision {
+			log += "collision[${this.name}, ${it.name}]"
+		}
+		container["view1"]!!.updateSingleView(0.0)
+		assertEquals(listOf("collision[view1, view1]"), log)
+	}
+}
