ee4cbf0fed57b97177e90a844a639d747f29af20
soywiz <soywiz@gmail.com>
2019-11-30 21:53:30 +0100

    Moved plugins to korge-plugins repo : https://github.com/korlibs/korge-plugins

--- plugins/korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/util/FileExt.kt
+++
@@ -1,7 +0,0 @@
-package com.soywiz.korge.gradle.util
-
-import java.io.File
-
-fun File.ensureParents() = this.apply { parentFile.mkdirs() }
-fun <T> File.conditionally(ifNotExists: Boolean = true, block: File.() -> T): T? = if (!ifNotExists || !this.exists()) block() else null
-operator fun File.get(name: String) = File(this, name)
