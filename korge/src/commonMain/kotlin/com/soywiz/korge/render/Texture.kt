7943227af9b30491f2208909c9cd70cf97c0b60c
soywiz <soywiz@gmail.com>
2019-12-28 03:21:14 +0100

    Texture and Texture.Base api documentation

--- korge/src/commonMain/kotlin/com/soywiz/korge/render/Texture.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/render/Texture.kt
@@ -10,32 +10,54 @@ import com.soywiz.korio.file.*
 import com.soywiz.korio.lang.*
 import com.soywiz.korma.geom.*
 
-//e: java.lang.UnsupportedOperationException: Class literal annotation arguments are not yet supported: Factory
-//@AsyncFactoryClass(TextureAsyncFactory::class)
+/**
+ * A [Texture] is a region (delimited by [left], [top], [right] and [bottom]) of a [Texture.Base].
+ * A [Texture.Base] wraps a [AG.Texture] but adds [width] and [height] information.
+ */
 class Texture(
 	val base: Base,
+    /** Left position of the region of the texture in pixels */
 	val left: Int = 0,
+    /** Top position of the region of the texture in pixels */
 	val top: Int = 0,
+    /** Right position of the region of the texture in pixels */
 	val right: Int = base.width,
+    /** Bottom position of the region of the texture in pixels */
 	val bottom: Int = base.height
 ) : Closeable {
+    /** Wether the texture is multiplied or not */
 	val premultiplied get() = base.premultiplied
+    /** Left position of the region of the texture in pixels */
 	val x = left
+    /** Top position of the region of the texture in pixels */
 	val y = top
+    /** Width of this texture region in pixels */
 	val width = right - left
+    /** Height of this texture region in pixels */
 	val height = bottom - top
 
+    /** Left coord of the texture region as a ratio (a value between 0 and 1) */
 	val x0: Float = (left).toFloat() / base.width.toFloat()
+    /** Right coord of the texture region as a ratio (a value between 0 and 1) */
 	val x1: Float = (right).toFloat() / base.width.toFloat()
+    /** Top coord of the texture region as a ratio (a value between 0 and 1) */
 	val y0: Float = (top).toFloat() / base.height.toFloat()
+    /** Bottom coord of the texture region as a ratio (a value between 0 and 1) */
 	val y1: Float = (bottom).toFloat() / base.height.toFloat()
 
-	override fun toString(): String = "Texture($base, (x=$x, y=$y, width=$width, height=$height))"
-
+    /**
+     * Creates a slice of this texture, by [x], [y], [width] and [height].
+     */
 	fun slice(x: Int, y: Int, width: Int, height: Int) = sliceBounds(x, y, x + width, y + height)
 
+    /**
+     * Createa a slice of this texture by [rect].
+     */
 	fun slice(rect: Rectangle) = slice(rect.x.toInt(), rect.y.toInt(), rect.width.toInt(), rect.height.toInt())
 
+    /**
+     * Creates a slice of this texture by its bounds [left], [top], [right], [bottom].
+     */
 	fun sliceBounds(left: Int, top: Int, right: Int, bottom: Int): Texture {
 		val tleft = (this.x + left).clamp(this.left, this.right)
 		val tright = (this.x + right).clamp(this.left, this.right)
@@ -45,10 +67,16 @@ class Texture(
 	}
 
 	companion object {
+        /**
+         * Creates a [Texture] from a texture [agBase] and its wanted size [width], [height].
+         */
 		operator fun invoke(agBase: AG.Texture, width: Int, height: Int): Texture =
 			Texture(Base(agBase, width, height), 0, 0, width, height)
 	}
 
+    /**
+     * Represents a full texture region wraping a [base] [AG.Texture] and specifying its [width] and [height]
+     */
 	class Base(val base: AG.Texture, val width: Int, val height: Int) : Closeable {
 		val premultiplied get() = base.premultiplied
 		override fun close() = base.close()
@@ -57,11 +85,19 @@ class Texture(
 		}
 	}
 
+    /**
+     * Updates this texture from a [bmp] and optionally generates [mipmaps].
+     */
 	fun update(bmp: Bitmap32, mipmaps: Boolean = false) {
 		base.update(bmp, mipmaps)
 	}
 
+    /**
+     * Closes the texture
+     */
 	override fun close() = base.close()
+
+    override fun toString(): String = "Texture($base, (x=$x, y=$y, width=$width, height=$height))"
 }
 
 //suspend fun VfsFile.readTexture(ag: AG, imageFormats: ImageFormats, mipmaps: Boolean = true): Texture {
