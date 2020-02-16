4ad68dbecdcf5a91f481482f5a9ef88f05b9dbf8
soywiz <soywiz@gmail.com>
2020-02-16 17:17:19 +0100

    Added StageComponent and Views.registerStageComponent to watch entering/exiting the stage

---
+++ korge/src/commonMain/kotlin/com/soywiz/korge/component/StageComponent.kt
@@ -0,0 +1,41 @@
+package com.soywiz.korge.component
+
+import com.soywiz.korge.view.*
+
+/**
+ * Component with [added] and [removed] methods that are executed
+ * once the view is going to be displayed, and when the view has been removed
+ *
+ * Important NOTE: To use this compoennt you have to call the [Views.registerStageComponent] extension method at the start of the APP.
+ */
+interface StageComponent : Component {
+    fun added(views: Views)
+    fun removed(views: Views)
+}
+
+/**
+ * Enables the use of [StageComponent] components.
+ */
+fun Views.registerStageComponent() {
+    val componentsInStagePrev = linkedSetOf<StageComponent>()
+    val componentsInStageCur = linkedSetOf<StageComponent>()
+    val componentsInStage = linkedSetOf<StageComponent>()
+    val tempComponents: ArrayList<Component> = arrayListOf()
+    onBeforeRender {
+        componentsInStagePrev.clear()
+        componentsInStagePrev += componentsInStageCur
+        componentsInStageCur.clear()
+        stage.forEachComponent<StageComponent>(tempComponents) {
+            componentsInStageCur += it
+            if (it !in componentsInStage) {
+                componentsInStage += it
+                it.added(views)
+            }
+        }
+        for (it in componentsInStagePrev) {
+            if (it !in componentsInStageCur) {
+                it.removed(views)
+            }
+        }
+    }
+}
