947bf80aebfc2799eb8bc7de5bd3150ff024b7c6
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-02-19 22:27:23 +0100

    Feature/allow.suspend.events (#87)
    * MAke MouseEvents event handlers suspend to not require launching inside its body
    
    * Make KeyEvents suspend
    
    * Make gamepad event methods suspend
    
    * Limit visibility of coroutineContext and views in the *Events classes

--- korge/src/commonMain/kotlin/com/soywiz/korge/input/KeysEvents.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/input/KeysEvents.kt
@@ -6,21 +6,28 @@ import com.soywiz.korge.view.*
 import com.soywiz.korio.async.*
 import com.soywiz.korio.lang.*
 import com.soywiz.korev.*
+import kotlin.reflect.*
 
 class KeysEvents(override val view: View) : KeyComponent {
-	val onKeyDown = AsyncSignal<KeyEvent>()
+    @PublishedApi
+    internal lateinit var views: Views
+    @PublishedApi
+    internal val coroutineContext get() = views.coroutineContext
+
+    val onKeyDown = AsyncSignal<KeyEvent>()
 	val onKeyUp = AsyncSignal<KeyEvent>()
 	val onKeyTyped = AsyncSignal<KeyEvent>()
 
-	fun down(key: Key, callback: (key: Key) -> Unit): Closeable = onKeyDown { e -> if (e.key == key) callback(key) }
-	fun up(key: Key, callback: (key: Key) -> Unit): Closeable = onKeyUp { e -> if (e.key == key) callback(key) }
-	fun typed(key: Key, callback: (key: Key) -> Unit): Closeable = onKeyTyped { e -> if (e.key == key) callback(key) }
+	fun down(key: Key, callback: suspend (key: Key) -> Unit): Closeable = onKeyDown { e -> if (e.key == key) callback(key) }
+	fun up(key: Key, callback: suspend (key: Key) -> Unit): Closeable = onKeyUp { e -> if (e.key == key) callback(key) }
+	fun typed(key: Key, callback: suspend (key: Key) -> Unit): Closeable = onKeyTyped { e -> if (e.key == key) callback(key) }
 
-	fun down(callback: (key: Key) -> Unit): Closeable = onKeyDown { e -> callback(e.key) }
-	fun up(callback: (key: Key) -> Unit): Closeable = onKeyUp { e -> callback(e.key) }
-	fun typed(callback: (key: Key) -> Unit): Closeable = onKeyTyped { e -> callback(e.key) }
+	fun down(callback: suspend (key: Key) -> Unit): Closeable = onKeyDown { e -> callback(e.key) }
+	fun up(callback: suspend (key: Key) -> Unit): Closeable = onKeyUp { e -> callback(e.key) }
+	fun typed(callback: suspend (key: Key) -> Unit): Closeable = onKeyTyped { e -> callback(e.key) }
 
 	override fun onKeyEvent(views: Views, event: KeyEvent) {
+        this.views = views
 		when (event.type) {
 			KeyEvent.Type.TYPE -> launchImmediately(views.coroutineContext) { onKeyTyped.invoke(event) }
 			KeyEvent.Type.DOWN -> launchImmediately(views.coroutineContext) { onKeyDown.invoke(event) }
@@ -32,11 +39,10 @@ class KeysEvents(override val view: View) : KeyComponent {
 val View.keys by Extra.PropertyThis<View, KeysEvents> { this.getOrCreateComponent { KeysEvents(this) } }
 inline fun <T> View.keys(callback: KeysEvents.() -> T): T = keys.run(callback)
 
-inline fun <T : View?> T?.onKeyDown(noinline handler: suspend (KeyEvent) -> Unit) =
-	this.apply { this?.keys?.onKeyDown?.add(handler) }
+@PublishedApi
+internal inline fun <T : View?> T._keyEvent(prop: KProperty1<KeysEvents, AsyncSignal<KeyEvent>>, noinline handler: suspend (KeyEvent) -> Unit): T =
+    this.apply { this?.keys?.let { keys -> prop.get(keys).add(handler) } }
 
-inline fun <T : View?> T?.onKeyUp(noinline handler: suspend (KeyEvent) -> Unit) =
-	this.apply { this?.keys?.onKeyUp?.add(handler) }
-
-inline fun <T : View?> T?.onKeyTyped(noinline handler: suspend (KeyEvent) -> Unit) =
-	this.apply { this?.keys?.onKeyTyped?.add(handler) }
+inline fun <T : View?> T.onKeyDown(noinline handler: suspend (KeyEvent) -> Unit): T = _keyEvent(KeysEvents::onKeyDown, handler)
+inline fun <T : View?> T.onKeyUp(noinline handler: suspend (KeyEvent) -> Unit): T = _keyEvent(KeysEvents::onKeyUp, handler)
+inline fun <T : View?> T.onKeyTyped(noinline handler: suspend (KeyEvent) -> Unit): T = _keyEvent(KeysEvents::onKeyTyped, handler)
