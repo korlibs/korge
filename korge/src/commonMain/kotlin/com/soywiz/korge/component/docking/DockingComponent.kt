195b146d94885641c0bc940d9a82af9447a281a7
soywiz <soywiz@gmail.com>
2020-02-24 00:36:46 +0100

    Fixes DockingComponent to initially reshape asap

--- korge/src/commonMain/kotlin/com/soywiz/korge/component/docking/DockingComponent.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/component/docking/DockingComponent.kt
@@ -4,12 +4,20 @@ import com.soywiz.korge.component.*
 import com.soywiz.korge.view.*
 import com.soywiz.korma.geom.*
 
-fun <T : View> T.dockedTo(anchor: Anchor, scaleMode: ScaleMode = ScaleMode.NO_SCALE) = DockingComponent(this, anchor, scaleMode).attach()
+fun <T : View> T.dockedTo(anchor: Anchor, scaleMode: ScaleMode = ScaleMode.NO_SCALE): T = this.apply {
+    DockingComponent(this, anchor, scaleMode).attach()
+}
 
 class DockingComponent(override val view: View, var anchor: Anchor, var scaleMode: ScaleMode = ScaleMode.NO_SCALE) : ResizeComponent {
     val initialViewSize = Size(view.width, view.height)
     private val actualVirtualSize = Size(0, 0)
     private val targetSize = Size(0, 0)
+
+    init {
+        view.deferWithViews { views ->
+            resized(views, views.actualWidth, views.actualHeight)
+        }
+    }
 
     override fun resized(views: Views, width: Int, height: Int) {
         view.position(
