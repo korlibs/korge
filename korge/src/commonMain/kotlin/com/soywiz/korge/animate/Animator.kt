80916329307ed81a7120d33002fd8964b5eb766b
RezMike <mihail.reznichenko.98@mail.ru>
2020-03-19 11:26:26 +0700

    Add sequenceLazy and parallelLazy functions

--- korge/src/commonMain/kotlin/com/soywiz/korge/animate/Animator.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/animate/Animator.kt
@@ -11,6 +11,10 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.*
 import kotlin.math.*
 
+@DslMarker
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+annotation class AnimatorDslMarker
+
 interface BaseAnimatorNode {
     suspend fun execute()
     fun executeImmediately()
@@ -85,7 +89,7 @@ open class Animator(
         speed: Double = this.speed,
         easing: Easing = this.easing,
         completeOnCancel: Boolean = this.completeOnCancel,
-        callback: Animator.() -> Unit
+        callback: @AnimatorDslMarker Animator.() -> Unit
     ) = Animator(root, time, speed, easing, completeOnCancel, NodeKind.Parallel).apply(callback).also { nodes.add(it) }
 
     inline fun sequence(
@@ -93,7 +97,7 @@ open class Animator(
         speed: Double = this.speed,
         easing: Easing = this.easing,
         completeOnCancel: Boolean = this.completeOnCancel,
-        callback: Animator.() -> Unit
+        callback: @AnimatorDslMarker Animator.() -> Unit
     ) = Animator(root, time, speed, easing, completeOnCancel, NodeKind.Sequence).apply(callback).also { nodes.add(it) }
 
     fun parallelLazy(
@@ -101,7 +105,7 @@ open class Animator(
         speed: Double = this.speed,
         easing: Easing = this.easing,
         completeOnCancel: Boolean = this.completeOnCancel,
-        init: Animator.() -> Unit
+        init: @AnimatorDslMarker Animator.() -> Unit
     ) = Animator(root, time, speed, easing, completeOnCancel, NodeKind.Parallel, init).also { nodes.add(it) }
 
     fun sequenceLazy(
@@ -109,7 +113,7 @@ open class Animator(
         speed: Double = this.speed,
         easing: Easing = this.easing,
         completeOnCancel: Boolean = this.completeOnCancel,
-        init: Animator.() -> Unit
+        init: @AnimatorDslMarker Animator.() -> Unit
     ) = Animator(root, time, speed, easing, completeOnCancel, NodeKind.Sequence, init).also { nodes.add(it) }
 
     inner class TweenNode(val view: View, vararg val vfs: V2<*>, val time: TimeSpan = 1.seconds, val lazyTime: (() -> TimeSpan)? = null, val easing: Easing) : BaseAnimatorNode {
@@ -162,7 +166,7 @@ fun View.animator(
     easing: Easing = Animator.DEFAULT_EASING,
     completeOnCancel: Boolean = Animator.DEFAULT_COMPLETE_ON_CANCEL,
     kind: Animator.NodeKind = Animator.NodeKind.Sequence,
-    block: Animator.() -> Unit = {}
+    block: @AnimatorDslMarker Animator.() -> Unit = {}
 ): Animator = Animator(this, time, speed, easing, completeOnCancel, kind).apply(block)
 
 suspend fun View.launchAnimate(
@@ -171,7 +175,7 @@ suspend fun View.launchAnimate(
     easing: Easing = Animator.DEFAULT_EASING,
     completeOnCancel: Boolean = Animator.DEFAULT_COMPLETE_ON_CANCEL,
     kind: Animator.NodeKind = Animator.NodeKind.Sequence,
-    block: Animator.() -> Unit = {}
+    block: @AnimatorDslMarker Animator.() -> Unit = {}
 ): Job = launchImmediately(coroutineContext) { animate(time, speed, easing, completeOnCancel, kind, block) }
 
 suspend fun View.animate(
@@ -180,7 +184,7 @@ suspend fun View.animate(
     easing: Easing = Animator.DEFAULT_EASING,
     completeOnCancel: Boolean = Animator.DEFAULT_COMPLETE_ON_CANCEL,
     kind: Animator.NodeKind = Animator.NodeKind.Sequence,
-    block: Animator.() -> Unit = {}
+    block: @AnimatorDslMarker Animator.() -> Unit = {}
 ): Animator = Animator(this, time, speed, easing, completeOnCancel, kind).apply(block).also { it.execute() }
 
 suspend fun View.animateSequence(
@@ -188,7 +192,7 @@ suspend fun View.animateSequence(
     speed: Double = Animator.DEFAULT_SPEED,
     easing: Easing = Animator.DEFAULT_EASING,
     completeOnCancel: Boolean = Animator.DEFAULT_COMPLETE_ON_CANCEL,
-    block: Animator.() -> Unit = {}
+    block: @AnimatorDslMarker Animator.() -> Unit = {}
 ): Animator = animate(time, speed, easing, completeOnCancel, Animator.NodeKind.Sequence, block)
 
 suspend fun View.animateParallel(
@@ -196,5 +200,5 @@ suspend fun View.animateParallel(
     speed: Double = Animator.DEFAULT_SPEED,
     easing: Easing = Animator.DEFAULT_EASING,
     completeOnCancel: Boolean = Animator.DEFAULT_COMPLETE_ON_CANCEL,
-    block: Animator.() -> Unit = {}
+    block: @AnimatorDslMarker Animator.() -> Unit = {}
 ): Animator = animate(time, speed, easing, completeOnCancel, Animator.NodeKind.Parallel, block)
