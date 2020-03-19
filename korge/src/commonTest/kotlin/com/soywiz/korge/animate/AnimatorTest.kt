66c32a1aa110cf70667b3bbe72a523dc63fe1be0
soywiz <soywiz@gmail.com>
2020-03-19 12:27:56 +0100

    Test block

--- korge/src/commonTest/kotlin/com/soywiz/korge/animate/AnimatorTest.kt
+++ korge/src/commonTest/kotlin/com/soywiz/korge/animate/AnimatorTest.kt
@@ -10,12 +10,15 @@ class AnimatorTest : ViewsForTesting()  {
     @Test
     fun test() = viewsTest {
         val view = solidRect(100, 100, Colors.RED)
+        val log = arrayListOf<String>()
         animate(completeOnCancel = false) {
             view.moveTo(100, 0)
             view.moveBy(y = +100.0)
+            block { log += "${view.pos}" }
             view.moveBy(x = +10.0)
             view.moveTo(x = { view.x + 10 })
         }
         assertEquals("(120, 100)", view.pos.toString())
+        assertEquals("[(100, 100)]", log.toString())
     }
 }
