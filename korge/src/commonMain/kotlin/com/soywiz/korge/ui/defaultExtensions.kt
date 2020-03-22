c1746e1c191daef4103740ccf148e3d6e81726b6
Mikhail Reznichenko <mihail.reznichenko.98@mail.ru>
2020-03-22 23:47:18 +0700

    Implement UIText, rearrange skins (#115)

---
+++ korge/src/commonMain/kotlin/com/soywiz/korge/ui/defaultExtensions.kt
@@ -0,0 +1,17 @@
+package com.soywiz.korge.ui
+
+import com.soywiz.kds.*
+import com.soywiz.korge.html.*
+import com.soywiz.korge.view.*
+
+var View.defaultTextSkin: TextSkin by extraProperty { DefaultTextSkin }
+var View.defaultUIFont: Html.FontFace by extraProperty { DefaultUIFont }
+var View.defaultUISkin: UISkin by extraProperty { DefaultUISkin }
+var View.defaultCheckSkin: IconSkin by extraProperty { DefaultCheckSkin }
+var View.defaultUpSkin: IconSkin by extraProperty { DefaultUpSkin }
+var View.defaultDownSkin: IconSkin by extraProperty { DefaultDownSkin }
+var View.defaultLeftSkin: IconSkin by extraProperty { DefaultLeftSkin }
+var View.defaultRightSkin: IconSkin by extraProperty { DefaultRightSkin }
+var View.defaultComboBoxSkin: ComboBoxSkin by extraProperty { DefaultComboBoxSkin }
+var View.defaultVerScrollBarSkin: ScrollBarSkin by extraProperty { DefaultVerScrollBarSkin }
+var View.defaultHorScrollBarSkin: ScrollBarSkin by extraProperty { DefaultHorScrollBarSkin }
