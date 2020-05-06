467301099535ce8f2113e0585f83dcf4d3f935cd
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-05-06 19:16:58 +0200

    Try to skip I/O based tests for now until we copy files to the test folder

--- korge/src/commonTest/kotlin/com/soywiz/korge3d/test.kt
+++ korge/src/commonTest/kotlin/com/soywiz/korge3d/test.kt
@@ -3,11 +3,12 @@ package com.soywiz.korge3d
 import com.soywiz.korge3d.format.*
 import com.soywiz.korio.async.*
 import com.soywiz.korio.file.std.*
+import doIOTest
 import kotlin.test.*
 
 class Library3DTest {
     @Test
-    fun test() = suspendTest {
+    fun test() = suspendTest({ doIOTest }) {
         val library = resourcesVfs["scene.dae"].readColladaLibrary()
         println(library)
     }
