9e8f5f06ae703705fe90ca444b4e05514805dc08
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-13 21:32:07 +0200

    Added SpriteAnimation.kt (#149)
    * Added SpriteAnimation.kt
    For a sample how to use it see PR from korge-samples
    
    * Added SpriteAnimation.kt. Rework after review
    For a sample how to use it see PR from korge-samples
    
    * Rework after review #2
    For a sample how to use it see PR from korge-samples
    
    * Cleanup
    
    * - Cleanup
    - Improvements
    - Added doc
    - Added trigger Signals
    - Added package
    
    * - Added Builder Function
    *CANNOT TEST*
    
    * - Revised Builder Functions And Constructors

---
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/Sprite.kt
@@ -0,0 +1,146 @@
+package com.soywiz.korge.view
+
+import com.soywiz.klock.*
+import com.soywiz.korge.view.*
+import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korim.bitmap.Bitmaps
+import com.soywiz.korim.bitmap.BmpSlice
+import com.soywiz.korio.async.Signal
+import com.soywiz.korma.geom.vector.VectorPath
+
+inline fun Container.sprite(
+    initialAnimation: SpriteAnimation, anchorX: Double = 0.0, anchorY: Double = 0.0, callback: @ViewsDslMarker Sprite.() -> Unit = {}
+): Sprite = Sprite(initialAnimation, anchorX, anchorY).addTo(this).apply(callback)
+
+inline fun Container.sprite(
+    texture: BmpSlice, anchorX: Double = 0.0, anchorY: Double = 0.0, callback: @ViewsDslMarker Sprite.() -> Unit = {}
+): Sprite = Sprite(texture, anchorX, anchorY).addTo(this).apply(callback)
+
+inline fun Container.sprite(
+    texture: Bitmap, anchorX: Double = 0.0, anchorY: Double = 0.0, callback: @ViewsDslMarker Sprite.() -> Unit = {}
+): Sprite = Sprite(texture, anchorX, anchorY).addTo(this).apply(callback)
+
+/**
+ * A [Sprite] is basically an [Image] with added abilities to display a [SpriteAnimation]
+ * The regular usage is to initialize the [Sprite] with one [SpriteAnimation]. The first
+ * displayed bitmap will be the first element of the [SpriteAnimation]s spriteStack.
+ * @property animationRequested Boolean
+ * @property animationCyclesRequested Int
+ * @property onAnimationCompleted Signal<SpriteAnimation>
+ * @property onAnimationStopped Signal<SpriteAnimation>
+ * @property onAnimationStarted Signal<SpriteAnimation>
+ * @property animationLooped Boolean
+ * @property lastAnimationFrameTime TimeSpan
+ * @property animationRequestedDuration TimeSpan
+ * @property spriteDisplayTime TimeSpan
+ * @property currentAnimation SpriteAnimation?
+ * @property currentSpriteIndex Int
+ * @constructor It is possible to initialize a [Sprite] with a static [Bitmap] or [BmpSlice].
+ * This will be exchanged when starting a [SpriteAnimation] with one of the available play functions
+ */
+open class Sprite(
+    bitmap : Bitmap,
+    anchorX: Double = 0.0,
+    anchorY: Double = anchorX,
+    hitShape: VectorPath? = null,
+    smoothing: Boolean = true) : Image(bitmap, anchorX, anchorY, hitShape, smoothing) {
+    constructor(
+        bmpSlice : BmpSlice,
+        anchorX: Double = 0.0,
+        anchorY: Double = anchorX,
+        hitShape: VectorPath? = null,
+        smoothing: Boolean = true) : this(bmpSlice.bmp, anchorX, anchorY, hitShape, smoothing)
+    constructor(
+        initialAnimation : SpriteAnimation,
+        anchorX: Double = 0.0,
+        anchorY: Double = anchorX,
+        hitShape: VectorPath? = null,
+        smoothing: Boolean = true) : this(initialAnimation.firstSprite, anchorX, anchorY, hitShape, smoothing){
+        currentAnimation = initialAnimation
+        bitmap = currentAnimation?.firstSprite ?: Bitmaps.transparent
+    }
+
+    private var animationRequested = false
+    private var animationCyclesRequested = 0
+        set(value) {
+            if (value == 0)
+                triggerEvent(onAnimationCompleted)
+            field = value
+        }
+    val onAnimationCompleted = Signal<SpriteAnimation>()
+    val onAnimationStopped = Signal<SpriteAnimation>()
+    val onAnimationStarted = Signal<SpriteAnimation>()
+
+    var spriteDisplayTime : TimeSpan = 50.milliseconds
+    private var animationLooped = false
+    private var lastAnimationFrameTime  : TimeSpan = 0.milliseconds
+    private var animationRequestedDuration : TimeSpan = 0.milliseconds
+
+    private var currentAnimation : SpriteAnimation? = null
+    private var currentSpriteIndex = 0
+
+    init {
+        addUpdater { frameTime ->
+            if (animationRequested){
+                nextSprite(frameTime)
+            }
+        }
+    }
+
+
+    fun playAnimation(spriteAnimation: SpriteAnimation, spriteDisplayTime: TimeSpan = this.spriteDisplayTime) = updateCurrentAnimation(spriteAnimation = spriteAnimation, spriteDisplayTime = spriteDisplayTime)
+
+    fun playAnimation(times: Int = 1, spriteAnimation: SpriteAnimation, spriteDisplayTime: TimeSpan = this.spriteDisplayTime) =
+        updateCurrentAnimation(
+            spriteAnimation = spriteAnimation,
+            spriteDisplayTime = spriteDisplayTime,
+            animationCyclesRequested = times*(currentAnimation?.spriteStackSize ?: 0)
+        )
+
+    fun playAnimationForDuration(duration: TimeSpan, spriteAnimation: SpriteAnimation, spriteDisplayTime: TimeSpan = this.spriteDisplayTime) =
+        updateCurrentAnimation(
+            spriteAnimation = spriteAnimation,
+            spriteDisplayTime = spriteDisplayTime,
+            duration = duration
+        )
+
+    fun playAnimationLooped(spriteAnimation: SpriteAnimation, spriteDisplayTime: TimeSpan = this.spriteDisplayTime) =
+        updateCurrentAnimation(
+            spriteAnimation = spriteAnimation,
+            spriteDisplayTime = spriteDisplayTime,
+            looped = true
+        )
+
+    fun stopAnimation() {
+        animationRequested = false
+        triggerEvent(onAnimationStopped)
+    }
+
+    private fun nextSprite(frameTime : TimeSpan){
+        lastAnimationFrameTime+=frameTime
+        if ((animationCyclesRequested > 0 || animationRequestedDuration > 0.milliseconds || animationLooped) && lastAnimationFrameTime+frameTime >= this.spriteDisplayTime){
+            bitmap = currentAnimation?.getSprite(++currentSpriteIndex) ?: Bitmaps.transparent
+            animationCyclesRequested--
+            animationRequestedDuration-=(frameTime+spriteDisplayTime)
+            lastAnimationFrameTime = 0.milliseconds
+        }
+    }
+
+    private fun updateCurrentAnimation(
+        spriteAnimation: SpriteAnimation,
+        spriteDisplayTime: TimeSpan = this.spriteDisplayTime,
+        animationCyclesRequested : Int = 1,
+        duration : TimeSpan = 0.milliseconds,
+        looped : Boolean = false
+    ){
+        triggerEvent(onAnimationStarted)
+        this.spriteDisplayTime = spriteDisplayTime
+        currentAnimation = spriteAnimation
+        animationRequested = true
+        animationLooped = looped
+        animationRequestedDuration = duration
+        this.animationCyclesRequested = if (!looped) animationCyclesRequested else 1
+    }
+
+    private fun triggerEvent(signal : Signal<SpriteAnimation>) = currentAnimation?.let { signal.invoke(it) }
+}
---
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/SpriteAnimation.kt
@@ -0,0 +1,48 @@
+package com.soywiz.korge.view
+
+import com.soywiz.klock.TimeSpan
+import com.soywiz.klock.measureTime
+import com.soywiz.klock.milliseconds
+import com.soywiz.korge.view.Image
+import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korim.bitmap.BmpSlice
+import com.soywiz.korim.bitmap.sliceWithSize
+import com.soywiz.korio.async.delay
+
+class SpriteAnimation(
+    spriteMap: Bitmap,
+    spriteWidth: Int = 16,
+    spriteHeight: Int = 16,
+    marginTop: Int = 0,
+    marginLeft: Int = 0,
+    columns: Int = 1,
+    rows: Int = 1,
+    offsetBetweenColumns: Int = 0,
+    offsetBetweenRows: Int = 0) {
+
+    private var spriteStack: MutableList<BmpSlice> = mutableListOf()
+    private var cycles = 0
+    private var currentSpriteIndex = 0
+    private var stop = false
+    val spriteStackSize : Int
+        get() = spriteStack.size
+    val firstSprite : BmpSlice
+        get() = spriteStack[0]
+
+    init{
+        for (col in 0 until columns){
+            for (row in 0 until rows){
+                spriteStack.add(
+                    spriteMap.sliceWithSize(
+                        marginLeft + (spriteWidth + offsetBetweenColumns) * col,
+                        marginTop + (spriteHeight + offsetBetweenRows) * row,
+                        spriteWidth,
+                        spriteHeight
+                    )
+                )
+            }
+        }
+    }
+
+    fun getSprite(index : Int) = spriteStack[(index % spriteStack.size)]
+}
