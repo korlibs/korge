76f4d1ac2bbc5aafab3b40648e3754410ec0fabc
soywiz <soywiz@gmail.com>
2020-06-03 04:04:41 +0200

    Do not apply the kernel to the alpha component by default in the Convolute3Filter

--- korge/src/commonMain/kotlin/com/soywiz/korge/view/filter/Convolute3Filter.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/filter/Convolute3Filter.kt
@@ -14,26 +14,27 @@ class Convolute3Filter(
     /** Distance between the origin pixel for sampling for edges */
     dist: Double = 1.0
 ) : ShaderFilter() {
-	companion object {
+    companion object {
+        private val u_ApplyAlpha = Uniform("apply_alpha", VarType.Float1)
         private val u_Dist = Uniform("dist", VarType.Float1)
-		private val u_Weights = Uniform("weights", VarType.Mat3)
+        private val u_Weights = Uniform("weights", VarType.Mat3)
 
         /** A Gaussian Blur Kernel. This [Matrix3D] can be used as [kernel] for [Convolute3Filter] */
-		val KERNEL_GAUSSIAN_BLUR: Matrix3D = Matrix3D.fromRows3x3(
+        val KERNEL_GAUSSIAN_BLUR: Matrix3D = Matrix3D.fromRows3x3(
             1f, 2f, 1f,
             2f, 4f, 2f,
             1f, 2f, 1f
         ) * (1f / 16f)
 
         /** A Box Blur Kernel. This [Matrix3D] can be used as [kernel] for [Convolute3Filter] */
-		val KERNEL_BOX_BLUR: Matrix3D = Matrix3D.fromRows3x3(
+        val KERNEL_BOX_BLUR: Matrix3D = Matrix3D.fromRows3x3(
             1f, 1f, 1f,
             1f, 1f, 1f,
             1f, 1f, 1f
         ) * (1f / 9f)
 
         /** An Identity Kernel (doesn't perform any operation). This [Matrix3D] can be used as [kernel] for [Convolute3Filter] */
-		val KERNEL_IDENTITY: Matrix3D = Matrix3D.fromRows3x3(
+        val KERNEL_IDENTITY: Matrix3D = Matrix3D.fromRows3x3(
             0f, 0f, 0f,
             0f, 1f, 0f,
             0f, 0f, 0f
@@ -59,24 +60,29 @@ class Convolute3Filter(
 
                 for (y in 0 until 3) {
                     for (x in 0 until 3) {
-                        out setTo out + (tex(
+                        val color = tex(
                             fragmentCoords + vec2(
                                 (x - 1).toFloat().lit * u_Dist,
                                 (y - 1).toFloat().lit * u_Dist
                             )
-                        )) * u_Weights[x][y]
+                        )
+                        out setTo out + (color * u_Weights[x][y])
                     }
+                }
+                IF(u_ApplyAlpha ne 1f.lit) {
+                    out["a"] setTo tex(fragmentCoords)["a"]
                 }
             }
         }
-	}
+    }
 
-    private val distStorage = uniforms.storageFor(u_Dist)
     /** 3x3 matrix representing a convolution kernel */
-	val weights by uniforms.storageForMatrix3D(u_Weights, kernel)
+    val weights by uniforms.storageForMatrix3D(u_Weights, kernel)
     /** Distance between the origin pixel for sampling for edges */
-    var dist by distStorage.doubleDelegateX(dist)
+    var dist by uniforms.storageFor(u_Dist).doubleDelegateX(dist)
+    /** Whether or not kernel must be applied to the alpha component */
+    var applyAlpha by uniforms.storageFor(u_ApplyAlpha).boolDelegateX(false)
 
-	override val border: Int get() = dist.toInt()
+    override val border: Int get() = dist.toInt()
     override val fragment = FRAGMENT_SHADER
 }
