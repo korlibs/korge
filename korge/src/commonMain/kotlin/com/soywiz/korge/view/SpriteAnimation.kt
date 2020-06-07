1ac8b68cb67ae906c9803f01ff4c8c8692fbe759
soywiz <soywiz@gmail.com>
2020-06-07 14:26:59 +0200

    Refactor SpriteAnimation to be able to build from a list of BmpSlice

--- korge/src/commonMain/kotlin/com/soywiz/korge/view/SpriteAnimation.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/SpriteAnimation.kt
@@ -5,38 +5,40 @@ import com.soywiz.korim.bitmap.Bitmap
 import com.soywiz.korim.bitmap.BmpSlice
 import com.soywiz.korim.bitmap.sliceWithSize
 
-class SpriteAnimation(
-    spriteMap: Bitmap,
-    spriteWidth: Int = 16,
-    spriteHeight: Int = 16,
-    marginTop: Int = 0,
-    marginLeft: Int = 0,
-    columns: Int = 1,
-    rows: Int = 1,
-    offsetBetweenColumns: Int = 0,
-    offsetBetweenRows: Int = 0
-) {
-    private val spriteStack: List<BmpSlice> = ArrayList<BmpSlice>().apply {
-        for (row in 0 until rows){
-            for (col in 0 until columns){
-                add(
-                    spriteMap.sliceWithSize(
-                        marginLeft + (spriteWidth + offsetBetweenColumns) * col,
-                        marginTop + (spriteHeight + offsetBetweenRows) * row,
-                        spriteWidth,
-                        spriteHeight
-                    )
-                )
-            }
+class SpriteAnimation(val spriteStack: List<BmpSlice>) {
+    companion object {
+        operator fun invoke(
+            spriteMap: Bitmap,
+            spriteWidth: Int = 16,
+            spriteHeight: Int = 16,
+            marginTop: Int = 0,
+            marginLeft: Int = 0,
+            columns: Int = 1,
+            rows: Int = 1,
+            offsetBetweenColumns: Int = 0,
+            offsetBetweenRows: Int = 0
+        ): SpriteAnimation {
+            return SpriteAnimation(
+                ArrayList<BmpSlice>().apply {
+                    for (row in 0 until rows){
+                        for (col in 0 until columns){
+                            add(
+                                spriteMap.sliceWithSize(
+                                    marginLeft + (spriteWidth + offsetBetweenColumns) * col,
+                                    marginTop + (spriteHeight + offsetBetweenRows) * row,
+                                    spriteWidth,
+                                    spriteHeight
+                                )
+                            )
+                        }
+                    }
+                }
+            )
         }
     }
-    private var cycles = 0
-    private var stop = false
-    private var currentSpriteIndex = 0
-    val spriteStackSize : Int
-        get() = spriteStack.size
-    val firstSprite : BmpSlice
-        get() = spriteStack[0]
 
-    fun getSprite(index : Int) : BmpSlice = spriteStack[index umod spriteStack.size]
+    val spriteStackSize : Int get() = spriteStack.size
+    val firstSprite : BmpSlice get() = spriteStack[0]
+    fun getSprite(index: Int): BmpSlice = spriteStack[index umod spriteStack.size]
+    operator fun get(index: Int) = getSprite(index)
 }
