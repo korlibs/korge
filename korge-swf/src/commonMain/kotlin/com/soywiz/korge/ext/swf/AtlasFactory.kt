044011d90ec3f70fd3b3b9d4092c6adb8ce4a1d5
soywiz <soywiz@gmail.com>
2020-06-20 00:24:21 +0200

    Unified AtlasPacker for atlas processor + SWF

--- korge-swf/src/commonMain/kotlin/com/soywiz/korge/ext/swf/AtlasFactory.kt
+++ korge-swf/src/commonMain/kotlin/com/soywiz/korge/ext/swf/AtlasFactory.kt
@@ -2,6 +2,7 @@ package com.soywiz.korge.ext.swf
 
 import com.soywiz.kds.*
 import com.soywiz.kmem.*
+import com.soywiz.korge.atlas.AtlasPacker
 import com.soywiz.korge.render.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.bitmap.*
@@ -28,7 +29,23 @@ suspend fun <T> Map<T, BitmapWithScale>.toAtlas(
 	maxTextureSide: Int,
 	mipmaps: Boolean
 ): Map<T, TextureWithBitmapSlice> {
-    val borderSize = 4
+    val atlas = AtlasPacker.pack(this.entries.toList().map { it to it.value.bitmap.slice() }, maxSide = maxTextureSide)
+    val out = LinkedHashMap<T, TextureWithBitmapSlice>()
+    for (at in atlas.atlases) {
+        for (item in at.packedItems) {
+            val ibmp = item.item.value
+            out[item.item.key] = TextureWithBitmapSlice(
+                texture = item.slice,
+                bitmapSlice = item.slice,
+                scale = ibmp.scale,
+                bounds = ibmp.bounds
+            )
+        }
+    }
+    return out
+    //val borderSize = 4
+    /*
+    val borderSize = 8
     val alignTo = 8
     val borderSize2 = borderSize * 2
 
@@ -55,26 +72,6 @@ suspend fun <T> Map<T, BitmapWithScale>.toAtlas(
             val dy1 = dy0 + height - 1
 
 			bmp.put(ibmp.bitmap.toBMP32(), dx0, dy0)
-
-            //println("dx=$dx0,dy=$dy0 - $dx1,$dy1 -- $width, $height, [[${bmp.width}x${bmp.height}]]")
-
-            val alphaThresold = 0.75
-
-            for (y in 0 until height) {
-                val py = dy0 + y
-                val v0 = bmp[dx0, py]
-                val v1 = bmp[dx1, py]
-                if (v0.ad >= alphaThresold) for (i in 1..borderSize) bmp[dx0 - i, py] = v0
-                if (v1.ad >= alphaThresold) for (i in 1..borderSize) bmp[dx1 + i, py] = v1
-            }
-
-            for (x in -2 until width + 2) {
-                val px = dx0 + x
-                val v0 = bmp[px, dy0]
-                val v1 = bmp[px, dy1]
-                if (v0.ad >= alphaThresold) for (i in 1..borderSize) bmp[px, dy0 - i] = v0
-                if (v1.ad >= alphaThresold) for (i in 1..borderSize) bmp[px, dy1 + i] = v1
-            }
 		}
 
 		val texture = bmp.slice()
@@ -91,4 +88,5 @@ suspend fun <T> Map<T, BitmapWithScale>.toAtlas(
 		}
 	}
 	return this.mapValues { bitmapsToTextures[it.value]!! }
+     */
 }
