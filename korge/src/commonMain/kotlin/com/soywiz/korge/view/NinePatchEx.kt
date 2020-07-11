96aa61fe7f0c6630e77c01131880124d252e9732
soywiz <soywiz@gmail.com>
2020-07-11 13:11:07 +0200

    Fixes 9 patch rendering to not deform (keeping aspect ratio) on fixed X and Y segments

--- korge/src/commonMain/kotlin/com/soywiz/korge/view/NinePatchEx.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/NinePatchEx.kt
@@ -1,9 +1,11 @@
 package com.soywiz.korge.view
 
 import com.soywiz.kds.*
+import com.soywiz.kds.iterators.*
 import com.soywiz.korge.render.*
 import com.soywiz.korim.bitmap.*
 import com.soywiz.korma.geom.*
+import kotlin.math.*
 
 inline fun Container.ninePatch(
 	tex: NinePatchEx.Tex, width: Double, height: Double, callback: @ViewDslMarker NinePatchEx.() -> Unit
@@ -42,7 +44,7 @@ class NinePatchEx(
 
 		m.keep {
 			prescale(1.0 / xscale, 1.0 / yscale)
-			ninePatch.info.computeScale(bounds) { segment, x, y, width, height ->
+			ninePatch.info.computeScaleFixed(bounds) { segment, x, y, width, height ->
 				ctx.batch.drawQuad(
 					ctx.getTex(ninePatch.getSliceTex(segment)),
 					x.toFloat(), y.toFloat(),
@@ -56,6 +58,85 @@ class NinePatchEx(
 			}
 		}
 	}
+
+    private val xComputed = IntArray(64)
+    private val yComputed = IntArray(64)
+    // @TODO: Apply this fix to KorIM
+    // @TODO: Refactor this
+    fun NinePatchInfo.computeScaleFixed(
+        bounds: RectangleInt,
+        callback: (segment: NinePatchInfo.Segment, x: Int, y: Int, width: Int, height: Int) -> Unit
+    ) {
+        //println("scaleFixed=($scaleFixedX,$scaleFixedY)")
+
+        ysegments.fastForEachWithIndex { index, _ -> yComputed[index] = Int.MAX_VALUE }
+        xsegments.fastForEachWithIndex { index, _ -> xComputed[index] = Int.MAX_VALUE }
+
+        ysegments.fastForEachWithIndex { yindex, y ->
+            val segHeight = y.computedLength(this.yaxis, bounds.height)
+            xsegments.fastForEachWithIndex { xindex, x ->
+                val segWidth = x.computedLength(this.xaxis, bounds.width)
+                if (x.fixed && y.fixed) {
+                    val xScale = segWidth / x.length.toDouble()
+                    val yScale = segHeight / y.length.toDouble()
+                    val minScale = min(xScale, yScale)
+                    xComputed[xindex] = min(xComputed[xindex], (x.length * minScale).toInt())
+                    yComputed[yindex] = min(yComputed[yindex], (y.length * minScale).toInt())
+                } else {
+                    xComputed[xindex] = min(xComputed[xindex], segWidth.toInt())
+                    yComputed[yindex] = min(yComputed[yindex], segHeight.toInt())
+                }
+            }
+        }
+
+        val denormalizedWidth = xComputed.sum()
+        val denormalizedHeight = yComputed.sum()
+        val denormalizedScaledWidth = xsegments.mapIndexed { index, it -> if (it.scaled) xComputed[index] else 0 }.sum()
+        val denormalizedScaledHeight = ysegments.mapIndexed { index, it -> if (it.scaled) yComputed[index] else 0 }.sum()
+        val xScaledRatio = if (denormalizedWidth > 0) denormalizedScaledWidth.toDouble() / denormalizedWidth.toDouble() else 1.0
+        val yScaledRatio = if (denormalizedWidth > 0) denormalizedScaledHeight.toDouble() / denormalizedHeight.toDouble() else 1.0
+
+        for (n in 0 until 2) {
+            val segments = if (n == 0) ysegments else xsegments
+            val computed = if (n == 0) yComputed else xComputed
+            val denormalizedScaledLen = if (n == 0) denormalizedScaledHeight else denormalizedScaledWidth
+            val side = if (n == 0) bounds.height else bounds.width
+            val scaledRatio = if (n == 0) yScaledRatio else xScaledRatio
+            val scaledSide = side * scaledRatio
+
+            segments.fastForEachWithIndex { index, v ->
+                if (v.scaled) {
+                    computed[index] = (scaledSide * (computed[index].toDouble() / denormalizedScaledLen.toDouble())).toInt()
+                }
+            }
+        }
+
+        val xRemaining = bounds.width - xComputed.sum()
+        val yRemaining = bounds.height - yComputed.sum()
+        val xScaledFirst = xsegments.indexOfFirst { it.scaled }
+        val yScaledFirst = ysegments.indexOfFirst { it.scaled }
+        if (xRemaining > 0 && xScaledFirst >= 0) xComputed[xScaledFirst] += xRemaining
+        if (yRemaining > 0 && yScaledFirst >= 0) yComputed[yScaledFirst] += yRemaining
+
+        var ry = 0
+        for (yindex in ysegments.indices) {
+            val segHeight = yComputed[yindex].toInt()
+            var rx = 0
+            for (xindex in xsegments.indices) {
+                val segWidth = xComputed[xindex].toInt()
+
+                val seg = segments[yindex][xindex]
+                val segLeft = (rx + bounds.left).toInt()
+                val segTop = (ry + bounds.top).toInt()
+
+                //println("($x,$y):($segWidth,$segHeight)")
+                callback(seg, segLeft, segTop, segWidth.toInt(), segHeight.toInt())
+
+                rx += segWidth
+            }
+            ry += segHeight
+        }
+    }
 
 	override fun getLocalBoundsInternal(out: Rectangle) {
 		out.setTo(0.0, 0.0, width, height)
---
+++ korge/src/commonTest/kotlin/com/soywiz/korge/view/NinePatchTest.kt
@@ -0,0 +1,114 @@
+package com.soywiz.korge.view
+
+import com.soywiz.korge.render.*
+import com.soywiz.korge.tests.*
+import com.soywiz.korim.bitmap.*
+import com.soywiz.korio.file.std.*
+import kotlin.test.*
+
+class NinePatchTest : ViewsForTesting() {
+    @Test
+    fun testNinePatch() = viewsTest {
+        val vertices = arrayListOf<List<VertexInfo>>()
+        val ninePatch = resourcesVfs["npatch/9patch.9.png"].readNinePatch()
+
+        ninePatch(ninePatch, 450.0, 600.0) {
+            position(0, 0)
+        }
+        val log = testRenderContext { ctx ->
+            ctx.batch.beforeFlush { vertices.add(it.readVertices()) }
+            render(ctx)
+        }
+        assertEquals(1, vertices.size) // 1 batch
+        val batch = vertices[0]
+        assertEquals(36, batch.size) // 36 vertices
+        assertEquals(
+            """
+                [(0,0), (1, 1)]
+                [(49,0), (50, 1)]
+                [(49,47), (50, 48)]
+                [(0,47), (1, 48)]
+                [(49,0), (50, 1)]
+                [(400,0), (78, 1)]
+                [(400,47), (78, 48)]
+                [(49,47), (50, 48)]
+                [(400,0), (78, 1)]
+                [(450,0), (128, 1)]
+                [(450,47), (128, 48)]
+                [(400,47), (78, 48)]
+                [(0,47), (1, 48)]
+                [(49,47), (50, 48)]
+                [(49,553), (50, 57)]
+                [(0,553), (1, 57)]
+                [(49,47), (50, 48)]
+                [(400,47), (78, 48)]
+                [(400,553), (78, 57)]
+                [(49,553), (50, 57)]
+                [(400,47), (78, 48)]
+                [(450,47), (128, 48)]
+                [(450,553), (128, 57)]
+                [(400,553), (78, 57)]
+                [(0,553), (1, 57)]
+                [(49,553), (50, 57)]
+                [(49,600), (50, 104)]
+                [(0,600), (1, 104)]
+                [(49,553), (50, 57)]
+                [(400,553), (78, 57)]
+                [(400,600), (78, 104)]
+                [(49,600), (50, 104)]
+                [(400,553), (78, 57)]
+                [(450,553), (128, 57)]
+                [(450,600), (128, 104)]
+                [(400,600), (78, 104)]
+            """.trimIndent(),
+            batch.joinToString("\n") { it.toStringXYUV() }
+        )
+        //assertEquals("--", log.getLogAsString())
+    }
+
+    @Test
+    fun testNinePatchSmaller() = viewsTest {
+        val ninePatch = resourcesVfs["npatch/9patch.9.png"].readNinePatch()
+
+        fun computeVertices(ninePatch: NinePatchBitmap32, width: Double, height: Double): List<List<VertexInfo>> {
+            val vertices = arrayListOf<List<VertexInfo>>()
+            stage.removeChildren()
+            testRenderContext { ctx ->
+                val view = ninePatch(ninePatch, width, height) {
+                    position(0, 0)
+                }
+                ctx.batch.beforeFlush { vertices.add(it.readVertices()) }
+                view.render(ctx)
+            }
+            return vertices
+        }
+
+        fun computeInterestingPoints(width: Number, height: Number): String {
+            val batch = computeVertices(ninePatch, width.toDouble(), height.toDouble())[0]
+            return listOf(batch[0], batch[2], batch[32], batch[34]).joinToString(", ") { it.toStringXY() }
+        }
+
+        assertEquals(
+            """
+                [0,0], [49,47], [400,553], [450,600]
+                [0,0], [15,14], [435,18], [450,32]
+                [0,0], [12,11], [20,589], [32,600]
+                [0,0], [12,11], [20,21], [32,32]
+            """.trimIndent(),
+            listOf(
+                computeInterestingPoints(450, 600),
+                computeInterestingPoints(450, 32),
+                computeInterestingPoints(32, 600),
+                computeInterestingPoints(32, 32)
+            ).joinToString("\n")
+        )
+    }
+}
+
+//
+// +-+--------+-+
+// +-+--------+-+
+// | |        | |
+// | |        | |
+// +-+--------+-+
+// +-+--------+-+
