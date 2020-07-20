676983463381130525bd02e895ed730ae090161d
soywiz <soywiz@gmail.com>
2020-07-20 23:54:51 +0200

    Revert "Rename .java to .kt"
    This reverts commit 04025f92

--- korge-spine/src/jvmMain/java/com/badlogic/gdx/utils/Predicate.kt
+++ korge-spine/src/jvmMain/java/com/badlogic/gdx/utils/Predicate.kt
@@ -5,110 +5,106 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
-package com.badlogic.gdx.utils;
-
-import java.util.Iterator;
+package com.badlogic.gdx.utils
 
 /** Interface used to select items within an iterator against a predicate.
- * @author Xoppa */
-public interface Predicate<T> {
+ * @author Xoppa
+ */
+interface Predicate<T> {
 
-    /** @return true if the item matches the criteria and should be included in the iterator's items */
-    boolean evaluate (T arg0);
+    /** @return true if the item matches the criteria and should be included in the iterator's items
+     */
+    fun evaluate(arg0: T): Boolean
 
-    public class PredicateIterator<T> implements Iterator<T> {
-        public Iterator<T> iterator;
-        public Predicate<T> predicate;
-        public boolean end = false;
-        public boolean peeked = false;
-        public T next = null;
+    class PredicateIterator<T>(iterator: MutableIterator<T?>, predicate: Predicate<T?>) : MutableIterator<T?> {
+        lateinit var iterator: MutableIterator<T?>
+        lateinit var predicate: Predicate<T?>
+        var end = false
+        var peeked = false
+        var next: T? = null
 
-        public PredicateIterator (final Iterable<T> iterable, final Predicate<T> predicate) {
-            this(iterable.iterator(), predicate);
+        constructor(iterable: MutableIterable<T>, predicate: Predicate<T?>) : this(iterable.iterator(), predicate) {}
+
+        init {
+            set(iterator, predicate)
         }
 
-        public PredicateIterator (final Iterator<T> iterator, final Predicate<T> predicate) {
-            set(iterator, predicate);
+        fun set(iterable: MutableIterable<T?>, predicate: Predicate<T?>) {
+            set(iterable.iterator(), predicate)
         }
 
-        public void set (final Iterable<T> iterable, final Predicate<T> predicate) {
-            set(iterable.iterator(), predicate);
+        fun set(iterator: MutableIterator<T?>, predicate: Predicate<T?>) {
+            this.iterator = iterator
+            this.predicate = predicate
+            peeked = false
+            end = peeked
+            next = null
         }
 
-        public void set (final Iterator<T> iterator, final Predicate<T> predicate) {
-            this.iterator = iterator;
-            this.predicate = predicate;
-            end = peeked = false;
-            next = null;
-        }
-
-        @Override
-        public boolean hasNext () {
-            if (end) return false;
-            if (next != null) return true;
-            peeked = true;
+        override fun hasNext(): Boolean {
+            if (end) return false
+            if (next != null) return true
+            peeked = true
             while (iterator.hasNext()) {
-                final T n = iterator.next();
+                val n = iterator.next()
                 if (predicate.evaluate(n)) {
-                    next = n;
-                    return true;
+                    next = n
+                    return true
                 }
             }
-            end = true;
-            return false;
+            end = true
+            return false
         }
 
-        @Override
-        public T next () {
-            if (next == null && !hasNext()) return null;
-            final T result = next;
-            next = null;
-            peeked = false;
-            return result;
+        override fun next(): T? {
+            if (next == null && !hasNext()) return null
+            val result = next
+            next = null
+            peeked = false
+            return result
         }
 
-        @Override
-        public void remove () {
-            if (peeked) throw new GdxRuntimeException("Cannot remove between a call to hasNext() and next().");
-            iterator.remove();
+        override fun remove() {
+            if (peeked) throw GdxRuntimeException("Cannot remove between a call to hasNext() and next().")
+            iterator.remove()
         }
     }
 
-    public static class PredicateIterable<T> implements Iterable<T> {
-        public Iterable<T> iterable;
-        public Predicate<T> predicate;
-        public PredicateIterator<T> iterator = null;
+    class PredicateIterable<T>(iterable: MutableIterable<T?>, predicate: Predicate<T?>) : MutableIterable<T?> {
+        lateinit var iterable: MutableIterable<T?>
+        lateinit var predicate: Predicate<T?>
+        var iterator: PredicateIterator<T?>? = null
 
-        public PredicateIterable (Iterable<T> iterable, Predicate<T> predicate) {
-            set(iterable, predicate);
+        init {
+            set(iterable, predicate)
         }
 
-        public void set (Iterable<T> iterable, Predicate<T> predicate) {
-            this.iterable = iterable;
-            this.predicate = predicate;
+        operator fun set(iterable: MutableIterable<T?>, predicate: Predicate<T?>) {
+            this.iterable = iterable
+            this.predicate = predicate
         }
 
         /** Returns an iterator. Remove is supported.
-         * <p>
-         * If {@link Collections#allocateIterators} is false, the same iterator instance is returned each time this method is called. Use
-         * the {@link Predicate.PredicateIterator} constructor for nested or multithreaded iteration. */
-        @Override
-        public Iterator<T> iterator () {
-            if (Collections.allocateIterators) return new PredicateIterator<T>(iterable.iterator(), predicate);
+         *
+         *
+         * If [Collections.allocateIterators] is false, the same iterator instance is returned each time this method is called. Use
+         * the [Predicate.PredicateIterator] constructor for nested or multithreaded iteration.  */
+        override fun iterator(): MutableIterator<T?> {
+            if (Collections.allocateIterators) return PredicateIterator(iterable.iterator(), predicate)
             if (iterator == null)
-                iterator = new PredicateIterator<T>(iterable.iterator(), predicate);
+                iterator = PredicateIterator(iterable.iterator(), predicate)
             else
-                iterator.set(iterable.iterator(), predicate);
-            return iterator;
+                iterator!!.set(iterable.iterator(), predicate)
+            return iterator!!
         }
     }
 }
