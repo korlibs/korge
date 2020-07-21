0660674e94a4bc0b74f79832426bea0a778bf33e
soywiz <soywiz@gmail.com>
2020-07-21 11:11:55 +0200

    [Spine] Converted some Java files into Kotlin ones

--- korge-spine/src/jvmMain/java/com/badlogic/gdx/utils/JDataInput.kt
+++ korge-spine/src/jvmMain/java/com/badlogic/gdx/utils/JDataInput.kt
@@ -1,40 +1,15 @@
-package com.badlogic.gdx.utils;
+package com.badlogic.gdx.utils
 
-import java.io.IOException;
+import java.io.IOException
 
-public class JDataInput {
-    public JDataInput(byte[] data) {
-
-    }
-
-    public float readFloat() {
-        return 0;
-    }
-
-    public boolean readBoolean() {
-        return false;
-    }
-
-    public int readInt(boolean b) {
-        return 0;
-    }
-
-    public int readInt() {
-        return 0;
-    }
-
-    public byte readByte() {
-        return 0;
-    }
-
-    public int read() {
-        return 0;
-    }
-
-    public void close() throws IOException  {
-    }
-
-    public short readShort() {
-        return 0;
-    }
+open class JDataInput(private val data: ByteArray) {
+    fun readFloat(): Float = TODO()
+    fun readBoolean(): Boolean = TODO()
+    fun readInt(b: Boolean): Int = TODO()
+    fun readInt(): Int = TODO()
+    fun readByte(): Byte = TODO()
+    fun read(): Int = TODO()
+    @Throws(IOException::class)
+    fun close(): Unit = TODO()
+    fun readShort(): Short = TODO()
 }
