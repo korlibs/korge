c4ce40284e4315c5f14f87fbc0bdc541f859e060
soywiz <soywiz@gmail.com>
2020-07-21 11:18:03 +0200

    [Spine] Converted some Java files into Kotlin ones

--- korge-spine/src/jvmMain/java/com/badlogic/gdx/utils/NumberUtils.kt
+++ korge-spine/src/jvmMain/java/com/badlogic/gdx/utils/NumberUtils.kt
@@ -5,51 +5,58 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
-package com.badlogic.gdx.utils;
+package com.badlogic.gdx.utils
 
-public final class NumberUtils {
-    public static int floatToIntBits (float value) {
-        return Float.floatToIntBits(value);
+object NumberUtils {
+    @JvmStatic
+    fun floatToIntBits(value: Float): Int {
+        return java.lang.Float.floatToIntBits(value)
     }
 
-    public static int floatToRawIntBits (float value) {
-        return Float.floatToRawIntBits(value);
+    @JvmStatic
+    fun floatToRawIntBits(value: Float): Int {
+        return java.lang.Float.floatToRawIntBits(value)
     }
 
     /** Converts the color from a float ABGR encoding to an int ABGR encoding. The alpha is expanded from 0-254 in the float
-     * encoding (see {@link #intToFloatColor(int)}) to 0-255, which means converting from int to float and back to int can be
-     * lossy. */
-    public static int floatToIntColor (float value) {
-        int intBits = Float.floatToRawIntBits(value);
-        intBits |= (int)((intBits >>> 24) * (255f / 254f)) << 24;
-        return intBits;
+     * encoding (see [.intToFloatColor]) to 0-255, which means converting from int to float and back to int can be
+     * lossy.  */
+    @JvmStatic
+    fun floatToIntColor(value: Float): Int {
+        var intBits = java.lang.Float.floatToRawIntBits(value)
+        intBits = intBits or ((intBits.ushr(24) * (255f / 254f)).toInt() shl 24)
+        return intBits
     }
 
     /** Encodes the ABGR int color as a float. The alpha is compressed to 0-254 to avoid using bits in the NaN range (see
-     * {@link Float#intBitsToFloat(int)} javadocs). Rendering which uses colors encoded as floats should expand the 0-254 back to
-     * 0-255. */
-    public static float intToFloatColor (int value) {
-        return Float.intBitsToFloat(value & 0xfeffffff);
+     * [Float.intBitsToFloat] javadocs). Rendering which uses colors encoded as floats should expand the 0-254 back to
+     * 0-255.  */
+    @JvmStatic
+    fun intToFloatColor(value: Int): Float {
+        return java.lang.Float.intBitsToFloat(value and -0x1000001)
     }
 
-    public static float intBitsToFloat (int value) {
-        return Float.intBitsToFloat(value);
+    @JvmStatic
+    fun intBitsToFloat(value: Int): Float {
+        return java.lang.Float.intBitsToFloat(value)
     }
 
-    public static long doubleToLongBits (double value) {
-        return Double.doubleToLongBits(value);
+    @JvmStatic
+    fun doubleToLongBits(value: Double): Long {
+        return java.lang.Double.doubleToLongBits(value)
     }
 
-    public static double longBitsToDouble (long value) {
-        return Double.longBitsToDouble(value);
+    @JvmStatic
+    fun longBitsToDouble(value: Long): Double {
+        return java.lang.Double.longBitsToDouble(value)
     }
 }
