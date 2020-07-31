31cb5178c8a2812a351c5743dc8b0ef74ee6b105
soywiz <soywiz@gmail.com>
2020-07-31 10:49:19 +0200

    Support selecting animations on SpineEditor

--- korge-intellij-plugin/src/main/kotlin/com/soywiz/korge/intellij/editor/formats/SpineEditor.kt
+++ korge-intellij-plugin/src/main/kotlin/com/soywiz/korge/intellij/editor/formats/SpineEditor.kt
@@ -6,6 +6,7 @@ import com.esotericsoftware.spine.Skeleton
 import com.esotericsoftware.spine.korge.skeletonView
 import com.esotericsoftware.spine.readSkeletonBinary
 import com.soywiz.korge.input.onClick
+import com.soywiz.korge.intellij.components.*
 import com.soywiz.korge.intellij.editor.*
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.ui.textButton
@@ -16,39 +17,37 @@ import com.soywiz.korio.file.VfsFile
 import com.soywiz.korio.file.baseName
 
 suspend fun spineEditor(file: VfsFile): KorgeBaseKorgeFileEditor.EditorModule {
-    return createModule(null) {
-        sceneView.textButton(text = "Spine").apply {
-            width = 80.0
-            height = 24.0
-            x = 0.0
-            y = 0.0
-            onClick {
-            }
-        }
-        val atlas = file.parent.listSimple().firstOrNull { it.baseName.endsWith(".atlas") }
-            ?: error("Can't find atlas in ${file.parent}")
-        val skeletonData = file.readSkeletonBinary(atlas.readAtlas(asumePremultiplied = true))
-        val skeleton = Skeleton(skeletonData)
-        val stateData = AnimationStateData(skeletonData)
-        val state = AnimationState(stateData)
+    val atlas = file.parent.listSimple().firstOrNull { it.baseName.endsWith(".atlas") }
+        ?: error("Can't find atlas in ${file.parent}")
+    val skeletonData = file.readSkeletonBinary(atlas.readAtlas(asumePremultiplied = true))
+    val skeleton = Skeleton(skeletonData)
+    val stateData = AnimationStateData(skeletonData)
+    val state = AnimationState(stateData)
 
-        state.setAnimation(0, skeleton.data.animations.last().name, true)
+    val animationNames = skeleton.data.animations.map { it.name }
+
+    val defaultAnimationName = when {
+        "idle" in animationNames -> "idle"
+        "walk" in animationNames -> "walk"
+        "run" in animationNames -> "run"
+        else -> animationNames.lastOrNull() ?: "unknown"
+    }
+
+    val animationProperty = EditableEnumerableProperty("animation", String::class, defaultAnimationName, animationNames.toSet()).apply {
+        this.onChange {
+            state.setAnimation(0, it, true)
+        }
+    }
+
+    return createModule(EditableNodeList {
+        add(EditableSection("Animation", animationProperty))
+    }) {
+        state.setAnimation(0, defaultAnimationName, true)
         //skeleton.setPosition(250f, 20f)
         state.apply(skeleton) // Poses skeleton using current animations. This sets the bones' local SRT.
         //skeleton.updateWorldTransform() // Uses the bones' local SRT to compute their world SRT.
         val skeletonView = sceneView.skeletonView(skeleton, state)
 
         skeletonView.repositionOnResize(views)
-
-        /*
-    sceneView.container {
-        //speed = 2.0
-        speed = 0.5
-        //speed = 1.0
-        scale(1.0)
-        position(400, 800)
-        //solidRect(10.0, 10.0, Colors.RED).centered
-    }
-    */
     }
 }
