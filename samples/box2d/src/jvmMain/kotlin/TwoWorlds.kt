dec8c60a965c05594d0937dd76b1b6b482b9857f
RezMike <mihail.reznichenko.98@mail.ru>
2020-08-01 13:00:06 +0700

    Small improvements for box2d samples

--- samples/box2d/src/jvmMain/kotlin/TwoWorlds.kt
+++ samples/box2d/src/jvmMain/kotlin/TwoWorlds.kt
@@ -7,18 +7,6 @@ import com.soywiz.korma.geom.*
 import org.jbox2d.dynamics.*
 
 suspend fun main() = Korge(quality = GameWindow.Quality.PERFORMANCE, title = "My Awesome Box2D Game!") {
-    /*
-    worldView {
-        solidRect(20, 20, Colors.RED).position(100, 100).rotation(30.degrees)
-            .registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
-        solidRect(20, 20, Colors.RED).position(109, 75)
-            .registerBodyWithFixture(type = BodyType.DYNAMIC)
-        solidRect(20, 20, Colors.RED).position(93, 50).rotation((-15).degrees)
-            .registerBodyWithFixture(type = BodyType.DYNAMIC)
-        solidRect(400, 100, Colors.WHITE).position(0, 300)
-            .registerBodyWithFixture(friction = 0.2)
-    }
-     */
     worldView(gravityY = 100.0) {
         solidRect(20, 20, Colors.RED).position(100, 100).rotation(30.degrees).registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
         solidRect(20, 20, Colors.RED).position(109, 75).registerBodyWithFixture(type = BodyType.DYNAMIC)
@@ -27,7 +15,7 @@ suspend fun main() = Korge(quality = GameWindow.Quality.PERFORMANCE, title = "My
     }
 
     val world2 = worldView(gravityY = 100.0) {
-        position(400, 0)
+        position(410, 0)
         solidRect(20, 20, Colors.RED).position(150, 100).rotation(30.degrees).registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
         solidRect(20, 20, Colors.RED).position(109, 75).registerBodyWithFixture(type = BodyType.DYNAMIC)
         solidRect(20, 20, Colors.RED).position(93, 50).rotation((-15).degrees).registerBodyWithFixture(type = BodyType.DYNAMIC)
@@ -35,6 +23,6 @@ suspend fun main() = Korge(quality = GameWindow.Quality.PERFORMANCE, title = "My
     }
 
     world2.apply {
-        solidRect(20, 20, Colors.RED).position(150, 100).rotation(30.degrees).registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
+        solidRect(20, 20, Colors.RED).position(150, 120).rotation(30.degrees).registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
     }
 }
