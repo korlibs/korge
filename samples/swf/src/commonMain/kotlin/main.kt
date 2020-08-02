d66281f1a7128d3e8b07b4d7892b2ad520bd65da
soywiz <soywiz@gmail.com>
2020-08-02 17:00:19 +0200

    More work

--- samples/swf/src/commonMain/kotlin/main.kt
+++ samples/swf/src/commonMain/kotlin/main.kt
@@ -13,46 +13,6 @@ import com.soywiz.korma.geom.*
 import com.soywiz.korma.geom.vector.*
 
 suspend fun main() = Korge(quality = GameWindow.Quality.PERFORMANCE, title = "SWF", bgcolor = Colors.DARKGREY) {
-    //val swf = resourcesVfs["dog.swf"].readSWF(views, atlasPacking = false)
-    //val swf = resourcesVfs["test1.swf"].readSWF(views, SWFExportConfig(rasterizerMethod = ShapeRasterizerMethod.NONE), false)
-    val swf = resourcesVfs["test1.swf"].readSWF(views, SWFExportConfig(rasterizerMethod = ShapeRasterizerMethod.X4), false)
-    //val swf = resourcesVfs["test4.swf"].readSWF(views, SWFExportConfig(rasterizerMethod = ShapeRasterizerMethod.X4), false)
-    //val swf = resourcesVfs["test4.swf"].readSWF(views, SWFExportConfig(rasterizerMethod = ShapeRasterizerMethod.NONE), false)
-
-    this += swf.createMainTimeLine()
-
-    //image(NativeImage(512, 512).context2d {
-    //    rect(20, 20, 150, 100)
-    //    scale(1.5, 1.5)
-    //    fill(LinearGradientPaint(90, 0, 170, 0).addColorStop(0.0, Colors.DARKGREEN).addColorStop(1.0, Colors.RED))
-    //})
-
-
-    /*
-    graphics {
-        //fill(LinearGradientPaint(0.0, 0.0, 200.0, 0.0, transform = Matrix().pretranslate(50.0, 0.0).scale(1.0)).addColorStop(0.3, Colors.RED).addColorStop(0.7, Colors.BLUE)) {
-        fill(GradientPaint.fromGradientBox(GradientKind.RADIAL, 50.0, 100.0, 0.degrees, 25.0, 0.0).addColorStop(0.0, Colors.GREEN).addColorStop(1.0, Colors.BLUE)) {
-        //fill(GradientPaint.fromGradientBox(GradientKind.LINEAR, 100.0, 50.0, 90.0.degrees, 0.0, 50.0).addColorStop(0.0, Colors.GREEN).addColorStop(1.0, Colors.BLUE)) {
-        //fill(GradientPaint.fromGradientBox(GradientKind.LINEAR, 100.0, 50.0, 90.0.degrees, 0.0, 0.0).addColorStop(0.0, Colors.GREEN).addColorStop(1.0, Colors.BLUE)) {
-        //fill(GradientPaint.fromGradientBox(GradientKind.LINEAR, 50.0, 100.0, 0.0.degrees, 0.0, 0.0).addColorStop(0.0, Colors.GREEN).addColorStop(1.0, Colors.BLUE)) {
-        //fill(GradientPaint.fromGradientBox(GradientKind.LINEAR, 100.0, 100.0, 45.0.degrees, 0.0, 0.0).addColorStop(0.0, Colors.GREEN).addColorStop(1.0, Colors.BLUE)) {
-        //fill(GradientPaint.fromGradientBox(GradientKind.LINEAR, 100.0, 100.0, 45.0.degrees, 0.0, 0.0).addColorStop(0.0, Colors.GREEN).addColorStop(1.0, Colors.BLUE)) {
-            rect(100.0, 100.0, 100.0, 100.0)
-        }
-    }
-     */
-
-    /*
-    val bmp = resourcesVfs["korge.png"].readBitmapOptimized().toBMP32().scaled(96, 96)
-    val shape = buildShape {
-        scale(2.0)
-        fill(BitmapPaint(bmp, Matrix().translate(64, 0))) {
-            fillRect(64.0, 0.0, 96.0, 96.0)
-        }
-    }
-    graphics {
-        shape(shape)
-    }
-
-     */
+    this += resourcesVfs["dog.swf"].readSWF(views, SWFExportConfig(rasterizerMethod = ShapeRasterizerMethod.X4), false).createMainTimeLine()
+    this += resourcesVfs["test1.swf"].readSWF(views, SWFExportConfig(rasterizerMethod = ShapeRasterizerMethod.X4), false).createMainTimeLine().position(400, 0)
 }
