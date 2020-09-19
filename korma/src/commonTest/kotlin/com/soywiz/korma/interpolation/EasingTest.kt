f3511c2c14e8c386ff70d999864084da8bd6f9d1
Max A <nanodeath@gmail.com>
2020-09-19 12:34:39 -0700

    Store all standard easings in an internal enum. (#31)
    Co-authored-by: Max Aller <max@maxaller.name>

--- korma/src/commonTest/kotlin/com/soywiz/korma/interpolation/EasingTest.kt
+++ korma/src/commonTest/kotlin/com/soywiz/korma/interpolation/EasingTest.kt
@@ -1,5 +1,6 @@
 package com.soywiz.korma.interpolation
 
+import kotlin.math.abs
 import kotlin.test.*
 
 class EasingTest {
@@ -25,5 +26,21 @@ class EasingTest {
     fun testSine() {
         assertEquals(0.0, Easing.EASE_SINE(0.0))
         assertEquals(1.0, Easing.EASE_SINE(1.0))
+    }
+
+    @Test
+    fun testStartsAtZero() {
+        Easing.ALL.values.forEach { easing ->
+            val v = easing(0.0)
+            assertTrue(abs(v) < 0.0001, "Easing $easing did not start at 0, was $v")
+        }
+    }
+
+    @Test
+    fun testEndsAtOne() {
+        Easing.ALL.values.forEach { easing ->
+            val v = easing(1.0)
+            assertTrue(abs(v - 1.0) < 0.0001, "Easing $easing did not end at 1, was $v")
+        }
     }
 }
