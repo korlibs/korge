33cf9479762183acadaa6d419f7732b131c92ea0
soywiz <soywiz@gmail.com>
2020-10-08 18:13:19 +0200

    Update krypto

--- krypto/src/jsMain/kotlin/com/soywiz/krypto/SecureRandomJs.kt
+++ krypto/src/jsMain/kotlin/com/soywiz/krypto/SecureRandomJs.kt
@@ -3,14 +3,14 @@ package com.soywiz.krypto
 import org.khronos.webgl.Int8Array
 import org.khronos.webgl.Uint8Array
 
-//private val isNodeJs by lazy { js("(typeof process === 'object' && typeof require === 'function')").unsafeCast<Boolean>() }
-//private external fun require(name: String): dynamic
-private val global: dynamic = js("(typeof global !== 'undefined') ? global : self")
+private val isNodeJs by lazy { js("(typeof process === 'object' && typeof require === 'function')").unsafeCast<Boolean>() }
+private external fun require(name: String): dynamic
+private val _global: dynamic = js("((typeof global !== 'undefined') ? global : self)")
 
 actual fun fillRandomBytes(array: ByteArray) {
-    //if (isNodeJs) {
-    //    require("crypto").randomFillSync(Uint8Array(array.unsafeCast<Int8Array>().buffer))
-    //} else {
-        global.crypto.getRandomValues(array)
-    //}
+    if (isNodeJs) {
+        require("crypto").randomFillSync(Uint8Array(array.unsafeCast<Int8Array>().buffer))
+    } else {
+        _global.crypto.getRandomValues(array)
+    }
 }
