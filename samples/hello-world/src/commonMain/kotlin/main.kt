ec8bc47b9dca73a82d56f2468275f8c6166fe57a
soywiz <soywiz@gmail.com>
2020-10-25 01:22:34 +0200

    Update hello-world sample showing how to use resources https://github.com/korlibs/korge/issues/283

--- samples/hello-world/src/commonMain/kotlin/main.kt
+++ samples/hello-world/src/commonMain/kotlin/main.kt
@@ -10,13 +10,17 @@ import com.soywiz.korio.resources.*
 import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
 
+// @TODO: We could autogenerate this via gradle
 val ResourcesContainer.korge_png by resourceBitmap("korge.png")
 
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+    gameWindow.icon = korge_png.get().bmp.toBMP32().scaled(32, 32)
+
     val minDegrees = (-16).degrees
     val maxDegrees = (+16).degrees
 
     val image = image(korge_png) {
+    //val image = image(resourcesVfs["korge.png"].readbitmapslice) {
         rotation = maxDegrees
         anchor(.5, .5)
         scale(.8)
