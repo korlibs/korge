b25e174419e63400def02841861177ab5ff266cc
soywiz <soywiz@gmail.com>
2020-10-25 01:29:08 +0200

    Some more work on resources https://github.com/korlibs/korge/issues/283

--- korge/src/commonMain/kotlin/com/soywiz/korge/resources/ResourcesExt.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/resources/ResourcesExt.kt
@@ -1,5 +1,6 @@
 package com.soywiz.korge.resources
 
+import com.soywiz.korim.bitmap.*
 import com.soywiz.korim.font.*
 import com.soywiz.korim.format.*
 import com.soywiz.korinject.*
@@ -7,7 +8,7 @@ import com.soywiz.korio.resources.*
 
 suspend fun resources(): Resources = injector().get()
 
-fun resourceBitmap(@ResourcePath path: String, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readBitmapSlice() }
-fun resourceFont(@ResourcePath path: String, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readFont() }
-fun resourceBitmapFont(@ResourcePath path: String, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readBitmapFont() }
-fun resourceTtfFont(@ResourcePath path: String, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readTtfFont() }
+fun resourceBitmap(@ResourcePath path: String, mipmaps: Boolean = true, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readBitmapSlice().also { it.bmp.mipmaps(enable = mipmaps) } }
+fun resourceFont(@ResourcePath path: String, mipmaps: Boolean = true, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readFont(mipmaps = mipmaps) }
+fun resourceBitmapFont(@ResourcePath path: String, mipmaps: Boolean = true, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readBitmapFont(mipmaps = mipmaps) }
+fun resourceTtfFont(@ResourcePath path: String, preload: Boolean = false, cache: ResourceCache = ResourceCache.LOCAL) = resource(cache) { root[path].readTtfFont(preload = preload) }
