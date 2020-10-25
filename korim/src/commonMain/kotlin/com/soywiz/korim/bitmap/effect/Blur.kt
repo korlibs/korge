8faa88c903edf2d35331ae80ba409f41878de470
soywiz <soywiz@gmail.com>
2020-10-25 22:41:24 +0100

    Prevent Blur from using UByteArray

--- korim/src/commonMain/kotlin/com/soywiz/korim/bitmap/effect/Blur.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/bitmap/effect/Blur.kt
@@ -32,7 +32,7 @@ fun Bitmap32.blurInplace(r: Int) {
         val channel = BitmapChannel[n]
         extractChannel(channel, t1)
         arraycopy(t1.data, 0, t2.data, 0, t2.area)
-        gaussBlur(t1.data.asUByteArray(), t2.data.asUByteArray(), width, height, r)
+        gaussBlur(t1.data, t2.data, width, height, r)
         writeChannel(channel, t2)
     }
 }
@@ -41,12 +41,12 @@ fun Bitmap8.blurInplace(r: Int) {
     val r = r.coerceAtMost(width / 2 - 1).coerceAtMost(height / 2 - 1)
     val t2 = Bitmap8(width, height)
     arraycopy(this.data, 0, t2.data, 0, this.area)
-    gaussBlur(t2.data.asUByteArray(), this.data.asUByteArray(), width, height, r)
+    gaussBlur(t2.data, this.data, width, height, r)
 }
 
 // http://blog.ivank.net/fastest-gaussian-blur.html
 
-private fun gaussBlur(scl: UByteArray, tcl: UByteArray, w: Int, h: Int, r: Int) {
+private fun gaussBlur(scl: ByteArray, tcl: ByteArray, w: Int, h: Int, r: Int) {
     var a = 0
     var b = 0
     var c = 0
@@ -60,63 +60,63 @@ private fun gaussBlur(scl: UByteArray, tcl: UByteArray, w: Int, h: Int, r: Int) 
     boxBlur(scl, tcl, w, h, (c - 1) / 2)
 }
 
-private fun boxBlur(scl: UByteArray, tcl: UByteArray, w: Int, h: Int, r: Int) {
-    arraycopy(scl.asByteArray(), 0, tcl.asByteArray(), 0, scl.size)
+private fun boxBlur(scl: ByteArray, tcl: ByteArray, w: Int, h: Int, r: Int) {
+    arraycopy(scl, 0, tcl, 0, scl.size)
     boxBlurH(tcl, scl, w, h, r)
     boxBlurT(scl, tcl, w, h, r)
 }
 
-private fun boxBlurH(scl: UByteArray, tcl: UByteArray, w: Int, h: Int, r: Int) {
+private fun boxBlurH(scl: ByteArray, tcl: ByteArray, w: Int, h: Int, r: Int) {
     val arr = (r + r + 1)
     for (i in 0 until h) {
         var ti = i * w
         var li = ti
         var ri = ti + r
-        val fv = scl[ti].toInt()
-        val lv = scl[ti + w - 1].toInt()
+        val fv = scl.readU8(ti)
+        val lv = scl.readU8(ti + w - 1)
         var v = (r + 1) * fv
-        for (j in 0 until r) v += scl[ti + j].toInt()
+        for (j in 0 until r) v += scl.readU8(ti + j)
         for (j in 0..r) {
-            v += scl[ri++].toInt() - fv
-            tcl[ti++] = (v / arr).toInt().toUByte()
+            v += scl.readU8(ri++) - fv
+            tcl[ti++] = (v / arr).toByte()
         }
         for (j in r + 1 until w - r) {
-            v += scl[ri++].toInt() - scl[li++].toInt()
-            tcl[ti++] = (v / arr).toInt().toUByte()
+            v += scl.readU8(ri++) - scl.readU8(li++)
+            tcl[ti++] = (v / arr).toByte()
         }
         for (j in w - r until w) {
-            v += lv - scl[li++].toInt()
-            tcl[ti++] = (v / arr).toInt().toUByte()
+            v += lv - scl.readU8(li++)
+            tcl[ti++] = (v / arr).toByte()
         }
     }
 }
 
-private fun boxBlurT(scl: UByteArray, tcl: UByteArray, w: Int, h: Int, r: Int) {
+private fun boxBlurT(scl: ByteArray, tcl: ByteArray, w: Int, h: Int, r: Int) {
     val arr = (r + r + 1)
     for (i in 0 until w) {
         var ti = i
         var li = ti
         var ri = ti + r * w
-        val fv = scl[ti].toInt()
-        val lv = scl[ti + w * (h - 1)].toInt()
+        val fv = scl.readU8(ti)
+        val lv = scl.readU8(ti + w * (h - 1))
         var v = (r + 1) * fv
-        for (j in 0 until r) v += scl[ti + j * w].toInt()
+        for (j in 0 until r) v += scl.readU8(ti + j * w)
         for (j in 0..r) {
-            v += scl[ri].toInt() - fv
-            tcl[ti] = (v / arr).toInt().toUByte()
+            v += scl.readU8(ri) - fv
+            tcl[ti] = (v / arr).toByte()
             ri += w
             ti += w
         }
         for (j in r + 1 until h - r) {
-            v += scl[ri].toInt() - scl[li].toInt()
-            tcl[ti] = (v / arr).toInt().toUByte()
+            v += scl.readU8(ri) - scl.readU8(li)
+            tcl[ti] = (v / arr).toByte()
             li += w
             ri += w
             ti += w
         }
         for (j in h - r until h) {
-            v += lv - scl[li].toInt()
-            tcl[ti] = (v / arr).toInt().toUByte()
+            v += lv - scl.readU8(li)
+            tcl[ti] = (v / arr).toByte()
             li += w
             ti += w
         }
