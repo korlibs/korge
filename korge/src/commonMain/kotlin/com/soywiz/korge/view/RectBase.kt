5e22faa4fe0167ae0cc0ac6463828df8c627eb8b
soywiz <soywiz@gmail.com>
2021-02-15 18:56:39 +0100

    Optimize Image and Sprite

--- korge/src/commonMain/kotlin/com/soywiz/korge/view/RectBase.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/RectBase.kt
@@ -29,7 +29,14 @@ open class RectBase(
 
     protected var anchorVersion = 0
 
-	protected var baseBitmap: BmpSlice = Bitmaps.white; set(v) { field = v; dirtyVertices = true }
+	protected var baseBitmap: BmpSlice = Bitmaps.white
+        set(v) {
+            if (field !== v) {
+                field = v
+                dirtyVertices = true
+            }
+        }
+
 	override var anchorX: Double = anchorX; set(v) { field = v; dirtyVertices = true; anchorVersion++ }
     override var anchorY: Double = anchorY; set(v) { field = v; dirtyVertices = true; anchorVersion++ }
 
