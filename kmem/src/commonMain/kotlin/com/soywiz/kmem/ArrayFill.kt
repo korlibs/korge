5e543eb94a0d31da65c6b953e6f3af46058296dd
soywiz <soywiz@gmail.com>
2021-02-24 22:01:00 +0100

    Use Array.fill from standard library

--- kmem/src/commonMain/kotlin/com/soywiz/kmem/ArrayFill.kt
+++ kmem/src/commonMain/kotlin/com/soywiz/kmem/ArrayFill.kt
@@ -1,35 +1,45 @@
 package com.soywiz.kmem
 
+import kotlin.collections.fill as stdFill
+
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun <T> arrayfill(array: Array<T>, value: T, start: Int = 0, end: Int = array.size): Unit
+fun <T> arrayfill(array: Array<T>, value: T, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: BooleanArray, value: Boolean, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: BooleanArray, value: Boolean, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: LongArray, value: Long, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: LongArray, value: Long, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: ByteArray, value: Byte, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: ByteArray, value: Byte, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: ShortArray, value: Short, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: ShortArray, value: Short, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: IntArray, value: Int, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: IntArray, value: Int, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: FloatArray, value: Float, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: FloatArray, value: Float, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 /** Fills the [array] with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-expect fun arrayfill(array: DoubleArray, value: Double, start: Int = 0, end: Int = array.size): Unit
+fun arrayfill(array: DoubleArray, value: Double, start: Int = 0, end: Int = array.size): Unit = array.stdFill(value, start, end)
 
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun <T> Array<T>.fill(value: T, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun <T> Array<T>.fill(value: T, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun BooleanArray.fill(value: Boolean, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun BooleanArray.fill(value: Boolean, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun LongArray.fill(value: Long, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun LongArray.fill(value: Long, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun ByteArray.fill(value: Byte, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun ByteArray.fill(value: Byte, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun ShortArray.fill(value: Short, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun ShortArray.fill(value: Short, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun IntArray.fill(value: Int, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun IntArray.fill(value: Int, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun FloatArray.fill(value: Float, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun FloatArray.fill(value: Float, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
 /** Fills [this] array with the [value] starting a [start] end ending at [end] (end is not inclusive) */
-inline fun DoubleArray.fill(value: Double, start: Int = 0, end: Int = this.size): Unit = arrayfill(this, value, start, end)
+@Deprecated("Use Array.fill from stdlib")
+inline fun DoubleArray.fill(value: Double, start: Int = 0, end: Int = this.size): Unit = this.stdFill(value, start, end)
