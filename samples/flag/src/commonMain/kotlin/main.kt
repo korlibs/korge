cf460ecdd097afaba38f9ddff18bd54ea6b23f18
MikeSmithEU <github@mikesmith.eu>
2021-03-03 19:43:13 +0100

    Make flag sample interactive (#184)

--- samples/flag/src/commonMain/kotlin/main.kt
+++ samples/flag/src/commonMain/kotlin/main.kt
@@ -1,5 +1,7 @@
 import com.soywiz.klock.*
+import com.soywiz.korev.Key
 import com.soywiz.korge.*
+import com.soywiz.korge.input.keys
 import com.soywiz.korge.view.*
 import com.soywiz.korge.view.filter.*
 import com.soywiz.korim.color.*
@@ -27,5 +29,22 @@ suspend fun main() = Korge(width = 592, height = 592, bgcolor = Colors["#2b2b2b"
     // Propagates the wave over time
     addUpdater { dt: TimeSpan ->
         flagFilter.time = flagFilter.time.plus(dt)
+    }
+
+    fun min(a: Double, b: Double) = if (a > b) b else a
+    fun max(a: Double, b: Double) = if (a > b) a else b
+
+    keys {
+        down {
+            when (it.key) {
+                Key.LEFT -> flagFilter.amplitude = max(0.0, flagFilter.amplitude - 5)
+                Key.RIGHT -> flagFilter.amplitude = min(100.0, flagFilter.amplitude + 5)
+                Key.DOWN -> flagFilter.crestCount = max(0.0, flagFilter.crestCount - 0.5)
+                Key.UP -> flagFilter.crestCount = min(10.0, flagFilter.crestCount + 0.5)
+                Key.PLUS, Key.RIGHT_BRACKET, Key.CLOSE_BRACKET -> flagFilter.cyclesPerSecond = min(10.0, flagFilter.cyclesPerSecond + 0.5)
+                Key.MINUS, Key.LEFT_BRACKET, Key.OPEN_BRACKET -> flagFilter.cyclesPerSecond = max(0.0, flagFilter.cyclesPerSecond - 0.5)
+            }
+            println("amplitude = ${flagFilter.amplitude}, crestCount = ${flagFilter.crestCount}, cyclesPerSecond = ${flagFilter.cyclesPerSecond}")
+        }
     }
 }
