5fad4cd0f94058c740778347d89e09d639c51387
SerVB <servbul@yandex.ru>
2021-04-23 20:53:38 +0300

    Remove tests relying on external resources

--- korio/src/commonTest/kotlin/com/soywiz/korio/net/http/HttpHeadersTest.kt
+++ korio/src/commonTest/kotlin/com/soywiz/korio/net/http/HttpHeadersTest.kt
@@ -1,6 +1,5 @@
 package com.soywiz.korio.net.http
 
-import com.soywiz.korio.async.suspendTest
 import kotlin.test.*
 
 class HttpHeadersTest {
@@ -8,20 +7,5 @@ class HttpHeadersTest {
     fun test() {
         assertEquals(HttpClient.DEFAULT_USER_AGENT, HttpClient.combineHeadersForHost(Http.Headers(), null)["User-agent"])
         assertEquals("Hello", HttpClient.combineHeadersForHost(Http.Headers("user-agent" to "Hello"), null)["user-agent"])
-    }
-    @Test
-    fun testIgnoredWhitespaceInHeadersValue() = suspendTest {
-        // this test checks "Whitespace before the value is ignored" rule
-        // from https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
-
-        val fullUrl = "https://example.com"
-        val client = HttpPortable.createClient()
-
-        val result = client.request(Http.Method.HEAD, fullUrl)
-        assertTrue(result.headers.items.isNotEmpty(), "headers of a valid request should be non-empty")
-        assertTrue(
-            result.headers.items.all { (_, v) -> v.trimStart() == v },
-            "Whitespace before the value isn't ignored: ${result.headers}"
-        )
     }
 }
