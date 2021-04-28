ed2ba7f72803f455c4f81a004bf237447a36a855
jobe <marko.koschak@tisno.de>
2021-04-28 21:24:14 +0200

    Add Layer, FrameTag and Slice config to AtlasInfo (#217)
    * Add Layer, FrameTag and Slice config to AtlasInfo
    
    In Aseprite it is possible to save in the sprite atlas config file
    also information about layers, frame tags and slices. This commit
    adds support for reading this additional information also into the
    AtlasInfo object. If this information is not present in a json
    file then the three lists "layers", "frameTags" and "slices" will
    be empty.
    
    * Add AtlasInfoTest file with test resources
    
    AtlasInfoTest is testing if the atlas file which was exported from
    Aseprite is correctly loaded. It tests for layer, frame tags and slices
    information in the meta section of the atlas info object.
    
    * Fix atlas unit test
    
    Move atlas into test resources to jvmTest and force running the test on
    jvm platform. This should fix the unit test run.

--- korim/src/commonMain/kotlin/com/soywiz/korim/atlas/AtlasInfo.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/atlas/AtlasInfo.kt
@@ -18,34 +18,65 @@ data class AtlasInfo(
     constructor(
         frames: List<Region>,
         meta: Meta,
-    ) : this(meta, listOf(Page(
-        meta.image,
-        meta.size,
-        meta.format,
-        true, true,
-        false, false, frames
-    )))
+    ) : this(
+        meta, listOf(
+            Page(
+                meta.image,
+                meta.size,
+                meta.format,
+                true, true,
+                false, false, frames
+            )
+        )
+    )
 
-	data class Rect(val x: Int, val y: Int, val w: Int, val h: Int) {
+    data class Rect(val x: Int, val y: Int, val w: Int, val h: Int) {
         val rect get() = Rectangle(x, y, w, h)
-	}
+    }
 
-	data class Size(val width: Int, val height: Int) {
-		val size get() = com.soywiz.korma.geom.Size(width, height)
-	}
+    data class Size(val width: Int, val height: Int) {
+        val size get() = com.soywiz.korma.geom.Size(width, height)
+    }
 
-	data class Meta(
+    data class Meta(
         val app: String = "app",
         val format: String = "format",
         val image: String = "image",
         val scale: Double = 1.0,
         val size: Size = Size(1, 1),
-        val version: String = VERSION
-	) {
-		companion object {
-			val VERSION = "1.0.0"
-		}
-	}
+        val version: String = VERSION,
+        val frameTags: List<FrameTag> = listOf(),
+        val layers: List<Layer> = listOf(),
+        val slices: List<Slice> = listOf()
+    ) {
+        companion object {
+            val VERSION = "1.0.0"
+        }
+    }
+
+    data class FrameTag(
+        val name: String = "",
+        val from: Int = 0,
+        val to: Int = 0,
+        val direction: String = ""
+    )
+
+    data class Layer(
+        val name: String = "",
+        val opacity: Int = 255,
+        val blendMode: String = ""
+    )
+
+    data class Slice(
+        val name: String = "slice",
+        val color: String = "#0000ffff",
+        val keys: List<Key>
+    )
+
+    data class Key(
+        val frame: Int = 0,
+        val bounds: Rect
+    )
 
     data class Page(
         val fileName: String,
@@ -58,7 +89,7 @@ data class AtlasInfo(
         val regions: List<Region>
     )
 
-	data class Region(
+    data class Region(
         val name: String,
         val frame: Rect,
         val rotated: Boolean,
@@ -67,35 +98,35 @@ data class AtlasInfo(
         val trimmed: Boolean,
         val orig: Size = Size(0, 0),
         val offset: Point = Point(),
-	) {
-        // @TODO: Renemae to path or name
+    ) {
+        // @TODO: Rename to path or name
         val filename get() = name
 
-		fun applyRotation() = if (rotated) {
-			this.copy(
-				frame = frame.copy(w = frame.h, h = frame.w),
-				spriteSourceSize = spriteSourceSize.copy(
-					x = spriteSourceSize.y,
-					y = spriteSourceSize.x,
-					w = spriteSourceSize.h,
-					h = spriteSourceSize.w
-				)
-			)
-		} else {
-			this
-		}
-	}
+        fun applyRotation() = if (rotated) {
+            this.copy(
+                frame = frame.copy(w = frame.h, h = frame.w),
+                spriteSourceSize = spriteSourceSize.copy(
+                    x = spriteSourceSize.y,
+                    y = spriteSourceSize.x,
+                    w = spriteSourceSize.h,
+                    h = spriteSourceSize.w
+                )
+            )
+        } else {
+            this
+        }
+    }
 
-	val app: String get() = meta.app
-	val format: String get() = meta.format
-	val image: String get() = meta.image
-	val scale: Double get() = meta.scale
-	val size: Size get() = meta.size
-	val version: String get() = meta.version
+    val app: String get() = meta.app
+    val format: String get() = meta.format
+    val image: String get() = meta.image
+    val scale: Double get() = meta.scale
+    val size: Size get() = meta.size
+    val version: String get() = meta.version
 
-	companion object {
-		private fun Any?.toRect() = KDynamic(this) { Rect(it["x"].int, it["y"].int, it["w"].int, it["h"].int) }
-		private fun Any?.toSize() = KDynamic(this) { Size(it["w"].int, it["h"].int) }
+    companion object {
+        private fun Any?.toRect() = KDynamic(this) { Rect(it["x"].int, it["y"].int, it["w"].int, it["h"].int) }
+        private fun Any?.toSize() = KDynamic(this) { Size(it["w"].int, it["h"].int) }
         private fun KDynamic.createEntry(name: String, it: Any?) = Region(
             name = name,
             frame = it["frame"].toRect(),
@@ -105,55 +136,106 @@ data class AtlasInfo(
             trimmed = it["trimmed"].bool
         )
 
-		// @TODO: kotlinx-serialization?
-		fun loadJsonSpriter(json: String): AtlasInfo {
-			val info = KDynamic(Json.parse(json)) {
-				AtlasInfo(
-					it["frames"].let { frames ->
+        // @TODO: kotlinx-serialization?
+        fun loadJsonSpriter(json: String): AtlasInfo {
+            val info = KDynamic(Json.parse(json)) {
+                AtlasInfo(
+                    frames = it["frames"].let { frames ->
                         when (frames) {
                             // Hash-based
                             is Map<*, *> -> frames.keys.map { createEntry(it.str, frames[it.str]) }
                             // Array-based
-                            else -> frames.list.map { createEntry(it["name"]?.str ?: it["filename"]?.str ?: "unknown", it) }
+                            else -> frames.list.map {
+                                createEntry(
+                                    it["name"]?.str ?: it["filename"]?.str ?: "unknown",
+                                    it
+                                )
+                            }
                         }
-					},
-					it["meta"].let {
-						Meta(
-							app = it["app"].str,
-							format = it["format"].str,
-							image = it["image"].str,
-							scale = it["scale"].double,
-							size = it["size"].toSize(),
-							version = it["version"].str
-						)
-					}
-				)
-			}
-			return info.copy(pages = info.pages.map { it.copy(regions = it.regions.map { it.apply {  } }) })
-		}
+                    },
+                    meta = it["meta"].let {
+                        Meta(
+                            app = it["app"].str,
+                            format = it["format"].str,
+                            image = it["image"].str,
+                            scale = it["scale"].double,
+                            size = it["size"].toSize(),
+                            version = it["version"].str,
+                            frameTags = it["frameTags"].let { frameTags ->
+                                if (frameTags is List<*>) frameTags.list.map {
+                                    FrameTag(
+                                        name = it["name"].str,
+                                        from = it["from"].int,
+                                        to = it["to"].int,
+                                        direction = it["direction"].str
+                                    )
+                                }
+                                else listOf()
+                            },
+                            layers = it["layers"].let { layers ->
+                                if (layers is List<*>) layers.list.map {
+                                    Layer(
+                                        name = it["name"].str,
+                                        opacity = it["opacity"].int,
+                                        blendMode = it["blendMode"].str
+                                    )
+                                }
+                                else listOf()
+                            },
+                            slices = it["slices"].let { slices ->
+                                if (slices is List<*>) slices.list.map {
+                                    Slice(
+                                        name = it["name"].str,
+                                        color = it["color"].str,
+                                        keys = it["keys"].let { keys ->
+                                            if (keys is List<*>) keys.list.map {
+                                                Key(
+                                                    frame = it["frame"].int,
+                                                    bounds = it["bounds"].toRect()
+                                                )
+                                            }
+                                            else listOf()
+                                        }
+                                    )
+                                }
+                                else listOf()
+                            }
+                        )
+                    }
+                )
+            }
+            return info.copy(pages = info.pages.map { it.copy(regions = it.regions.map { it.apply { } }) })
+        }
 
         fun loadXml(content: String): AtlasInfo {
             val xml = Xml(content)
             val imagePath = xml.str("imagePath")
 
-            return AtlasInfo( (xml.children("SubTexture") + xml.children("sprite")).map {
-                val rect = Rect(it.int("x"), it.int("y"), it.intNull("width") ?: it.int("w"), it.intNull("height") ?: it.int("h"))
-                Region(
-                    name = it.strNull("name") ?: it.str("n"),
-                    frame = rect,
-                    rotated = false,
-                    sourceSize = Size(rect.w, rect.h),
-                    spriteSourceSize = rect,
-                    trimmed = false
+            return AtlasInfo(
+                (xml.children("SubTexture") + xml.children("sprite")).map {
+                    val rect = Rect(
+                        it.int("x"),
+                        it.int("y"),
+                        it.intNull("width") ?: it.int("w"),
+                        it.intNull("height") ?: it.int("h")
+                    )
+                    Region(
+                        name = it.strNull("name") ?: it.str("n"),
+                        frame = rect,
+                        rotated = false,
+                        sourceSize = Size(rect.w, rect.h),
+                        spriteSourceSize = rect,
+                        trimmed = false
+                    )
+                }, Meta(
+                    app = "Unknown",
+                    format = "xml",
+                    image = imagePath,
+                    scale = 1.0,
+                    size = Size(-1, -1),
+                    version = "1.0"
                 )
-            }, Meta(
-                app = "Unknown",
-                format = "xml",
-                image = imagePath,
-                scale = 1.0,
-                size = Size(-1, -1),
-                version = "1.0"
-            ))
+            )
         }
 
         fun loadText(content: String): AtlasInfo {
@@ -164,6 +246,7 @@ data class AtlasInfo(
                 val list = this.split(',', limit = 2)
                 return Point(list.first().trim().toInt(), list.last().trim().toInt())
             }
+
             fun String.size(): Size = point().let { Size(it.x.toInt(), it.y.toInt()) }
 
             fun String.keyValue(): Pair<String, String> {
---
+++ korim/src/commonTest/kotlin/com/soywiz/korim/atlas/AtlasInfoTest.kt
@@ -0,0 +1,24 @@
+package com.soywiz.korim.atlas
+
+import com.soywiz.korio.async.suspendTest
+import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korio.util.OS
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AtlasInfoTest {
+    @Test
+    /* This test is reading a sprite atlas which was created by Aseprite version 1.2.24
+     */
+    fun name() = suspendTest({ OS.isJvm }) {
+        val atlas = resourcesVfs["atlas_info_aseprite_test.json"].readAtlas()
+        // Check for layer info
+        assertEquals("layer3d", atlas.info.meta.layers[0].name)
+        assertEquals("layer_9", atlas.info.meta.layers[10].name)
+        // Check for slices info
+        assertEquals("Slice 1", atlas.info.meta.slices[0].name)
+        assertEquals("Slice 334", atlas.info.meta.slices[333].name)
+        // Check for frame tags info
+        assertEquals("default", atlas.info.meta.frameTags[0].name)
+    }
+}
