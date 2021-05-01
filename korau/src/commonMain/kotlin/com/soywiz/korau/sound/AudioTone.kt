1345e7af9b365800b0e0f3a5bfc2674d668191f5
soywiz <soywiz@gmail.com>
2021-05-01 18:27:50 +0200

    Optimized AudioTone

--- korau/src/commonMain/kotlin/com/soywiz/korau/sound/AudioTone.kt
+++ korau/src/commonMain/kotlin/com/soywiz/korau/sound/AudioTone.kt
@@ -8,10 +8,11 @@ object AudioTone {
     fun generate(length: TimeSpan, freq: Double, rate: Int = 44100): AudioData {
         val nsamples = (rate * length.seconds).toInt()
         val samples = AudioSamples(1, nsamples)
+        val samples0 = samples[0]
         for (n in 0 until nsamples) {
             val ratio = (n.toDouble() * freq) / rate
             val sample = sin(ratio * PI * 2)
-            samples[0, n] = SampleConvert.floatToShort(sample.toFloat())
+            samples0[n] = SampleConvert.floatToShort(sample.toFloat())
         }
         return AudioData(rate, samples)
     }
