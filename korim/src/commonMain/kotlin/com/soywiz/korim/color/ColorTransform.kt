dd72edc3ae9bff59d32cbfcdfde890b387e59e39
soywiz <soywiz@gmail.com>
2021-05-05 16:57:57 +0200

    [K/N] Optimize ColorTransform and ColorAdd classes to not have non-inlined companion object members

--- korim/src/commonMain/kotlin/com/soywiz/korim/color/ColorTransform.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/color/ColorTransform.kt
@@ -16,10 +16,11 @@ data class ColorTransform(
     private var _aA: Int
 ) : MutableInterpolable<ColorTransform>, Interpolable<ColorTransform> {
     companion object {
-        val identity = ColorTransform()
+        @Deprecated("This being mutable is dangeour", level = DeprecationLevel.ERROR)
+        inline val identity: ColorTransform get() = TODO()
 
-        fun Multiply(r: Double, g: Double, b: Double, a: Double) = ColorTransform(r, g, b, a, 0, 0, 0, 0)
-        fun Add(r: Int, g: Int, b: Int, a: Int) = ColorTransform(1, 1, 1, 1, r, g, b, a)
+        inline fun Multiply(r: Double, g: Double, b: Double, a: Double) = ColorTransform(r, g, b, a, 0, 0, 0, 0)
+        inline fun Add(r: Int, g: Int, b: Int, a: Int) = ColorTransform(1, 1, 1, 1, r, g, b, a)
     }
 
     override fun setToInterpolated(ratio: Double, l: ColorTransform, r: ColorTransform): ColorTransform = setTo(
@@ -86,20 +87,20 @@ data class ColorTransform(
             }
         }
 
-    var mR: Double get() = _mR; set(v) = run { _mR = v; dirty = true }
-    var mG: Double get() = _mG; set(v) = run { _mG = v; dirty = true }
-    var mB: Double get() = _mB; set(v) = run { _mB = v; dirty = true }
-    var mA: Double get() = _mA; set(v) = run { _mA = v; dirty = true }
+    var mR: Double get() = _mR; set(v) { _mR = v; dirty = true }
+    var mG: Double get() = _mG; set(v) { _mG = v; dirty = true }
+    var mB: Double get() = _mB; set(v) { _mB = v; dirty = true }
+    var mA: Double get() = _mA; set(v) { _mA = v; dirty = true }
 
-    var mRf: Float get() = _mR.toFloat(); set(v) = run { _mR = v.toDouble(); dirty = true }
-    var mGf: Float get() = _mG.toFloat(); set(v) = run { _mG = v.toDouble(); dirty = true }
-    var mBf: Float get() = _mB.toFloat(); set(v) = run { _mB = v.toDouble(); dirty = true }
-    var mAf: Float get() = _mA.toFloat(); set(v) = run { _mA = v.toDouble(); dirty = true }
+    var mRf: Float get() = _mR.toFloat(); set(v) { _mR = v.toDouble(); dirty = true }
+    var mGf: Float get() = _mG.toFloat(); set(v) { _mG = v.toDouble(); dirty = true }
+    var mBf: Float get() = _mB.toFloat(); set(v) { _mB = v.toDouble(); dirty = true }
+    var mAf: Float get() = _mA.toFloat(); set(v) { _mA = v.toDouble(); dirty = true }
 
-    var aR: Int get() = _aR; set(v) = run { _aR = v; dirty = true }
-    var aG: Int get() = _aG; set(v) = run { _aG = v; dirty = true }
-    var aB: Int get() = _aB; set(v) = run { _aB = v; dirty = true }
-    var aA: Int get() = _aA; set(v) = run { _aA = v; dirty = true }
+    var aR: Int get() = _aR; set(v) { _aR = v; dirty = true }
+    var aG: Int get() = _aG; set(v) { _aG = v; dirty = true }
+    var aB: Int get() = _aB; set(v) { _aB = v; dirty = true }
+    var aA: Int get() = _aA; set(v) { _aA = v; dirty = true }
 
     var alphaMultiplier: Double
         get() = mA
@@ -261,10 +262,10 @@ inline class ColorAdd(val value: Int) {
     // Alias
     val rgba get() = value
 
-    val r get() = unpackComponent((value ushr 0) and 0xFF)
-    val g get() = unpackComponent((value ushr 8) and 0xFF)
-    val b get() = unpackComponent((value ushr 16) and 0xFF)
-    val a get() = unpackComponent((value ushr 24) and 0xFF)
+    val r get() = ColorAdd_unpackComponent((value ushr 0) and 0xFF)
+    val g get() = ColorAdd_unpackComponent((value ushr 8) and 0xFF)
+    val b get() = ColorAdd_unpackComponent((value ushr 16) and 0xFF)
+    val a get() = ColorAdd_unpackComponent((value ushr 24) and 0xFF)
 
     fun withR(r: Int) = ColorAdd(r, g, b, a)
     fun withG(g: Int) = ColorAdd(r, g, b, a)
@@ -276,19 +277,15 @@ inline class ColorAdd(val value: Int) {
     val shex get() = value.shex
 
     companion object {
-        val NEUTRAL = ColorAdd(0x7f7f7f7f)
-
-        operator fun invoke(r: Int, g: Int, b: Int, a: Int) = ColorAdd(pack(r, g, b, a))
-
-        fun packComponent(v: Int) = (0x7f + (v shr 1)).clamp(0, 0xFF)
-        fun unpackComponent(v: Int): Int = (v - 0x7F) * 2
-        fun pack(r: Int, g: Int, b: Int, a: Int) =
-            (ColorAdd.packComponent(r) shl 0) or
-                (ColorAdd.packComponent(g) shl 8) or
-                (ColorAdd.packComponent(b) shl 16) or
-                (ColorAdd.packComponent(a) shl 24)
+        inline val NEUTRAL get() = ColorAdd_NEUTRAL
+        inline operator fun invoke(r: Int, g: Int, b: Int, a: Int) = ColorAdd(ColorAdd_pack(r, g, b, a))
     }
 }
+
+@PublishedApi internal val ColorAdd_NEUTRAL = ColorAdd(0x7f7f7f7f)
+@PublishedApi internal fun ColorAdd_packComponent(v: Int) = (0x7f + (v shr 1)).clamp(0, 0xFF)
+@PublishedApi internal fun ColorAdd_unpackComponent(v: Int): Int = (v - 0x7F) * 2
+@PublishedApi internal fun ColorAdd_pack(r: Int, g: Int, b: Int, a: Int) = (ColorAdd_packComponent(r) shl 0) or (ColorAdd_packComponent(g) shl 8) or (ColorAdd_packComponent(b) shl 16) or (ColorAdd_packComponent(a) shl 24)
 
 fun RGBA.toColorAdd() = ColorAdd(r, g, b, a)
 
