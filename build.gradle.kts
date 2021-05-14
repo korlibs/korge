e6bd41d7e5f28d84dc91c54569987a0e83c774ea
Carlos Ballesteros Velasco <soywiz@gmail.com>
2021-05-15 00:58:37 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,11 @@ val buildExtraGradleFile = File(rootDir, "build.extra.gradle.kts")
 if (buildExtraGradleFile.exists()) {
     apply(from = buildExtraGradleFile)
 }
+
+allprojects {
+    tasks.withType(Copy::class.java).all {
+        //this.duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.WARN
+        this.duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.EXCLUDE
+        //println("Task $this")
+    }
+}
