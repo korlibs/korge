e9b17fa56187d2096d7e09f0dbcadc49fedc760e
soywiz <soywiz@gmail.com>
2021-06-19 14:14:48 +0200

    Circumvents stack overflow on Kotlin-JS IR Compiler

--- kds/src/commonMain/kotlin/com/soywiz/kds/comparator/ComparatorExt.kt
+++ kds/src/commonMain/kotlin/com/soywiz/kds/comparator/ComparatorExt.kt
@@ -8,5 +8,5 @@ object ReverseComparatorComparable : Comparator<Comparable<Any>> {
     override fun compare(a: Comparable<Any>, b: Comparable<Any>): Int = -a.compareTo(b)
 }
 
-inline fun <T : Comparable<T>> ComparatorComparable() = ComparatorComparable as Comparator<T>
-inline fun <T : Comparable<T>> ReverseComparatorComparable() = ReverseComparatorComparable as Comparator<T>
+fun <T : Comparable<T>> ComparatorComparable() = ComparatorComparable as Comparator<T>
+fun <T : Comparable<T>> ReverseComparatorComparable() = ReverseComparatorComparable as Comparator<T>
