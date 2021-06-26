70e0dea62da8abcce644a42ecb36907798ea0c5d
soywiz <soywiz@gmail.com>
2021-06-26 16:53:34 +0200

    Try to fix BonePositionTest on linux

--- korge-editor/src/jvmMain/kotlin/com/soywiz/korge/editor/KorgeEditor.kt
+++ korge-editor/src/jvmMain/kotlin/com/soywiz/korge/editor/KorgeEditor.kt
@@ -1,24 +1,39 @@
+import com.soywiz.kmem.*
 import com.soywiz.korge.*
-import com.soywiz.korge.annotations.*
+import com.soywiz.korge.render.*
 import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.*
-import com.soywiz.korge.view.Container
-import com.soywiz.korim.color.*
 import com.soywiz.korim.font.*
 import com.soywiz.korma.geom.*
-import java.awt.*
+import com.soywiz.korma.geom.Rectangle
 
 suspend fun main() = Korge(scaleMode = ScaleMode.NO_SCALE, scaleAnchor = Anchor.TOP_LEFT, clipBorders = false) {
-    //val component = injector.get<ViewsDebuggerComponent>()
-    //ktreeEditorKorge(stage, component.actions, views, BaseKorgeFileToEdit(MemoryVfsMix(mapOf("test.ktree" to "<ktree></ktree>"))["test.ktree"]), { })
-
     val font = DefaultTtfFont.toBitmapFont(16.0)
-
-    val solidRect = solidRect(100, 100, Colors.RED).position(300, 300).anchor(Anchor.CENTER)
-
     uiSkin = UISkin {
         this.textFont = font
     }
+
+    /*
+    uiBreadCrumbArray("hello", "world") {
+        onClickPath {
+            println(it)
+            gameWindow.showContextMenu {
+                item("hello", action = {})
+                separator()
+                item("world", action = {})
+            }
+        }
+    }
+
+    //val component = injector.get<ViewsDebuggerComponent>()
+    //ktreeEditorKorge(stage, component.actions, views, BaseKorgeFileToEdit(MemoryVfsMix(mapOf("test.ktree" to "<ktree></ktree>"))["test.ktree"]), { })
+
+    val solidRect = solidRect(100, 100, Colors.RED).position(300, 300).anchor(Anchor.CENTER)
+    //val grid = OrthographicGrid(20, 20)
+    //renderableView() { grid.draw(ctx, 500.0, 500.0, globalMatrix) }
+
+
+
     uiWindow("Properties", 300.0, 100.0) {
         it.isCloseable = false
         it.container.mobileBehaviour = false
@@ -30,7 +45,7 @@ suspend fun main() = Korge(scaleMode = ScaleMode.NO_SCALE, scaleAnchor = Anchor.
             uiPropertyNumberRow("Size", *UIEditableNumberPropsList(solidRect::width, solidRect::height, min = -1024.0, max = +1024.0, clamped = false))
             uiPropertyNumberRow("Scale", *UIEditableNumberPropsList(solidRect::scaleX, solidRect::scaleY, min = -1.0, max = +1.0, clamped = false))
             uiPropertyNumberRow("Rotation", *UIEditableNumberPropsList(solidRect::rotationDeg, min = -360.0, max = +360.0, clamped = true))
-            uiPropertyNumberRow("Skew", *UIEditableNumberPropsList(solidRect::skewXDeg, solidRect::skewYDeg, min = -360.0, max = +360.0, clamped = true))
+            val skewProp = uiPropertyNumberRow("Skew", *UIEditableNumberPropsList(solidRect::skewXDeg, solidRect::skewYDeg, min = -360.0, max = +360.0, clamped = true))
             append(UIPropertyRow("Visible")) {
                 this.container.append(uiCheckBox(checked = solidRect.visible, text = "").also {
                     it.onChange {
@@ -38,7 +53,20 @@ suspend fun main() = Korge(scaleMode = ScaleMode.NO_SCALE, scaleAnchor = Anchor.
                     }
                 })
             }
+
+            println(skewProp.getVisibleGlobalArea())
+
         }
+    }
+    */
+
+    uiScrollable {
+        uiVerticalList(object : UIVerticalList.Provider {
+            override val numItems: Int = 1000
+            override val fixedHeight: Double = 20.0
+            override fun getItemHeight(index: Int): Double = fixedHeight
+            override fun getItemView(index: Int): View = UIText("HELLO WORLD $index")
+        })
     }
 }
 
