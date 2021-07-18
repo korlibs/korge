501858aba102f59b1b4028ff6a79f03a37d26110
soywiz <soywiz@gmail.com>
2021-07-18 18:36:48 +0200

    Try to reproduce https://github.com/korlibs/korge-next/issues/313

--- korio/src/commonTest/kotlin/com/soywiz/korio/file/std/UrlVfsTest.kt
+++ korio/src/commonTest/kotlin/com/soywiz/korio/file/std/UrlVfsTest.kt
@@ -1,10 +1,23 @@
 package com.soywiz.korio.file.std
 
+import com.soywiz.korio.async.*
+import com.soywiz.korio.net.http.*
 import kotlin.test.*
 
 class UrlVfsTest {
     @Test
     fun test() {
         assertEquals("http://127.0.0.1:8080/", UrlVfs("http://127.0.0.1:8080")["."].absolutePath)
+    }
+
+    @Test
+    fun testDownloadFileUsingUrlVfsWithoutReceivingContentLength() = suspendTest {
+        val client = FakeHttpClient().apply {
+            onRequest().ok("test")
+        }
+        val vfs = UrlVfs("http://demo.test", client = client)
+        assertEquals(false, vfs["test"].open().hasLength())
+        assertFailsWith<UnsupportedOperationException> { vfs["test"].open().getLength() }
+        assertEquals("test", vfs["test"].readString())
     }
 }
