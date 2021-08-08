b2c92fb430eebb8a823611bd64771dff5c1ccbd9
soywiz <soywiz@gmail.com>
2021-08-08 09:22:17 +0200

    Try to fix https://github.com/korlibs/korge/issues/389

--- korgw/src/jvmMain/kotlin/com/soywiz/korgw/awt/AwtGameWindow.kt
+++ korgw/src/jvmMain/kotlin/com/soywiz/korgw/awt/AwtGameWindow.kt
@@ -191,9 +191,18 @@ class AwtGameWindow(checkGl: Boolean, logGl: Boolean) : BaseAwtGameWindow() {
                         //println("TOGGLING!")
                         if (fullscreen != value) {
                             queue {
-                                MicroDynamic {
-                                    //println("INVOKE!: ${getClass("com.apple.eawt.Application").invoke("getApplication")}")
-                                    getClass("com.apple.eawt.Application").invoke("getApplication").invoke("requestToggleFullScreen", frame)
+                                try {
+                                    MicroDynamic {
+                                        //println("INVOKE!: ${getClass("com.apple.eawt.Application").invoke("getApplication")}")
+                                        getClass("com.apple.eawt.Application").invoke("getApplication")
+                                            .invoke("requestToggleFullScreen", frame)
+                                    }
+                                } catch (e: Throwable) {
+                                    if (e::class.qualifiedName != "java.lang.reflect.InaccessibleObjectException") {
+                                        e.printStackTrace()
+                                    }
+                                    GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.fullScreenWindow = if (value) frame else null
+                                    frame.isVisible = true
                                 }
                             }
                         }
