aebd1c72bad5312f5340925cba524c8b2fae5177
soywiz <soywiz@gmail.com>
2021-10-02 14:07:52 +0200

    Korte: Join JVM and Android implementations

--- korte/src/androidMain/kotlin/com/soywiz/korte/internal/KorteLock.kt
+++
@@ -1,5 +0,0 @@
-package com.soywiz.korte.internal
-
-internal actual class KorteLock actual constructor() {
-	actual inline operator fun <T> invoke(callback: () -> T): T = synchronized(this) { callback() }
-}
