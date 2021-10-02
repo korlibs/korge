5a2397bc6e01e8d251c92cd40cb5ed80bd62a710
soywiz <soywiz@gmail.com>
2021-10-02 13:03:13 +0200

    Update DummAG to mark instanced rendering support + update reference test for particles

--- korgw/src/commonMain/kotlin/com/soywiz/korag/log/DummyAG.kt
+++ korgw/src/commonMain/kotlin/com/soywiz/korag/log/DummyAG.kt
@@ -6,4 +6,8 @@ open class DummyAG(width: Int = 640, height: Int = 480) : AG() {
     override val nativeComponent: Any = Any()
     override var backWidth: Int = width
     override var backHeight: Int = height
+
+    override val graphicExtensions: Set<String> get() = emptySet()
+    override val isInstancedSupported: Boolean get() = true
+    override val isFloatTextureSupported: Boolean get() = true
 }
