43c6e678be974682706b2864e019742c29a9cb97
soywiz <soywiz@gmail.com>
2021-10-15 00:04:02 +0200

    Be more lax with windows reference testing

--- e2e-test/src/jvmMain/kotlin/CheckReferences.kt
+++ e2e-test/src/jvmMain/kotlin/CheckReferences.kt
@@ -16,17 +16,26 @@ object CheckReferences {
             val generatedVfs = localCurrentDirVfs["build/screenshots/$kind"]
             val exists = generatedVfs.exists()
             println("generatedVfs=$generatedVfs . exists=${exists}")
+
+            data class Result(val similar: Boolean, val equals: Boolean, val psnr: Double)
+
             if (exists) {
-                references.list().filter { it.extensionLC == "png" }.collect { file ->
+                references.list().filter { it.extensionLC == "png" && !it.baseName.endsWith(".alt.png") }.collect { file ->
+                    val files = listOf(file, file.withExtension("alt.png"))
                     val otherFile = generatedVfs[file.baseName]
-                    println("Comparing ${file.absolutePath} <-> ${otherFile.absolutePath}")
-                    val bitmap1 = file.readBitmap().toBMP32()
+                    //println(otherFilesUnfiltered)
+                    println("Comparing ${otherFile.absolutePath} <-> ${files.map { it.absolutePath }}")
+                    val bitmap1List = files.map { it.readBitmap().toBMP32() }
                     val bitmap2 = otherFile.readBitmap().toBMP32()
-                    val similar = Bitmap32.matches(bitmap1, bitmap2, threshold = 32)
-                    val equals = bitmap1.contentEquals(bitmap2)
-                    val psnr = Bitmap32.computePsnr(bitmap1, bitmap2)
-                    println(" --> equals=$equals, similar=$similar, psnr=$psnr")
-                    if (!similar) notSimilarCount++
+
+                    val results = bitmap1List.map { bitmap1 ->
+                        val similar = Bitmap32.matches(bitmap1, bitmap2, threshold = 32)
+                        val equals = bitmap1.contentEquals(bitmap2)
+                        val psnr = Bitmap32.computePsnr(bitmap1, bitmap2)
+                        Result(similar, equals, psnr)
+                    }
+                    println(" --> equals=${results.map { it.equals }}, similar=${results.map { it.similar }}, psnr=${results.map { it.psnr }}")
+                    if (!results.any { it.similar }) notSimilarCount++
                 }
             }
         }
