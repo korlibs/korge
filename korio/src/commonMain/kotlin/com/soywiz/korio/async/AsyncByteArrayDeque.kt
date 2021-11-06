4337efdf33a115975b3e126f11045db3b82e5c66
soywiz <soywiz@gmail.com>
2021-11-06 19:51:52 +0100

    Added AsyncByteArrayDequeChunked

--- korio/src/commonMain/kotlin/com/soywiz/korio/async/AsyncByteArrayDeque.kt
+++ korio/src/commonMain/kotlin/com/soywiz/korio/async/AsyncByteArrayDeque.kt
@@ -1,11 +1,15 @@
 package com.soywiz.korio.async
 
 import com.soywiz.kds.*
+import com.soywiz.klock.*
 import com.soywiz.kmem.*
+import com.soywiz.korio.lang.*
 import com.soywiz.korio.stream.*
 import kotlinx.coroutines.channels.*
 
-class AsyncByteArrayDeque(private val bufferSize: Int = 1024) : AsyncOutputStream, AsyncInputStream {
+interface IAsyncByteArrayDeque : AsyncOutputStream, AsyncInputStream
+
+class AsyncByteArrayDeque(private val bufferSize: Int = 1024) : IAsyncByteArrayDeque {
     var name: String? = null
 	private val notifyRead = Channel<Unit>(Channel.CONFLATED)
 	private val notifyWrite = Channel<Unit>(Channel.CONFLATED)
@@ -46,4 +50,60 @@ class AsyncByteArrayDeque(private val bufferSize: Int = 1024) : AsyncOutputStrea
 	}
 
     override fun toString(): String = "AsyncByteArrayDeque($name)"
+}
+
+class AsyncByteArrayDequeChunked(val maxSize: Int = AsyncByteArrayDequeChunked.DEFAULT_MAX_SIZE) : IAsyncByteArrayDeque {
+    companion object {
+        const val DEFAULT_MAX_SIZE = 8 * 1024 * 1024
+    }
+
+    var name: String? = null
+    private val chunks = ChunkedByteDeque()
+    private var completed = false
+
+    private suspend fun waitToWriteMore(len: Int) {
+        if (chunks.availableRead > maxSize) {
+            while (chunks.availableRead > 1 + (maxSize / 2)) {
+                //println("WRITE WAITING: availableRead=${chunks.availableRead} > ${1 + (maxSize / 2)}")
+                //delay(100.milliseconds) // @TODO: Proper synchronization
+                delay(1.milliseconds) // @TODO: Proper synchronization
+            }
+        }
+    }
+
+    override suspend fun write(buffer: ByteArray, offset: Int, len: Int) {
+        if (len <= 0) return
+        if (completed) error("Trying to write to a completed $this")
+
+        waitToWriteMore(len)
+        chunks.write(buffer, offset, len)
+    }
+
+    override suspend fun write(byte: Int) {
+        waitToWriteMore(1)
+        chunks.write(byte)
+    }
+
+    override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int {
+        if (len <= 0) return 0
+        if (offset < 0 || offset + len > buffer.size) throw OutOfBoundsException()
+        while (true) {
+            val out = chunks.read(buffer, offset, len)
+
+            if (out <= 0 && !completed) {
+                //println("READ WAITING: out=$out, completed=$completed")
+                //delay(100.milliseconds) // @TODO: Proper synchronization
+                delay(1.milliseconds) // @TODO: Proper synchronization
+                continue
+            }
+
+            return out
+        }
+    }
+
+    override suspend fun close() {
+        completed = true
+    }
+
+    override fun toString(): String = "AsyncByteArrayDequeV2($name)"
 }
