e6d1de916408e8f917949ebde0876f2e68e3e37f
Marko Koschak <marko.koschak@tisno.de>
2021-11-12 14:54:42 +0100

    Update unit test and cleanup
    The ASETest unit test was fixed. Also some meaningful tests for usage of
    the ImageDecodingProps were added.
    
    In ImageFrameLayer the properties targetX and targetY were reverted to
    "val" again. The change was not needed with the new design of loading
    the Aseprite image files.

--- korim/src/commonMain/kotlin/com/soywiz/korim/format/ImageFrameLayer.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/format/ImageFrameLayer.kt
@@ -5,8 +5,8 @@ import com.soywiz.korim.bitmap.*
 open class ImageFrameLayer constructor(
     val layer: ImageLayer,
     slice: BmpSlice,
-    var targetX: Int = 0,
-    var targetY: Int = 0,
+    val targetX: Int = 0,
+    val targetY: Int = 0,
     val main: Boolean = true,
     val includeInAtlas: Boolean = true,
     val linkedFrameLayer: ImageFrameLayer? = null,
--- korim/src/commonTest/kotlin/com/soywiz/korim/format/ASETest.kt
+++ korim/src/commonTest/kotlin/com/soywiz/korim/format/ASETest.kt
@@ -1,7 +1,10 @@
 package com.soywiz.korim.format
 
+import com.soywiz.kds.ExtraTypeCreate
+import com.soywiz.kds.setExtra
 import com.soywiz.korim.atlas.*
 import com.soywiz.korio.async.*
+import com.soywiz.korio.file.baseName
 import com.soywiz.korio.file.std.*
 import com.soywiz.korio.util.*
 import kotlin.test.*
@@ -13,16 +16,46 @@ class ASETest {
         val simple = resourcesVfs["simple.ase"].readImageData(ASE, atlas = atlas)
         val simple2 = resourcesVfs["simple2.ase"].readImageData(ASE, atlas = atlas)
         val simple3 = resourcesVfs["simple3.ase"].readImageData(ASE, atlas = atlas)
-        val sliceExample = resourcesVfs["slice-example.ase"].readImageDataContainer(ASE, atlas = atlas)
-        val sliceExample2 = resourcesVfs["slice-example2.ase"].readImageDataContainer(ASE, atlas = atlas)
-        val hiddenLayer = resourcesVfs["hidden-layer.ase"].readImageData(ASE, atlas = atlas)
 
-        assertEquals(2, hiddenLayer.frames[0].layerData.size, "There are only two (visible) layers in layerData of hidden-layer.ase file")
-        assertEquals(4, sliceExample2.imageDatas.size, "4 image data objects, two for 2 slices on layer 1 and two for anothe 2 slices on layer 2")
+        // Check reading of objects from different slices of an image
+        val sliceExample = resourcesVfs["slice-example.ase"].readImageDataContainer(ASE, atlas = atlas)
         assertEquals(5, sliceExample.imageDatas.size, "5 slices create 5 image data objects")
 
-        // TODO: Add test for readParallaxDataContainerFromAseFile
-        //       Check if slicing is working per selected layer
+        // Check that invisible layers are ignored
+        val hiddenLayer = resourcesVfs["hidden-layer.ase"].readImageData(ASE, atlas = atlas)
+        assertEquals(2, hiddenLayer.frames[0].layerData.size, "There are only two (visible) layers in layerData of hidden-layer.ase file")
+
+        // Check slicing of an image with 2 layers
+        val sliceExample2 = resourcesVfs["slice-example2.ase"].readImageDataContainer(ASE, atlas = atlas)
+        assertEquals(2, sliceExample2.imageDatas.size, "2 image data objects for 2 slices")
+        assertEquals(2, sliceExample2.imageDatas[0].layers.size, "2 layers on slice 1")
+        assertEquals(2, sliceExample2.imageDatas[1].layers.size, "2 layers on slice 2")
+
+        // Check if loading only specific layer(s) works
+        val props = ImageDecodingProps(extra = ExtraTypeCreate())
+        props.setExtra("layers", "Layer 2")
+        var specificLayers = resourcesVfs["hidden-layer.ase"].readImageDataContainer(ASE, props = props, atlas = atlas)
+        assertEquals(1, specificLayers.imageDatas[0].layers.size, "Read only one layer from Aseprite file")
+        props.setExtra("layers", "Layer 2,Layer 3")
+        specificLayers = resourcesVfs["hidden-layer.ase"].readImageDataContainer(ASE, props = props, atlas = atlas)
+        assertEquals(2, specificLayers.imageDatas[0].layers.size, "Read two layers from Aseprite file")
+
+        // Check if disabling slicing is working for an Aseprite file which contains slices
+        val props2 = ImageDecodingProps(extra = ExtraTypeCreate())
+        props2.setExtra("disableSlicing", true)
+        val sliceExample3 = resourcesVfs["slice-example.ase"].readImageDataContainer(ASE, props = props2, atlas =
+        atlas)
+        assertEquals(1, sliceExample3.imageDatas.size, "1 image which contains all 5 \"objects\" because slicing is disabled")
+
+        // Check if position of sliced objects is taken over correctly
+        props2.setExtra("disableSlicing", false)
+        props2.setExtra("useSlicePosition", true)
+        val sliceExample4 = resourcesVfs["slice-example.ase"].readImageDataContainer(ASE, props = props2, atlas = atlas)
+        assertEquals(5, sliceExample4.imageDatas.size, "5 slices create 5 image data objects")
+        assertEquals(16, sliceExample4.imageDatasByName["giantHilt"]!!.frames[0].targetX, "X position of giantHilt set to slice X position")
+        assertEquals(16, sliceExample4.imageDatasByName["giantHilt"]!!.frames[0].targetY, "Y position of giantHilt set to slice Y position")
+        assertEquals(48, sliceExample4.imageDatasByName["coin"]!!.frames[0].targetX, "X position of coin set to slice X position")
+        assertEquals(80, sliceExample4.imageDatasByName["coin"]!!.frames[0].targetY, "Y position of coin set to slice Y position")
 
         //println(sliceExample)
         //atlas.allBitmaps.showImagesAndWait()
