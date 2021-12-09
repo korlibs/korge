20994f33e323e0d7bf6790a49693867ba20cdf4d
soywiz <soywiz@gmail.com>
2021-12-09 22:44:14 +0100

    Actually compile using the Kotlin version from gradle.properties (1.6.0 now)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     `kotlin-dsl`
     publishing
@@ -6,12 +8,25 @@ plugins {
     //kotlin("multiplatform")
     //kotlin("gradle-plugin")
 }
-val kotlinVersion = "1.5.31"
-val androidToolsBuildGradleVersion = "4.2.0"
+
+val gradleProperties = Properties().also { it.load(File(rootDir, "../gradle.properties").readText().reader()) }
+
+//Properties().also { it.load() }
+
+//val kotlinVersion = "1.5.31"
+//val kotlinVersion = "1.6.0"
+//val androidToolsBuildGradleVersion = "4.2.0"
+
+val kotlinVersion = gradleProperties["kotlinVersion"].toString()
+val androidBuildGradleVersion = when {
+    System.getProperty("java.version").startsWith("1.8") || System.getProperty("java.version").startsWith("9") -> "4.2.0"
+    else -> gradleProperties["androidBuildGradleVersion"].toString()
+}
 
 dependencies {
-    implementation("com.android.tools.build:gradle:$androidToolsBuildGradleVersion")
+    implementation("com.android.tools.build:gradle:$androidBuildGradleVersion")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    implementation("com.google.code.gson:gson:2.8.6")
 }
 
 
