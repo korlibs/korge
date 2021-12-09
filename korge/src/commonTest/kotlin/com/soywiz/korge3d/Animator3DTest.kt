a964a87df9d8f0da80797047f0f06fa799c6e594
soywiz <soywiz@gmail.com>
2021-12-09 22:40:46 +0100

    Disable vis-assert (somehow preventing intellij from importing the project in some cases)

--- korge/src/commonTest/kotlin/com/soywiz/korge3d/Animator3DTest.kt
+++ korge/src/commonTest/kotlin/com/soywiz/korge3d/Animator3DTest.kt
@@ -7,11 +7,12 @@ import com.soywiz.korge3d.format.*
 import com.soywiz.korio.async.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.interpolation.Easing
-import it.krzeminski.visassert.assertFunctionConformsTo
+//import it.krzeminski.visassert.assertFunctionConformsTo
 import kotlin.test.*
 
 @Korge3DExperimental
 class Animator3DTest {
+    /*
     @Test
     fun updateWithDefaultPlaybackPattern() = suspendTestNoJs {
         val library = resourcesVfs["skinning.dae"].readColladaLibrary()
@@ -91,4 +92,5 @@ class Animator3DTest {
             throw RuntimeException("Not relevant - shouldn't be called in this unit test")
         }
     }
+    */
 }
