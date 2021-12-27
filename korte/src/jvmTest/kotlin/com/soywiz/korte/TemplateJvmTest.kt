25d4520f3636657e12c1639aea03b9c7cd3eb716
soywiz <soywiz@gmail.com>
2021-12-27 14:13:48 +0100

    Verifies https://github.com/korlibs/korte/issues/20

--- korte/src/jvmTest/kotlin/com/soywiz/korte/TemplateJvmTest.kt
+++ korte/src/jvmTest/kotlin/com/soywiz/korte/TemplateJvmTest.kt
@@ -1,5 +1,6 @@
 package com.soywiz.korte
 
+import java.time.*
 import kotlin.test.*
 
 class TemplateJvmTest {
@@ -19,6 +20,21 @@ class TemplateJvmTest {
         assertEquals(
             "1,2",
             Template("{{ location.latitude }},{{ location.longitude }}")("location" to location)
+        )
+    }
+
+    @Test
+    // https://github.com/korlibs/korte/issues/20
+    fun testJvmLocalDate() = suspendTest {
+        val startDate = LocalDate.of(2021, Month.NOVEMBER, 7)
+        val days = 3
+
+        data class Entry(val title: String, val date: LocalDate)
+        val rows = (1..10).map { Entry(title = "test$it", date = LocalDate.of(2021, Month.NOVEMBER, 5 + it)) }
+
+        assertEquals(
+            "test2,test3,test4,",
+            Template("{% for row in rows %}{% if row.date >= startDate && row.date < startDate.plusDays(days) %}{{ row.title }},{% endif %}{% endfor %}")("startDate" to startDate, "days" to days, "rows" to rows)
         )
     }
 }
