ceff25b4816390463ecad3fcc4c6c660344b6dc0
soywiz <soywiz@gmail.com>
2022-01-15 01:57:10 +0100

    Make GLCanvasGameWindow exitProcessOnExit=false by default

--- korgw/src/jvmMain/kotlin/com/soywiz/korgw/awt/GLCanvasGameWindow.kt
+++ korgw/src/jvmMain/kotlin/com/soywiz/korgw/awt/GLCanvasGameWindow.kt
@@ -5,6 +5,7 @@ import java.awt.*
 
 open class GLCanvasGameWindow(val canvas: GLCanvas) : BaseAwtGameWindow() {
     init {
+        exitProcessOnExit = false
         canvas.ag.isGlAvailable = false
         canvas.defaultRenderer = { gl, g ->
             framePaint(g)
