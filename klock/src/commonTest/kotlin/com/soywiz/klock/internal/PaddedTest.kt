56bcd97a3b22c385d3247a4feaddef55231842a0
soywiz <soywiz@gmail.com>
2022-02-06 18:45:08 +0100

    Added extra test to PaddedTest

--- klock/src/commonTest/kotlin/com/soywiz/klock/internal/PaddedTest.kt
+++ klock/src/commonTest/kotlin/com/soywiz/klock/internal/PaddedTest.kt
@@ -10,5 +10,6 @@ class PaddedTest {
         assertEquals("01.013", 1.013.padded(2, 3))
         assertEquals("1.013", 1.013333.padded(1, 3))
         assertEquals("1.100", 1.1.padded(1, 3))
+        assertEquals("1.010", 1.010.padded(1, 3))
     }
 }
