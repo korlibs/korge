334ced392b565a325f283ad12cee2a00fd5eaf7c
Soywiz InVideo <soywiz@invideo.io>
2022-02-17 21:29:50 +0100

    Add reference test to verify View filters

--- korge/src/jvmTest/kotlin/com/soywiz/korge/view/ViewsJvmTest.kt
+++ korge/src/jvmTest/kotlin/com/soywiz/korge/view/ViewsJvmTest.kt
@@ -3,6 +3,10 @@ package com.soywiz.korge.view
 import com.soywiz.korge.scene.*
 import com.soywiz.korge.test.*
 import com.soywiz.korge.tests.*
+import com.soywiz.korge.view.filter.ColorMatrixFilter
+import com.soywiz.korge.view.filter.ComposedFilter
+import com.soywiz.korge.view.filter.Convolute3Filter
+import com.soywiz.korge.view.filter.Filter
 import com.soywiz.korim.bitmap.*
 import com.soywiz.korma.geom.*
 import kotlin.test.*
@@ -37,5 +41,25 @@ class ViewsJvmTest : ViewsForTesting(log = true) {
     fun textGetBounds2() = viewsTest {
         val font = views.debugBmpFont
         assertEquals(Rectangle(0, 0, 154, 16), TextOld("Hello World", font = font, textSize = 16.0).globalBounds)
+    }
+
+    @Test
+    fun testFilter() {
+        views.stage += Container().apply {
+            this += Image(tex).also {
+                it.addFilter(ColorMatrixFilter(ColorMatrixFilter.GRAYSCALE_MATRIX))
+                it.addFilter(Convolute3Filter(Convolute3Filter.KERNEL_EDGE_DETECTION))
+            }
+        }
+        assertEquals(
+            """
+				Stage
+				 Container
+				  Image:bitmap=BitmapSlice(null:SizeInt(width=10, height=10))
+			""".trimIndent(),
+            views.stage.dumpToString()
+        )
+        views.render()
+        assertEqualsFileReference("korge/render/ViewsJvmTestFilter.log", ag.getLogAsString())
     }
 }
