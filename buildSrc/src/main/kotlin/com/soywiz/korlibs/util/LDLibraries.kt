55b99f1930ef14cb77cebe84aebe7fc09ba3290a
Carlos Ballesteros Velasco <soywiz@gmail.com>
2022-03-22 00:26:44 +0100

    Try to fix linux libraries not found on /lib /usr/lib + now ncurses or tinfo are not needed & openal & glut do not require dev versions, only runtime ones (#519)

--- buildSrc/src/main/kotlin/com/soywiz/korlibs/util/LDLibraries.kt
+++ buildSrc/src/main/kotlin/com/soywiz/korlibs/util/LDLibraries.kt
@@ -6,20 +6,32 @@ import java.nio.file.Files
 object LDLibraries {
     private val libFolders = LinkedHashSet<File>()
     private val loadConfFiles = LinkedHashSet<File>()
+
+    val ldFolders: List<File> get() = libFolders.toList()
+
     // /etc/ld.so.conf
     // include /etc/ld.so.conf.d/*.conf
 
+    fun addPath(path: String) {
+        val file = File(path)
+        if (file.isDirectory) {
+            libFolders.add(file)
+        }
+    }
+
     init {
         try {
+            // Fixed paths as described https://renenyffenegger.ch/notes/Linux/fhs/etc/ld_so_conf
+            addPath("/lib")
+            addPath("/usr/lib")
+            // Load config file
             loadConfFile(File("/etc/ld.so.conf"))
         } catch (e: Throwable) {
             e.printStackTrace()
         }
     }
 
-    val ldFolders = libFolders.toList()
-
-    fun hasLibrary(name: String) = ldFolders.any { File(it, name).exists() }
+    fun hasLibrary(name: String) = libFolders.any { File(it, name).exists() }
 
     private fun loadConfFile(file: File) {
         if (file in loadConfFiles) return
@@ -29,11 +41,17 @@ object LDLibraries {
 
             if (tline.startsWith("include ")) {
                 val glob = tline.removePrefix("include ")
-                for (folder in Files.newDirectoryStream(File(glob).parentFile.toPath(), File(glob).name).toList().map { it.toFile() }) {
-                    loadConfFile(folder)
+                val globFolder = File(glob).parentFile
+                val globPattern = File(glob).name
+                if (globFolder.isDirectory) {
+                    for (folder in
+                    Files.newDirectoryStream(globFolder.toPath(), globPattern).toList().map { it.toFile() }
+                    ) {
+                        loadConfFile(folder)
+                    }
                 }
             } else {
-                libFolders += File(tline)
+                addPath(tline)
             }
         }
     }
--- korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/targets/linux/LDLibraries.kt
+++ korge-gradle-plugin/src/main/kotlin/com/soywiz/korge/gradle/targets/linux/LDLibraries.kt
@@ -6,20 +6,32 @@ import java.nio.file.Files
 object LDLibraries {
     private val libFolders = LinkedHashSet<File>()
     private val loadConfFiles = LinkedHashSet<File>()
+
+    val ldFolders: List<File> get() = libFolders.toList()
+
     // /etc/ld.so.conf
     // include /etc/ld.so.conf.d/*.conf
 
+    fun addPath(path: String) {
+        val file = File(path)
+        if (file.isDirectory) {
+            libFolders.add(file)
+        }
+    }
+
     init {
         try {
+            // Fixed paths as described https://renenyffenegger.ch/notes/Linux/fhs/etc/ld_so_conf
+            addPath("/lib")
+            addPath("/usr/lib")
+            // Load config file
             loadConfFile(File("/etc/ld.so.conf"))
         } catch (e: Throwable) {
             e.printStackTrace()
         }
     }
 
-    val ldFolders = libFolders.toList()
-
-    fun hasLibrary(name: String) = ldFolders.any { File(it, name).exists() }
+    fun hasLibrary(name: String) = libFolders.any { File(it, name).exists() }
 
     private fun loadConfFile(file: File) {
         if (file in loadConfFiles) return
@@ -33,13 +45,13 @@ object LDLibraries {
                 val globPattern = File(glob).name
                 if (globFolder.isDirectory) {
                     for (folder in
-                        Files.newDirectoryStream(globFolder.toPath(), globPattern).toList().map { it.toFile() }
+                    Files.newDirectoryStream(globFolder.toPath(), globPattern).toList().map { it.toFile() }
                     ) {
                         loadConfFile(folder)
                     }
                 }
             } else {
-                libFolders += File(tline)
+                addPath(tline)
             }
         }
     }
