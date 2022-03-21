f1c919e290bedb14cebe247a5880870ca999a301
Carlos Ballesteros Velasco <soywiz@gmail.com>
2022-03-21 21:52:49 +0100

    Execute BaseAwtGameWindow.DialogInterface methods with EventQueue.invokeLater (#513)

--- korgw/src/jvmMain/kotlin/com/soywiz/korgw/awt/BaseAwtGameWindow.kt
+++ korgw/src/jvmMain/kotlin/com/soywiz/korgw/awt/BaseAwtGameWindow.kt
@@ -16,6 +16,7 @@ import com.soywiz.korio.file.std.*
 import com.soywiz.korio.net.URL
 import com.soywiz.korio.util.*
 import com.sun.jna.*
+import kotlinx.coroutines.*
 import java.awt.*
 import java.awt.event.*
 import java.awt.event.KeyEvent
@@ -300,32 +301,42 @@ abstract class BaseAwtGameWindow : GameWindow() {
         }
     }
 
+    suspend inline fun <T> invokeLater(crossinline block: () -> T): T {
+        val deferred = CompletableDeferred<T>()
+        EventQueue.invokeLater {
+            deferred.complete(block())
+        }
+        return deferred.await()
+    }
+
     override suspend fun alert(message: String) {
-        JOptionPane.showMessageDialog(component, message, "Message", JOptionPane.WARNING_MESSAGE)
+        invokeLater { JOptionPane.showMessageDialog(component, message, "Message", JOptionPane.WARNING_MESSAGE) }
     }
 
     override suspend fun confirm(message: String): Boolean {
-        return JOptionPane.showConfirmDialog(component, message, "Confirm", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION
+        return invokeLater { JOptionPane.showConfirmDialog(component, message, "Confirm", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION }
     }
 
     override suspend fun prompt(message: String, default: String): String {
-        return JOptionPane.showInputDialog(component, message, "Input", JOptionPane.PLAIN_MESSAGE, null, null, default).toString()
+        return invokeLater { JOptionPane.showInputDialog(component, message, "Input", JOptionPane.PLAIN_MESSAGE, null, null, default).toString() }
     }
 
     override suspend fun openFileDialog(filter: FileFilter?, write: Boolean, multi: Boolean, currentDir: VfsFile?): List<VfsFile> {
         //val chooser = JFileChooser()
-        val mode = if (write) FileDialog.SAVE else FileDialog.LOAD
-        val chooser = FileDialog(this.component.getContainerFrame(), "Select file", mode)
-        if (currentDir != null) {
-            chooser.directory = currentDir.absolutePath
+        return invokeLater {
+            val mode = if (write) FileDialog.SAVE else FileDialog.LOAD
+            val chooser = FileDialog(this.component.getContainerFrame(), "Select file", mode)
+            if (currentDir != null) {
+                chooser.directory = currentDir.absolutePath
+            }
+            chooser.setFilenameFilter { dir, name -> filter == null || filter.matches(name) }
+            chooser.setLocationRelativeTo(null)
+            //chooser.fileFilter = filter // @TODO: Filters
+            chooser.isMultipleMode = multi
+            //chooser.isMultiSelectionEnabled = multi
+            chooser.isVisible = true
+            chooser.files.map { localVfs(it) }
         }
-        chooser.setFilenameFilter { dir, name -> filter == null || filter.matches(name) }
-        chooser.setLocationRelativeTo(null)
-        //chooser.fileFilter = filter // @TODO: Filters
-        chooser.isMultipleMode = multi
-        //chooser.isMultiSelectionEnabled = multi
-        chooser.isVisible = true
-        return chooser.files.map { localVfs(it) }
     }
 
     fun dispatchReshapeEvent() {
