7c290a413b93b6671a2dd8d67a841a5e270d2d5d
soywiz <soywiz@gmail.com>
2022-03-30 20:21:55 +0200

    Fix CycleMethodTest on JS

--- korim/src/commonTest/kotlin/com/soywiz/korim/vector/CycleMethodTest.kt
+++ korim/src/commonTest/kotlin/com/soywiz/korim/vector/CycleMethodTest.kt
@@ -1,5 +1,6 @@
 package com.soywiz.korim.vector
 
+import com.soywiz.korio.util.*
 import com.soywiz.korma.math.*
 import kotlin.test.*
 
@@ -10,13 +11,13 @@ class CycleMethodTest {
 
         assertEquals(
             """
-                NO_CYCLE: [-3.1, -2.5, -2.0, -1.9, -1.1, -1.0, -0.1, 0.0, 0.1, 1.0, 1.1, 1.9, 2.0, 2.5, 3.1]
-                NO_CYCLE_CLAMP: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-                REPEAT: [0.9, 0.5, 0.0, 0.1, 0.9, 0.0, 0.9, 0.0, 0.1, 0.0, 0.1, 0.9, 0.0, 0.5, 0.1]
-                REFLECT: [0.9, 0.5, 0.0, 0.1, 0.9, 1.0, 0.1, 0.0, 0.1, 1.0, 0.9, 0.1, 0.0, 0.5, 0.9]
+                NO_CYCLE: [-3.1, -2.5, -2, -1.9, -1.1, -1, -0.1, 0, 0.1, 1, 1.1, 1.9, 2, 2.5, 3.1]
+                NO_CYCLE_CLAMP: [0, 0, 0, 0, 0, 0, 0, 0, 0.1, 1, 1, 1, 1, 1, 1]
+                REPEAT: [0.9, 0.5, 0, 0.1, 0.9, 0, 0.9, 0, 0.1, 0, 0.1, 0.9, 0, 0.5, 0.1]
+                REFLECT: [0.9, 0.5, 0, 0.1, 0.9, 1, 0.1, 0, 0.1, 1, 0.9, 0.1, 0, 0.5, 0.9]
             """.trimIndent(),
             CycleMethod.values().joinToString("\n") { method ->
-                "$method: ${values.map { method.apply(it).roundDecimalPlaces(3) }}"
+                "$method: ${values.map { method.apply(it).roundDecimalPlaces(3).niceStr }}"
             }
         )
     }
