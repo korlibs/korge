bf8033244e7f6d531934c056b9344e73559112c9
soywiz <soywiz@gmail.com>
2022-04-04 00:55:31 +0200

    Add Incomplete and Internal annotations

---
+++ korio/src/commonMain/kotlin/com/soywiz/korio/annotations/Internal.kt
@@ -0,0 +1,7 @@
+package com.soywiz.korio.annotations
+
+/**
+ * This API is internal, and shouldn't be used for final users.
+ */
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
+annotation class Internal
