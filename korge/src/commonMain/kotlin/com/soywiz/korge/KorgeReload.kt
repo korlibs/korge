153f1858d7468e35e25c56e0dcd6572484f2a8dc
soywiz-invideo <soywiz@invideo.io>
2022-06-21 12:44:56 +0200

    Fixes mutability exception on K/N multithreaded

--- korge/src/commonMain/kotlin/com/soywiz/korge/KorgeReload.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/KorgeReload.kt
@@ -3,19 +3,21 @@ package com.soywiz.korge
 import com.soywiz.korev.Event
 import com.soywiz.korev.EventDispatcher
 import kotlin.jvm.JvmStatic
+import kotlin.native.concurrent.ThreadLocal
+
+@ThreadLocal
+private var KorgeReload_eventDispatcher: EventDispatcher? = null
 
 object KorgeReload {
-    var eventDispatcher: EventDispatcher? = null
-
     @JvmStatic
     @Suppress("unused") // This is called from [com.soywiz.korge.reloadagent.KorgeReloadAgent]
     fun triggerReload(classes: List<String>) {
         println("KorgeReloadAgent detected a class change. Reload: $classes")
-        eventDispatcher?.dispatch(ReloadEvent::class, ReloadEvent(classes))
+        KorgeReload_eventDispatcher?.dispatch(ReloadEvent::class, ReloadEvent(classes))
     }
 
     fun registerEventDispatcher(eventDispatcher: EventDispatcher) {
-        this.eventDispatcher = eventDispatcher
+        KorgeReload_eventDispatcher = eventDispatcher
     }
 }
 
