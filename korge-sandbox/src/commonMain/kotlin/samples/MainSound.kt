1aefdf2b3d4e7c5e15c54d008d0b254c5b48bc63
Marko Koschak <marko.koschak@tisno.de>
2022-06-28 18:14:50 +0200

    Added ogg music file for testing

--- korge-sandbox/src/commonMain/kotlin/samples/MainSound.kt
+++ korge-sandbox/src/commonMain/kotlin/samples/MainSound.kt
@@ -8,6 +8,7 @@ import com.soywiz.korio.file.std.*
 class MainSound : Scene() {
     override suspend fun Container.sceneMain() {
         val music = resourcesVfs["sounds/Snowland.mp3"].readMusic()
+//        val music = resourcesVfs["sounds/Deus_Ex_Tempus.ogg"].readMusic()
         val channel = music.play()
         channel.volume = 1.0
     }
