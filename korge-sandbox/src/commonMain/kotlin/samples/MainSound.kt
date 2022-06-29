3ce4504f98446b96e86dd2221ba56b3ae68607f8
soywiz-invideo <soywiz@invideo.io>
2022-06-29 10:26:55 +0200

    Fix MainSound sample

--- korge-sandbox/src/commonMain/kotlin/samples/MainSound.kt
+++ korge-sandbox/src/commonMain/kotlin/samples/MainSound.kt
@@ -1,12 +1,12 @@
 package samples
 
 import com.soywiz.korge.scene.Scene
-import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.SContainer
 import com.soywiz.korau.sound.*
 import com.soywiz.korio.file.std.*
 
 class MainSound : Scene() {
-    override suspend fun Container.sceneMain() {
+    override suspend fun SContainer.sceneMain() {
         val music = resourcesVfs["sounds/Snowland.mp3"].readMusic()
 //        val music = resourcesVfs["sounds/Deus_Ex_Tempus.ogg"].readMusic()
         val channel = music.play()
