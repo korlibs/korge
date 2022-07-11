249e4a9127992506dd1c046a384d1a75f6a4b991
soywiz <soywiz@gmail.com>
2022-07-11 20:57:44 +0200

    Support JVM & JS platform native Charset encoders/decoders

---
+++ korio/src/jsTest/kotlin/com/soywiz/korio/lang/CharsetJsTest.kt
@@ -0,0 +1,19 @@
+package com.soywiz.korio.lang
+
+import com.soywiz.krypto.encoding.hexLower
+import com.soywiz.krypto.encoding.unhex
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CharsetJsTest {
+    // JS TextEncoder only supports UTF-8
+    @Test
+    fun testPlatformCharset() {
+        val charset = platformCharsetProvider.invoke("GB2312", "GB-2312")!!
+        val text = "hello你好"
+        assertEquals(charset, Charset.forName("GB-2312"))
+        assertEquals("68656c6c6fe4bda0e5a5bd", text.toByteArray(UTF8).hexLower)
+        //assertEquals("68656c6c6fc4e3bac3", text.toByteArray(charset).hexLower)
+        assertEquals(text, "68656c6c6fc4e3bac3".unhex.toString(charset))
+    }
+}
