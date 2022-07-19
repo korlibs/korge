804e8a9459f984990a8bf007cef8a095f873a5b5
soywiz-invideo <soywiz@invideo.io>
2022-07-19 16:47:44 +0200

    Support currentThreadName

--- korio/src/mingwMain/kotlin/com/soywiz/korio/lang/ThreadWin.kt
+++ korio/src/mingwMain/kotlin/com/soywiz/korio/lang/ThreadWin.kt
@@ -3,3 +3,4 @@ package com.soywiz.korio.lang
 import platform.windows.GetCurrentThreadId
 
 actual val currentThreadId: Long get() = GetCurrentThreadId().toLong()
+actual val currentThreadName: String? get() = "Thread-$currentThreadId"
