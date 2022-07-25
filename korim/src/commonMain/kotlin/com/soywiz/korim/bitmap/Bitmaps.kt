1cfcb5c74584654c87611eccd2f9d4f20040e7f4
soywiz <soywiz@gmail.com>
2022-07-25 12:59:41 +0200

    Make Bitmaps.transparent and Bitmaps.white premultiplied

--- korim/src/commonMain/kotlin/com/soywiz/korim/bitmap/Bitmaps.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/bitmap/Bitmaps.kt
@@ -4,8 +4,8 @@ import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RgbaArray
 import kotlin.native.concurrent.SharedImmutable
 
-@SharedImmutable @PublishedApi internal val Bitmaps_transparent: BitmapSlice<Bitmap32> = Bitmap32(1, 1, premultiplied = true).slice(name = "transparent")
-@SharedImmutable @PublishedApi internal val Bitmaps_white: BitmapSlice<Bitmap32> = Bitmap32(1, 1, RgbaArray(1) { Colors.WHITE }).slice(name = "white")
+@SharedImmutable @PublishedApi internal val Bitmaps_transparent: BitmapSlice<Bitmap32> = Bitmap32(1, 1, Colors.TRANSPARENT_BLACK.premultiplied).slice(name = "transparent")
+@SharedImmutable @PublishedApi internal val Bitmaps_white: BitmapSlice<Bitmap32> = Bitmap32(1, 1, Colors.WHITE.premultiplied).slice(name = "white")
 
 object Bitmaps {
     inline val transparent: BitmapSlice<Bitmap32> get() = Bitmaps_transparent
