fbaf229a27eaaa633434005430da7c9c8f320313
soywiz <soywiz@gmail.com>
2022-07-30 17:28:28 +0200

    Move all clamping to kmem

--- korim/src/commonMain/kotlin/com/soywiz/korim/internal/ClampExt.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/internal/ClampExt.kt
@@ -1,10 +1,5 @@
 package com.soywiz.korim.internal
 
-internal fun Int.clamp0_255(): Int {
-    val n = this and -(if (this >= 0) 1 else 0)
-    return (n or (255 - n shr 31)) and 0xFF
-}
-
 // a.rgba + (b.rgba * mul) // mul = [0, 256]
 internal fun sumPacked4MulR(a: Int, b: Int, mul: Int): Int {
     val dstRB = (((b and 0x00FF00FF) * mul)) ushr 8
