318f2ad06b21b1ef5bf8c4f96107074d41c32459
soywiz <soywiz@gmail.com>
2022-07-31 17:26:43 +0200

    Enable new memory manager by default

--- buildSrc/src/main/kotlin/com/soywiz/korge/gradle/KorgeDefaults.kt
+++ buildSrc/src/main/kotlin/com/soywiz/korge/gradle/KorgeDefaults.kt
@@ -2,6 +2,6 @@ package com.soywiz.korge.gradle
 
 object KorgeDefaults {
     @JvmField
-    //val USE_NEW_MEMORY_MANAGER_BY_DEFAULT = true
-    val USE_NEW_MEMORY_MANAGER_BY_DEFAULT = false
+    val USE_NEW_MEMORY_MANAGER_BY_DEFAULT = true
+    //val USE_NEW_MEMORY_MANAGER_BY_DEFAULT = false
 }
