21397d2bb6c5d3c254030570fa688836ca80e63b
Carlos Ballesteros Velasco <soywiz@gmail.com>
2022-08-02 13:38:30 +0200

    Do not compile JVM for other targets (searching for plugins), and optimize e2e tests (#912)

--- build.gradle.kts
+++ build.gradle.kts
@@ -740,7 +740,9 @@ samples {
                     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
                     from(project.tasks.getByName("jsProcessResources").outputs.files)
                     afterEvaluate {
-                        from(project.tasks.getByName("korgeProcessedResourcesJsMain").outputs.files)
+                        project.tasks.findByName("korgeProcessedResourcesJsMain")?.outputs?.files?.let {
+                            from(it)
+                        }
                     }
                     //for (sourceSet in gkotlin.js().compilations.flatMap { it.kotlinSourceSets }) from(sourceSet.resources)
                     into(wwwFolder)
