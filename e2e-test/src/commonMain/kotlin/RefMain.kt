0d8493e378f68023c0c1e91c6364c5aff8046c0b
soywiz-invideo <soywiz@invideo.io>
2022-09-07 11:14:55 +0200

    Clean test views

--- e2e-test/src/commonMain/kotlin/RefMain.kt
+++ e2e-test/src/commonMain/kotlin/RefMain.kt
@@ -34,6 +34,7 @@ suspend fun main() = Korge(width = 768, height = 512, bgcolor = Colors["#2b2b2b"
         for (case in cases) {
             println("STARTING ${case.name}...")
             try {
+                removeChildren()
                 case.run(this)
             } catch (e: Throwable) {
                 e.printStackTrace()
