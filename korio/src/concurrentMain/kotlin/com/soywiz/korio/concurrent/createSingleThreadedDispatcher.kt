e19819474759a4ce99446ab2cabca39a76dd55c8
soywiz-invideo <soywiz@invideo.io>
2022-09-29 10:46:10 +0200

    Some adjustments

--- korio/src/concurrentMain/kotlin/com/soywiz/korio/concurrent/createSingleThreadedDispatcher.kt
+++ korio/src/concurrentMain/kotlin/com/soywiz/korio/concurrent/createSingleThreadedDispatcher.kt
@@ -8,8 +8,8 @@ import kotlin.coroutines.*
 actual fun Dispatchers.createFixedThreadDispatcher(name: String, threadCount: Int): CoroutineDispatcher {
     //println("Dispatchers.createSingleThreadedDispatcher['$name'] : Platform.hasMultithreadedSharedHeap=${Platform.hasMultithreadedSharedHeap}")
     if (Platform.hasMultithreadedSharedHeap) {
-        return newFixedThreadPoolContext(threadCount, name)
-        //return newSingleThreadContext(name)
+        //return newFixedThreadPoolContext(threadCount, name)
+        return newSingleThreadContext(name)
         //return createRedirectedDispatcher(name, Dispatchers.Default)
     } else {
         return createRedirectedDispatcher(name, Dispatchers.Default)
