4ae4c30e149fa94568b32e920c92d918f4dcc3b5
Carlos Ballesteros Velasco <soywiz@gmail.com>
2022-10-07 18:31:03 +0200

    Added StackedIntArray2 (#1029)

---
+++ kds/src/commonTest/kotlin/com/soywiz/kds/StackedIntArray2Test.kt
@@ -0,0 +1,38 @@
+package com.soywiz.kds
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StackedIntArray2Test {
+    @Test
+    fun test() {
+        val value = StackedIntArray2(2, 2)
+        assertEquals(-1, value.getFirst(0, 0))
+        value.add(0, 0, 1)
+        value.add(0, 0, 2)
+        value.add(0, 0, 3)
+
+        assertEquals(3, value.getStackLevel(0, 0))
+        assertEquals(1, value.getFirst(0, 0))
+        assertEquals(2, value.get(0, 0, 1))
+        assertEquals(3, value.getLast(0, 0))
+
+        value.removeLast(0, 0)
+        assertEquals(1, value.getFirst(0, 0))
+        assertEquals(2, value.getLast(0, 0))
+        assertEquals(2, value.getStackLevel(0, 0))
+
+        value.removeLast(0, 0)
+        assertEquals(1, value.getFirst(0, 0))
+        assertEquals(1, value.getLast(0, 0))
+        assertEquals(1, value.getStackLevel(0, 0))
+
+        value.removeLast(0, 0)
+        assertEquals(-1, value.getFirst(0, 0))
+        assertEquals(-1, value.getLast(0, 0))
+        assertEquals(0, value.getStackLevel(0, 0))
+
+        assertEquals(-1, value.getFirst(1, 0))
+        assertEquals(0, value.getStackLevel(1, 0))
+    }
+}
