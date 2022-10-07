e36580650d6d1664edd4fd25baac6fd5a51ef33a
soywiz <soywiz@gmail.com>
2022-10-07 18:37:29 +0200

    Fixes test

--- korgw/src/commonTest/kotlin/com/soywiz/korgw/GameWindowTest.kt
+++ korgw/src/commonTest/kotlin/com/soywiz/korgw/GameWindowTest.kt
@@ -1,13 +1,14 @@
 package com.soywiz.korgw
 
 import com.soywiz.korim.color.Colors
-import com.soywiz.korim.format.showImageAndWait
 import com.soywiz.korim.vector.buildShape
-import com.soywiz.korim.vector.renderWithHotspot
 import com.soywiz.korio.async.suspendTest
+import com.soywiz.korma.geom.PointInt
 import com.soywiz.korma.geom.Size
-import com.soywiz.korma.geom.vector.*
+import com.soywiz.korma.geom.vector.lineTo
+import com.soywiz.korma.geom.vector.moveTo
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class GameWindowTest {
     @Test
@@ -22,7 +23,7 @@ class GameWindowTest {
         })
         val bitmap = cursor.createBitmap()
         assertEquals(Size(64, 32), bitmap.bitmap.size)
-        assertEquals(Point(32, 32), bitmap.hospot)
+        assertEquals(PointInt(32, 31), bitmap.hotspot)
         //bitmap.bitmap.showImageAndWait()
     }
 }
