636593ca9b20cf713967b4736eafea3d428ff726
soywiz <soywiz@gmail.com>
2022-10-07 19:50:03 +0200

    Add View.convertViewSpace

--- korge/src/commonMain/kotlin/com/soywiz/korge/view/ViewExt.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/view/ViewExt.kt
@@ -3,6 +3,8 @@ package com.soywiz.korge.view
 import com.soywiz.klock.Frequency
 import com.soywiz.klock.timesPerSecond
 import com.soywiz.korio.lang.Cancellable
+import com.soywiz.korma.geom.IPoint
+import com.soywiz.korma.geom.Point
 
 val Double.fps get() = this.timesPerSecond
 val Int.fps get() = this.timesPerSecond
@@ -13,3 +15,12 @@ fun <T : View> T.addUpdater(referenceFps: Frequency, first: Boolean = true, upda
         updatable(it / time)
     }
 }
+
+
+fun View.Companion.convertViewSpace(src: View, srcPoint: IPoint, dst: View, dstPoint: Point = Point()): IPoint {
+    src.localToGlobal(srcPoint, dstPoint)
+    return dst.globalToLocal(dstPoint, dstPoint)
+}
+
+fun View.convertToSpace(srcPoint: IPoint, dst: View, dstPoint: Point = Point()): IPoint =
+    View.Companion.convertViewSpace(this, srcPoint, dst, dstPoint)
