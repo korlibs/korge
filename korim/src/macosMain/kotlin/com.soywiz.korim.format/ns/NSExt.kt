c88d592126c400f057993977e611dc8afcc1f437
soywiz <soywiz@gmail.com>
2022-10-08 15:19:03 +0200

    Try to fix NSExt errors on MacOS targets

--- korim/src/macosMain/kotlin/com.soywiz.korim.format/ns/NSExt.kt
+++ korim/src/macosMain/kotlin/com.soywiz.korim.format/ns/NSExt.kt
@@ -1,27 +1,12 @@
 package com.soywiz.korim.format.ns
 
-import com.soywiz.korim.bitmap.Bitmap32
-import com.soywiz.korim.format.cg.cg
-import com.soywiz.korim.format.cg.transferBitmap32CGContext
-import com.soywiz.korma.geom.IPoint
-import com.soywiz.korma.geom.IPointInt
-import com.soywiz.korma.geom.Point
-import com.soywiz.korma.geom.Rectangle
-import kotlinx.cinterop.CValue
-import kotlinx.cinterop.convert
-import kotlinx.cinterop.useContents
-import platform.AppKit.NSImage
-import platform.CoreGraphics.CGBitmapContextCreate
-import platform.CoreGraphics.CGBitmapContextCreateImage
-import platform.CoreGraphics.CGColorSpaceCreateDeviceRGB
-import platform.CoreGraphics.CGColorSpaceRelease
-import platform.CoreGraphics.CGContextRelease
-import platform.CoreGraphics.CGImageAlphaInfo
-import platform.CoreGraphics.CGImageRelease
-import platform.Foundation.NSMakePoint
-import platform.Foundation.NSMakeSize
-import platform.Foundation.NSPoint
-import platform.Foundation.NSRect
+import com.soywiz.korim.bitmap.*
+import com.soywiz.korim.format.cg.*
+import com.soywiz.korma.geom.*
+import kotlinx.cinterop.*
+import platform.AppKit.*
+import platform.CoreGraphics.*
+import platform.Foundation.*
 
 fun Bitmap32.toNSImage(): NSImage {
     val bmp = this
@@ -34,7 +19,7 @@ fun Bitmap32.toNSImage(): NSImage {
     val image = CGBitmapContextCreateImage(ctx)
     try {
         transferBitmap32CGContext(this, ctx, toBitmap = false)
-        return NSImage(image, NSMakeSize(bmp.width.cg, bmp.height.cg))
+        return NSImage(image, NSMakeSize(bmp.width.toDouble(), bmp.height.toDouble()))
     } finally {
         CGImageRelease(image)
         CGContextRelease(ctx)
@@ -44,7 +29,7 @@ fun Bitmap32.toNSImage(): NSImage {
 
 //fun NSImage.toBitmap32(): Bitmap32 { TODO() }
 
-fun IPoint.toNSPoint(): CValue<NSPoint> = NSMakePoint(x.cg, y.cg)
-fun IPointInt.toNSPoint(): CValue<NSPoint> = NSMakePoint(x.cg, y.cg)
+fun IPoint.toNSPoint(): CValue<NSPoint> = NSMakePoint(x.toDouble(), y.cg.toDouble())
+fun IPointInt.toNSPoint(): CValue<NSPoint> = NSMakePoint(x.cg.toDouble(), y.cg.toDouble())
 fun CValue<NSPoint>.toPoint(): Point = useContents { Point(this.x, this.y) }
 fun CValue<NSRect>.toRectangle(): Rectangle = useContents { Rectangle(this.origin.x, this.origin.y, this.size.width, this.size.height) }
