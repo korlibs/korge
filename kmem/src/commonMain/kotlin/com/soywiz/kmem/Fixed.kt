0240e001abe4abdf256c141c31608cfe34072c31
soywiz <soywiz@gmail.com>
2022-10-31 19:50:40 +0100

    Add code to support denormals

--- kmem/src/commonMain/kotlin/com/soywiz/kmem/Fixed.kt
+++ kmem/src/commonMain/kotlin/com/soywiz/kmem/Fixed.kt
@@ -5,20 +5,48 @@ package com.soywiz.kmem
  *
  * Floating points have more precision in the range [0, 1] and losses decimal precision outside that range.
  * This fixed point won't lose precision in any integer range.
+ *
+ * [Fixed] in decimal it has precision of:
+ * +-9999999.99
+ *
+ * 7 integer digits, 2 decimal digits.
+ *
+ * 1 bit for sign
+ * 23~24 bits of integer
+ * 6~7 bits of decimal
  */
 inline class Fixed private constructor(val value: Int) : Comparable<Fixed> {
     companion object {
-        const val SCALE_DIGITS = 3
-        const val SCALE = 1000
+        const val SCALE_DIGITS = 2
+        const val SCALE = 100
+        const val HANDLE_DENORMALS = false
+        //const val HANDLE_DENORMALS = true
+
+        val NEGATIVE_INFINITY: Fixed get() = Fixed(Int.MIN_VALUE)
+        val NaN: Fixed get() = Fixed(Int.MIN_VALUE + 1)
+        val POSITIVE_INFINITY: Fixed get() = Fixed(Int.MAX_VALUE)
 
         operator fun invoke(value: Int, unit: Unit = Unit): Fixed = Fixed((value * SCALE))
-        operator fun invoke(value: Double): Fixed = Fixed((value * SCALE).toInt())
+        operator fun invoke(value: Double): Fixed {
+            if (HANDLE_DENORMALS) {
+                if (value.isNaN()) return NaN
+                when (value) {
+                    Double.NEGATIVE_INFINITY -> return NEGATIVE_INFINITY
+                    Double.POSITIVE_INFINITY -> return POSITIVE_INFINITY
+                }
+            }
+            return Fixed((value * SCALE).toInt())
+        }
         operator fun invoke(value: String): Fixed {
-            val int = value.substringBefore('.')
-            val dec = value.substringAfter('.', "0")
-            return Fixed(int.toInt() * SCALE + (dec.toInt() % SCALE))
+            return Fixed(value.toDouble())
+            //val int = value.substringBefore('.')
+            //val dec = value.substringAfter('.', "0")
+            //return Fixed(int.toInt() * SCALE + (dec.toInt() % SCALE))
         }
     }
+
+    val valueInt: Int get() = value / SCALE
+    val valueRem: Int get() = value % SCALE
 
     operator fun unaryPlus(): Fixed = this
     operator fun unaryMinus(): Fixed = Fixed(-value)
@@ -29,12 +57,28 @@ inline class Fixed private constructor(val value: Int) : Comparable<Fixed> {
     operator fun div(other: Fixed): Fixed = Fixed((value / other.toDouble()).toInt())
     override fun compareTo(other: Fixed): Int = this.value.compareTo(other.value)
 
-    fun toDouble(): Double = value.toDouble() / SCALE
+    fun toDouble(): Double {
+        if (HANDLE_DENORMALS) {
+            when (this) {
+                NEGATIVE_INFINITY -> return Double.NEGATIVE_INFINITY
+                NaN -> return Double.NaN
+                POSITIVE_INFINITY -> return Double.POSITIVE_INFINITY
+            }
+        }
+        return value.toDouble() / SCALE
+    }
     fun toFloat(): Float = toDouble().toFloat()
     fun toLong(): Long = toInt().toLong()
     fun toInt(): Int = value / SCALE
 
     override fun toString(): String {
+        if (HANDLE_DENORMALS) {
+            when (this) {
+                NEGATIVE_INFINITY -> return "-Infinity"
+                NaN -> return "NaN"
+                POSITIVE_INFINITY -> return "Infinity"
+            }
+        }
         val str = "$value"
         return when {
             str.length <= SCALE_DIGITS -> "0.$str"
--- kmem/src/commonTest/kotlin/com/soywiz/kmem/FixedTest.kt
+++ kmem/src/commonTest/kotlin/com/soywiz/kmem/FixedTest.kt
@@ -7,9 +7,29 @@ class FixedTest {
     @Test
     fun test() {
         // 1123456f + 1.123f -> 1123457.1f // Precision lost!
+        assertEquals((-1).fixed, -(1.fixed))
+        assertEquals((+1).fixed, +(1.fixed))
         assertEquals(1.fixed, 1.fixed)
-        assertEquals("1123457.123".fixed, 1123456f.fixed + 1.123f.fixed)
+        assertEquals("1123457.12".fixed, 1123456f.fixed + 1.12f.fixed)
         assertEquals(0.5.fixed, 1.fixed / 2.fixed)
         assertEquals(2.fixed, 1.fixed * 2.fixed)
+        assertEquals("21474836.47".fixed, 1123456f.fixed * 77.33f.fixed)
+    }
+
+    @Test
+    fun testDenormals() {
+        if (!Fixed.HANDLE_DENORMALS) return
+
+        assertEquals("NaN".fixed, Double.NaN.fixed)
+        assertEquals("-Infinity".fixed, Double.NEGATIVE_INFINITY.fixed)
+        assertEquals("Infinity".fixed, Double.POSITIVE_INFINITY.fixed)
+
+        assertEquals("NaN", Double.NaN.fixed.toString())
+        assertEquals("-Infinity", Double.NEGATIVE_INFINITY.fixed.toString())
+        assertEquals("Infinity", Double.POSITIVE_INFINITY.fixed.toString())
+
+        assertEquals("NaN", Fixed.NaN.toString())
+        assertEquals("-Infinity", Fixed.NEGATIVE_INFINITY.toString())
+        assertEquals("Infinity", Fixed.POSITIVE_INFINITY.toString())
     }
 }
