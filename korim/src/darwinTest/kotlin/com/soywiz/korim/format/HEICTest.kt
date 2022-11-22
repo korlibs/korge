aa88b09c0f9042f25e1ff156cb44dea7ef9f23d3
soywiz <soywiz@gmail.com>
2022-11-22 11:40:15 +0100

    It seems that the decoded HEIC image is different on some devices, let's use PSNR instead to be more lax

--- korim/src/darwinTest/kotlin/com/soywiz/korim/format/HEICTest.kt
+++ korim/src/darwinTest/kotlin/com/soywiz/korim/format/HEICTest.kt
@@ -1,13 +1,9 @@
 package com.soywiz.korim.format
 
-import com.soywiz.korim.bitmap.Bitmap32
-import com.soywiz.korio.async.suspendTest
-import com.soywiz.korio.file.std.localVfs
-import com.soywiz.korio.file.std.resourcesVfs
-import kotlin.test.Test
-import kotlin.test.assertContentEquals
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import com.soywiz.korim.bitmap.*
+import com.soywiz.korio.async.*
+import com.soywiz.korio.file.std.*
+import kotlin.test.*
 
 class HEICTest {
     @Test
@@ -15,6 +11,10 @@ class HEICTest {
         val heic = resourcesVfs["heic.heic"].readBitmap().toBMP32().premultiplied()
         val png = resourcesVfs["heic.heic.png"].readBitmap(PNG.toProps()).toBMP32().premultiplied()
         //localVfs("/tmp/heic.heic.png").writeBitmap(heic, PNG)
-        assertEquals(true, Bitmap32.matches(heic, png))
+        assertGreaterOrEqual(30.0, Bitmap32.computePsnr(heic, png), message = "PSNR")
+    }
+
+    fun assertGreaterOrEqual(expected: Double, actual: Double, message: String = "") {
+        assertTrue("Expected $actual >= $expected : $message") { actual >= expected }
     }
 }
