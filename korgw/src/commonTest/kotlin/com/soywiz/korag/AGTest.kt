063b27c918161f40c844be193e6f3fa737869211
soywiz <soywiz@gmail.com>
2022-12-03 19:47:34 +0100

    Cleanup: Remove AGSoftware

--- korgw/src/commonTest/kotlin/com/soywiz/korag/AGTest.kt
+++ korgw/src/commonTest/kotlin/com/soywiz/korag/AGTest.kt
@@ -1,22 +1,8 @@
 package com.soywiz.korag
 
-import com.soywiz.korag.shader.Program
-import com.soywiz.korag.shader.VertexShader
-import com.soywiz.korag.shader.appending
-import com.soywiz.korag.software.AGFactorySoftware
-import com.soywiz.korio.async.suspendTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertSame
+import kotlin.test.*
 
 class AGTest {
-	@Test
-	fun testOnReady() = suspendTest {
-		val ag = AGFactorySoftware().create(null, AGConfig())
-		val buffer = ag.createIndexBuffer()
-		buffer.upload(intArrayOf(1, 2, 3, 4))
-	}
-
     @Test
     fun testCombineScissor() {
         assertEquals(AGScissor.NIL, AGScissor.combine(AGScissor.NIL, AGScissor.NIL))
@@ -24,25 +10,5 @@ class AGTest {
         assertEquals(AGScissor(50, 50, 50, 50), AGScissor.combine(AGScissor(0, 0, 100, 100), AGScissor(50, 50, 100, 100)))
         assertEquals(AGScissor(50, 50, 100, 100), AGScissor.combine(AGScissor.NIL, AGScissor(50, 50, 100, 100)))
         assertEquals(AGScissor(0, 0, 0, 0), AGScissor.combine(AGScissor(2000, 2000, 100, 100), AGScissor(50, 50, 100, 100)))
-    }
-
-    val VERTEX = VertexShaderDefault {
-        SET(v_Tex, a_Tex)
-        SET(out, (u_ProjMat * u_ViewMat) * vec4(a_Pos, 0f.lit, 1f.lit))
-    }
-    val FRAGMENT = FragmentShaderDefault {
-        SET(out, out["bgra"])
-    }
-
-    @Test
-    fun testReusedShaders() {
-        fun createProgram() = Program(
-            VERTEX,
-            FRAGMENT.appending { SET(out, out["gbba"]) }
-        )
-        val ag = AGFactorySoftware().create(null, AGConfig())
-        val program1 = ag.getProgram(createProgram())
-        val program2 = ag.getProgram(createProgram())
-        assertSame(program1, program2, "Recreating a shader model, should not regenerate a hardware shader")
     }
 }
