b96c6b72299dd3e51470346f90711a06f078abc9
soywiz <soywiz@gmail.com>
2022-12-28 07:25:27 +0100

    Fix UTF8 in JS

--- korio/src/jsMain/kotlin/com/soywiz/korio/lang/UTF8.kt
+++ korio/src/jsMain/kotlin/com/soywiz/korio/lang/UTF8.kt
@@ -3,7 +3,12 @@ package com.soywiz.korio.lang
 import com.soywiz.kmem.*
 import org.khronos.webgl.*
 
-actual val UTF8: Charset = object : UTC8CharsetBase("UTF-8") {
+actual val UTF8: Charset = UTC8CharsetBase("UTF-8")
+
+/**
+ * This won't support streaming decoding
+ */
+val JS_UTF8 = object : UTC8CharsetBase("UTF-8") {
 	val textDecoder: TextDecoder? = try {
 		TextDecoder("utf-8")
 	} catch (e: dynamic) {
@@ -16,13 +21,14 @@ actual val UTF8: Charset = object : UTC8CharsetBase("UTF-8") {
 		null
 	} // Do not fail if not supported!
 
-	override fun decode(out: StringBuilder, src: ByteArray, start: Int, end: Int) {
+	override fun decode(out: StringBuilder, src: ByteArray, start: Int, end: Int): Int {
 		if (textDecoder != null) {
 			val srcBuffer = src.unsafeCast<Int8Array>()
 			out.append(textDecoder.decode(Int8Array(srcBuffer.buffer, start, end - start)))
 		} else {
 			super.decode(out, src, start, end)
 		}
+        return end - start
 	}
 
 	override fun encode(out: ByteArrayBuilder, src: CharSequence, start: Int, end: Int) {
