333833d41dc144f622f77ac1a1053aefc592f252
Alexandre Mommers <none@ygdrasil.io>
2023-02-04 16:30:34 +0100

    try to render texture

--- graphics/backend/metal/src/darwinMain/kotlin/korge/graphics/backend/metal/Renderer02.kt
+++ graphics/backend/metal/src/darwinMain/kotlin/korge/graphics/backend/metal/Renderer02.kt
@@ -5,17 +5,22 @@ import com.soywiz.korag.shader.*
 import com.soywiz.korag.shader.gl.*
 import korge.graphics.backend.metal.shader.*
 import kotlinx.cinterop.*
+import kotlinx.cinterop.nativeHeap.alloc
+import platform.CoreFoundation.*
+import platform.CoreGraphics.*
 import platform.Foundation.NSError
 import platform.Foundation.NSMakeRange
+import platform.ImageIO.*
 import platform.Metal.*
 import platform.MetalKit.MTKView
-import platform.posix.memmove
+import platform.posix.*
 
 class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
 
+    private lateinit var samplerState: MTLSamplerStateProtocol
     private lateinit var vertexPositionsBuffer: MTLBufferProtocol
     private lateinit var vertexColorsBuffer: MTLBufferProtocol
-    private lateinit var textureCoordinate: MTLBufferProtocol
+    private lateinit var textureCoordinateBuffer: MTLBufferProtocol
     private lateinit var indexBuffer: MTLBufferProtocol
     private lateinit var renderPipelineStateProtocol: MTLRenderPipelineStateProtocol
     private lateinit var texture: MTLTextureProtocol
@@ -24,6 +29,15 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
         buildShaders()
         buildBuffers()
         buildTextures()
+        createSampler()
+    }
+
+    private fun createSampler() {
+        samplerState = MTLSamplerDescriptor().apply {
+            minFilter = MTLSamplerMinMagFilterLinear
+            magFilter = MTLSamplerMinMagFilterLinear
+            mipFilter = MTLSamplerMinMagFilterLinear
+        }.let(device::newSamplerStateWithDescriptor) ?: error("fail to create sampler")
     }
 
     override fun drawOnView(view: MTKView) {
@@ -37,8 +51,11 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
 
             renderCommanderEncoder.apply {
                 setRenderPipelineState(renderPipelineStateProtocol)
+                setFragmentTexture(texture, 0)
                 setVertexBuffer(vertexColorsBuffer, 0, 0)
-                setVertexBuffer(vertexPositionsBuffer, 0, 1)
+                setVertexBuffer(textureCoordinateBuffer, 0, 1)
+                setVertexBuffer(vertexPositionsBuffer, 0, 2)
+                setFragmentSamplerState(samplerState, 0)
                 drawIndexedPrimitives(MTLPrimitiveTypeTriangle, 6u, MTLIndexTypeUInt32, indexBuffer, 0)
             }
 
@@ -53,10 +70,11 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
 
         val vertexShader = VertexShader {
             SET(DefaultShaders.v_Col, DefaultShaders.a_Col)
+            SET(DefaultShaders.v_Tex, DefaultShaders.a_Tex)
             SET(out, vec4(DefaultShaders.a_Pos, 1f.lit, 1f.lit))
         }
         val fragmentShader = FragmentShader {
-            SET(out, DefaultShaders.v_Col)
+            SET(out, texture2D(DefaultShaders.u_Tex, DefaultShaders.v_Tex["xy"])["rgba"] * DefaultShaders.v_Col)
         }
 
         vertexShader.toNewGlslStringResult()
@@ -70,33 +88,38 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
         var shaderSrc = """
                 #include <metal_stdlib>
                 using namespace metal;
-        
+                
                 struct v2f
                 {
                     float4 v_Col;
+                    float2 v_Tex;
                     float4 position [[position]];
                 };
         
                 vertex v2f vertexMain( uint vertexId [[vertex_id]],
-                                       device const float4* colors [[buffer(0)]],
-                                       device const float2* positions [[buffer(1)]] )
+                                       device const float4* a_Col [[buffer(0)]],
+                                       device const float2* a_Tex [[buffer(1)]],
+                                       device const float2* a_Pos [[buffer(2)]] )
                 {
-                    v2f o;
-                    o.position = float4( positions[ vertexId ], 0.0, 1.0 );
-                    o.v_Col =  colors[ vertexId ];
-                    return o;
+                    v2f out;
+                    out.position = float4( a_Pos[ vertexId ], 0.0, 1.0 );
+	                out.v_Tex = a_Tex[ vertexId ];
+                    out.v_Col =  a_Col[ vertexId ];
+                    return out;
                 }
         
-                fragment float4 fragmentMain( v2f in [[stage_in]] )
+                fragment float4 fragmentMain( v2f in [[stage_in]], 
+                    texture2d<float> tex [[texture(0)]], 
+                    sampler sampler [[sampler(0)]])
                 {
                     float4 out;
-                    out = in.v_Col;
+                    out = in.v_Col - tex.sample(sampler, in.v_Tex);
                     return out;
                 }
         """.trimIndent()
 
-        shaderSrc =
-            (vertexShader to fragmentShader).toNewMetalShaderStringResult()
+        //shaderSrc =
+        (vertexShader to fragmentShader).toNewMetalShaderStringResult()
             .result
             .also(::println)
 
@@ -139,7 +162,7 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
             0f, 1f
         )
 
-        textureCoordinate = createBufferUsing(
+        textureCoordinateBuffer = createBufferUsing(
             0f, 0f,
             1f, 0f,
             1f, 1f,
@@ -147,7 +170,7 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
         )
 
         indexBuffer = createBufferUsing(
-            0,1,2, 2, 3, 0
+            0, 1, 2, 2, 3, 0
         )
     }
 
@@ -190,22 +213,20 @@ class Renderer02(device: MTLDeviceProtocol) : Renderer(device) {
 
         texture = device.newTextureWithDescriptor(mtlTextureDescriptor) ?: error("fail to create texture")
 
-        val pTextureData = allocArray<IntVar>((tw * th * 4u).toInt())
+        val pTextureData = allocArray<UIntVar>((tw * th * 4u).toInt())
         for (y in (0 until th.toInt())) {
             for (x in (0 until tw.toInt())) {
-                val isWhite = ((x xor y) and 0b1000000) != 0
-                val c = if (isWhite) 0xFF else 0xA
 
                 val i = y * tw.toInt() + x
 
-                pTextureData[i * 4 + 0] = c
-                pTextureData[i * 4 + 1] = c
-                pTextureData[i * 4 + 2] = c
-                pTextureData[i * 4 + 3] = 0xFF
+                pTextureData[i * 4 + 0] = 255u
+                pTextureData[i * 4 + 1] = 255u
+                pTextureData[i * 4 + 2] = 255u
+                pTextureData[i * 4 + 3] = 255u
             }
         }
 
-        texture.replaceRegion(MTLRegionMake3D(0uL, 0uL, 0uL, tw, th, 1uL), 0, pTextureData, tw * 4uL)
+        texture.replaceRegion(MTLRegionMake2D(0uL, 0uL, tw, th), 0, pTextureData, tw * 4uL)
 
     }
 }
@@ -218,4 +239,30 @@ public fun NativePlacement.allocArrayOf(vararg elements: Int): CArrayPointer<Int
         ++index
     }
     return res
+}
+
+
+fun MTLTextureProtocol.toCGImage(): CGImageRef {
+    val rowBytes = width * 4u
+    val region = MTLRegionMake2D(0, 0, width, height)
+
+    val bufferPointer = malloc(rowBytes * height)
+    getBytes(bufferPointer, rowBytes, region, 0)
+
+    val colorSpace = CGColorSpaceCreateDeviceRGB()
+    val bitmapInfo = CGImageAlphaInfo.kCGImageAlphaPremultipliedLast.value
+
+    val context = CGBitmapContextCreateWithData(
+         bufferPointer,
+        width,
+        height,
+        8,
+        rowBytes,
+        colorSpace,
+        bitmapInfo,
+        null,
+        null
+    )
+
+    return CGBitmapContextCreateImage(context) ?: error("")
 }
