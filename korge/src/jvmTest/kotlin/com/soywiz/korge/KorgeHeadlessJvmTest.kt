5fcd09ef971bc54b506d729b726cc670a8aaa9c5
soywiz <soywiz@gmail.com>
2023-02-10 14:56:26 +0100

    Disable try { } finally { } to avoid ignoring other errors

--- korge/src/jvmTest/kotlin/com/soywiz/korge/KorgeHeadlessJvmTest.kt
+++ korge/src/jvmTest/kotlin/com/soywiz/korge/KorgeHeadlessJvmTest.kt
@@ -1,5 +1,6 @@
 package com.soywiz.korge
 
+import com.soywiz.klogger.*
 import com.soywiz.korge.testing.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
@@ -8,26 +9,30 @@ import org.junit.Test
 import kotlin.test.*
 
 class KorgeHeadlessJvmTest {
+    val logger = Logger("KorgeHeadlessJvmTest")
+
     @Test
     fun testHeadlessTest() {
         var wasCalled = false
-        try {
-            korgeOffscreenTest(width = 256, height = 256, bgcolor = Colors["#2b2b2b"]) {
-                val image = solidRect(100, 100, Colors.RED) {
-                    rotation = 16.degrees
-                    anchor(.5, .5)
-                    scale(.8)
-                    position(128, 128)
-                }
-                val bmp = renderToBitmap(includeBackground = true)
-                assertEquals(views.clearColor, bmp[0, 0])
-                assertEquals(Colors.RED, bmp[128, 128])
-                //bmp.showImageAndWait()
-                //assertEquals()
-                wasCalled = true
+        logger.info { "1" }
+        korgeOffscreenTest(width = 256, height = 256, bgcolor = Colors["#2b2b2b"]) {
+            val image = solidRect(100, 100, Colors.RED) {
+                rotation = 16.degrees
+                anchor(.5, .5)
+                scale(.8)
+                position(128, 128)
             }
-        } finally {
-            assertEquals(true, wasCalled)
+            logger.info { "2" }
+            val bmp = renderToBitmap(includeBackground = true)
+            logger.info { "3" }
+            assertEquals(views.clearColor, bmp[0, 0])
+            assertEquals(Colors.RED, bmp[128, 128])
+            logger.info { "4" }
+            //bmp.showImageAndWait()
+            //assertEquals()
+            wasCalled = true
         }
+        logger.info { "5" }
+        assertEquals(true, wasCalled)
     }
 }
