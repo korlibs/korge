fb865bf08579a3992e406f8de91bb7f0841f8f95
soywiz <soywiz@gmail.com>
2023-02-14 01:48:34 +0100

    Move i18n functionality to kproject https://github.com/korlibs/korge-services/tree/main/korge-i18n

--- korge/src/commonMain/kotlin/com/soywiz/korge/i18n/TextContainer.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.korge.i18n
-
-import com.soywiz.korio.util.i18n.Language
-
-interface TextContainer {
-	fun updateText(language: Language) {
-	}
-}
--- korge/src/commonMain/kotlin/com/soywiz/korge/i18n/TextSource.kt
+++
@@ -1,7 +0,0 @@
-package com.soywiz.korge.i18n
-
-import com.soywiz.korio.util.i18n.Language
-
-interface TextSource {
-	fun getText(language: Language): String
-}
