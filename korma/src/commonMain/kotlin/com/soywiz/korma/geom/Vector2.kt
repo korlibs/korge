d87e89b2907e7d5be9e592780e5df5e9d64ee6a7
soywiz <soywiz@gmail.com>
2023-02-28 12:09:09 +0100

    Make Vector2 be a typealias of Point

--- korma/src/commonMain/kotlin/com/soywiz/korma/geom/Vector2.kt
+++ korma/src/commonMain/kotlin/com/soywiz/korma/geom/Vector2.kt
@@ -5,15 +5,7 @@ import com.soywiz.korma.annotations.*
 
 typealias PointF = Vector2
 
-@KormaValueApi
-//data class Vector2(val x: Float, val y: Float)
-inline class Vector2(val data: Long) {
-    val x: Float get() = data.toInt().reinterpretAsFloat()
-    val y: Float get() = (data ushr 32).toInt().reinterpretAsFloat()
-    constructor(x: Float, y: Float) : this(
-        (x.reinterpretAsInt().toLong() and 0xFFFFFFFFL)
-    )
-}
+typealias Vector2 = Point
 
 @KormaMutableApi
 interface IVector2 {
