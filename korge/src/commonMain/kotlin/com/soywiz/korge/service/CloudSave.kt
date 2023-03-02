e016be85a10c53b8ad9acb701d4b6134c2c4192a
soywiz <soywiz@gmail.com>
2023-03-02 01:44:36 +0100

    Cleanup unimplemented services

--- korge/src/commonMain/kotlin/com/soywiz/korge/service/CloudSave.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.korge.service
-
-//@Singleton
-open class CloudSave {
-	//companion object {
-	//	operator fun invoke() = Services.load(CloudSave::class.java).firstOrNull() ?: unsupported("Not ${CloudSave::class.java.name} implementation found")
-	//}
-}
--- korge/src/commonMain/kotlin/com/soywiz/korge/service/RateApp.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.korge.service
-
-//@Singleton
-open class RateApp {
-	//companion object {
-	//    operator fun invoke() = Services.load(RateApp::class.java).firstOrNull() ?: unsupported("Not ${RateApp::class.java.name} implementation found")
-	//}
-}
