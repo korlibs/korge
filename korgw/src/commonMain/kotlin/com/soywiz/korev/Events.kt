1bdd79fc7b415602ed69de605f88c8d8db035c11
soywiz <soywiz@gmail.com>
2023-03-07 17:57:50 +0100

    Minor refactor: move classes to other file

--- korgw/src/commonMain/kotlin/com/soywiz/korev/Events.kt
+++ korgw/src/commonMain/kotlin/com/soywiz/korev/Events.kt
@@ -9,6 +9,21 @@ import com.soywiz.korio.lang.*
 import com.soywiz.korio.util.*
 import com.soywiz.korma.geom.*
 
+open class TypedEvent<T : BEvent>(open override var type: EventType<T>) : Event(), TEvent<T>
+
+open class Event {
+    var target: Any? = null
+    var _stopPropagation = false
+    fun stopPropagation() {
+        _stopPropagation = true
+    }
+}
+
+fun Event.preventDefault(reason: Any? = null): Nothing = throw PreventDefaultException(reason)
+fun preventDefault(reason: Any? = null): Nothing = throw PreventDefaultException(reason)
+
+class PreventDefaultException(val reason: Any? = null) : Exception()
+
 interface BEvent {
     var target: Any?
     val type: EventType<out BEvent>
