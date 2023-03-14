8a93e5376e66114a525f71fbee19a98033ab74e4
soywiz <soywiz@gmail.com>
2023-03-14 20:15:26 +0100

    Implement some Vector3 & Vector4 operations and use Float4Pack

---
+++ korma/src/commonTest/kotlin/com/soywiz/korma/geom/Vector3Test.kt
@@ -0,0 +1,19 @@
+package com.soywiz.korma.geom
+
+import kotlin.test.*
+
+class Vector3Test {
+    @Test
+    fun testAdd() = assertEquals(Vector3(11f, 22f, 33f), Vector3(1f, 2f, 3f) + Vector3(10f, 20f, 30f))
+    @Test
+    fun testTimes() = assertEquals(Vector3(-1f, -4f, -9f), Vector3(1f, 2f, 3f) * Vector3(-1f, -2f, -3f))
+    @Test
+    fun testToString() = assertEquals("Vector3(1, 2, 3)", Vector3(1f, 2f, 3f).toString())
+    @Test
+    fun testNormalized() = assertEquals(1f, Vector3(1f, 2f, 4f).normalized().length, 0.00001f)
+    @Test
+    fun testEquals() {
+        assertEquals(true, Vector3(1f, 2f, 3f) == Vector3(1f, 2f, 3f))
+        assertEquals(false, Vector3(1f, 2f, 3f) == Vector3(1f, 2f, -3f))
+    }
+}
---
+++ korma/src/commonTest/kotlin/com/soywiz/korma/geom/Vector4Test.kt
@@ -0,0 +1,19 @@
+package com.soywiz.korma.geom
+
+import kotlin.test.*
+
+class Vector4Test {
+    @Test
+    fun testAdd() = assertEquals(Vector4(11f, 22f, 33f, 44f), Vector4(1f, 2f, 3f, 4f) + Vector4(10f, 20f, 30f, 40f))
+    @Test
+    fun testTimes() = assertEquals(Vector4(-1f, -4f, -9f, -16f), Vector4(1f, 2f, 3f, 4f) * Vector4(-1f, -2f, -3f, -4f))
+    @Test
+    fun testToString() = assertEquals("Vector4(1, 2, 3, 4)", Vector4(1f, 2f, 3f, 4f).toString())
+    @Test
+    fun testNormalized() = assertEquals(1f, Vector4(1f, 2f, 4f, 8f).normalized().length, 0.00001f)
+    @Test
+    fun testEquals() {
+        assertEquals(true, Vector4(1f, 2f, 3f, 4f) == Vector4(1f, 2f, 3f, 4f))
+        assertEquals(false, Vector4(1f, 2f, 3f, 4f) == Vector4(1f, 2f, 3f, -4f))
+    }
+}
