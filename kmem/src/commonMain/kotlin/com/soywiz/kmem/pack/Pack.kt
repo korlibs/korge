5e58caf9ff62b907154c686deeb6677725a7daad
soywiz <soywiz@gmail.com>
2023-03-22 20:35:34 +0100

    Unify packs on all targets

--- kmem/src/commonMain/kotlin/com/soywiz/kmem/pack/Pack.kt
+++ kmem/src/commonMain/kotlin/com/soywiz/kmem/pack/Pack.kt
@@ -1,104 +1,126 @@
 package com.soywiz.kmem.pack
 
-import com.soywiz.kmem.*
+import kotlin.jvm.*
 
-
-expect class Int4Pack
-expect val Int4Pack.i0: Int
-expect val Int4Pack.i1: Int
-expect val Int4Pack.i2: Int
-expect val Int4Pack.i3: Int
-expect fun int4PackOf(i0: Int, i1: Int, i2: Int, i3: Int): Int4Pack
-fun Int4Pack.copy(i0: Int = this.i0, i1: Int = this.i1, i2: Int = this.i2, i3: Int = this.i3): Int4Pack = int4PackOf(i0, i1, i2, i3)
-
-
-
-
-expect class Half8Pack
-expect val Half8Pack.h0: Float
-expect val Half8Pack.h1: Float
-expect val Half8Pack.h2: Float
-expect val Half8Pack.h3: Float
-expect val Half8Pack.h4: Float
-expect val Half8Pack.h5: Float
-expect val Half8Pack.h6: Float
-expect val Half8Pack.h7: Float
-expect fun half8PackOf(h0: Float, h1: Float, h2: Float, h3: Float, h4: Float, h5: Float, h6: Float, h7: Float): Half8Pack
-
-
-
-
-
-inline class Half4Pack private constructor(val data: Short4Pack) {
-    constructor(x: Half, y: Half, z: Half, w: Half) : this(short4PackOf(
-        x.rawBits.toShort(),
-        y.rawBits.toShort(),
-        z.rawBits.toShort(),
-        w.rawBits.toShort(),
-    ))
-    val x: Half get() = Half.fromBits(data.s0)
-    val y: Half get() = Half.fromBits(data.s1)
-    val z: Half get() = Half.fromBits(data.s2)
-    val w: Half get() = Half.fromBits(data.s3)
+data class Half8Pack(
+    val h0: Float,
+    val h1: Float,
+    val h2: Float,
+    val h3: Float,
+    val h4: Float,
+    val h5: Float,
+    val h6: Float,
+    val h7: Float,
+)
+val Half8Pack.h0: Float get() = this.h0
+val Half8Pack.h1: Float get() = this.h1
+val Half8Pack.h2: Float get() = this.h2
+val Half8Pack.h3: Float get() = this.h3
+val Half8Pack.h4: Float get() = this.h4
+val Half8Pack.h5: Float get() = this.h5
+val Half8Pack.h6: Float get() = this.h6
+val Half8Pack.h7: Float get() = this.h7
+fun half8PackOf(h0: Float, h1: Float, h2: Float, h3: Float, h4: Float, h5: Float, h6: Float, h7: Float): Half8Pack {
+    return Half8Pack(h0, h1, h2, h3, h4, h5, h6, h7)
 }
 
-expect class Float4Pack
-expect val Float4Pack.f0: Float
-expect val Float4Pack.f1: Float
-expect val Float4Pack.f2: Float
-expect val Float4Pack.f3: Float
-expect fun float4PackOf(f0: Float, f1: Float, f2: Float, f3: Float): Float4Pack
-fun Float4Pack.copy(f0: Float = this.f0, f1: Float = this.f1, f2: Float = this.f2, f3: Float = this.f3): Float4Pack = float4PackOf(f0, f1, f2, f3)
 
 
 
 
 
 
-expect class BFloat3Half4Pack
+@JvmInline
+value class Float4Pack(val x: Float, val y: Float, val z: Float, val w: Float)
+val Float4Pack.f0: Float get() = this.x
+val Float4Pack.f1: Float get() = this.y
+val Float4Pack.f2: Float get() = this.z
+val Float4Pack.f3: Float get() = this.w
+fun float4PackOf(f0: Float, f1: Float, f2: Float, f3: Float): Float4Pack = Float4Pack(f0, f1, f2, f3)
+
+
+
+
+
+
+
+
+data class BFloat6Pack(
+    val f0: Float,
+    val f1: Float,
+    val f2: Float,
+    val f3: Float,
+    val f4: Float,
+    val f5: Float,
+)
+
+val BFloat6Pack.bf0: Float get() = f0
+val BFloat6Pack.bf1: Float get() = f1
+val BFloat6Pack.bf2: Float get() = f2
+val BFloat6Pack.bf3: Float get() = f3
+val BFloat6Pack.bf4: Float get() = f4
+val BFloat6Pack.bf5: Float get() = f5
+
+fun bfloat6PackOf(bf0: Float, bf1: Float, bf2: Float, bf3: Float, bf4: Float, bf5: Float): BFloat6Pack =
+    BFloat6Pack(bf0, bf1, bf2, bf3, bf4, bf5)
+
+
+
+data class BFloat3Half4Pack(
+    val f0: Float,
+    val f1: Float,
+    val f2: Float,
+    val f3: Float,
+    val f4: Float,
+    val f5: Float,
+    val f6: Float,
+)
 // 21-bit BFloat precision
-expect val BFloat3Half4Pack.b0: Float
-expect val BFloat3Half4Pack.b1: Float
-expect val BFloat3Half4Pack.b2: Float
+val BFloat3Half4Pack.b0: Float get() = f0
+val BFloat3Half4Pack.b1: Float get() = f1
+val BFloat3Half4Pack.b2: Float get() = f2
 // 16-bit Half Float precision
-expect val BFloat3Half4Pack.hf0: Float
-expect val BFloat3Half4Pack.hf1: Float
-expect val BFloat3Half4Pack.hf2: Float
-expect val BFloat3Half4Pack.hf3: Float
-expect fun bfloat3Half4PackOf(b0: Float, b1: Float, b2: Float, hf0: Float, hf1: Float, hf2: Float, hf3: Float): BFloat3Half4Pack
+val BFloat3Half4Pack.hf0: Float get() = f3
+val BFloat3Half4Pack.hf1: Float get() = f4
+val BFloat3Half4Pack.hf2: Float get() = f5
+val BFloat3Half4Pack.hf3: Float get() = f6
+
+fun bfloat3Half4PackOf(
+    b0: Float, b1: Float, b2: Float,
+    hf0: Float, hf1: Float, hf2: Float, hf3: Float
+): BFloat3Half4Pack {
+    return BFloat3Half4Pack(b0, b1, b2, hf0, hf1, hf2, hf3)
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+data class Int4Pack(val x: Int, val y: Int, val z: Int, val w: Int)
+val Int4Pack.i0: Int get() = this.x
+val Int4Pack.i1: Int get() = this.y
+val Int4Pack.i2: Int get() = this.z
+val Int4Pack.i3: Int get() = this.w
+fun int4PackOf(i0: Int, i1: Int, i2: Int, i3: Int): Int4Pack = Int4Pack(i0, i1, i2, i3)
 
 
 
 
 
+data class Float2Pack(val x: Float, val y: Float)
+val Float2Pack.f0: Float get() = x
+val Float2Pack.f1: Float get() = y
+fun float2PackOf(f0: Float, f1: Float): Float2Pack = Float2Pack(f0, f1)
 
-expect class BFloat6Pack
-expect val BFloat6Pack.bf0: Float
-expect val BFloat6Pack.bf1: Float
-expect val BFloat6Pack.bf2: Float
-expect val BFloat6Pack.bf3: Float
-expect val BFloat6Pack.bf4: Float
-expect val BFloat6Pack.bf5: Float
-expect fun bfloat6PackOf(bf0: Float, bf1: Float, bf2: Float, bf3: Float, bf4: Float, bf5: Float): BFloat6Pack
+data class Int2Pack(val x: Int, val y: Int)
+val Int2Pack.i0: Int get() = x
+val Int2Pack.i1: Int get() = y
+fun int2PackOf(i0: Int, i1: Int): Int2Pack = Int2Pack(i0, i1)
 
-
-
-
-
-expect class Float2Pack
-expect val Float2Pack.f0: Float
-expect val Float2Pack.f1: Float
-expect fun float2PackOf(f0: Float, f1: Float): Float2Pack
-
-expect class Int2Pack
-expect val Int2Pack.i0: Int
-expect val Int2Pack.i1: Int
-expect fun int2PackOf(i0: Int, i1: Int): Int2Pack
-
-expect class Short4Pack
-expect val Short4Pack.s0: Short
-expect val Short4Pack.s1: Short
-expect val Short4Pack.s2: Short
-expect val Short4Pack.s3: Short
-expect fun short4PackOf(s0: Short, s1: Short, s2: Short, s3: Short): Short4Pack
-
+data class Short4Pack(val x: Short, val y: Short, val z: Short, val w: Short)
+val Short4Pack.s0: Short get() = x
+val Short4Pack.s1: Short get() = y
+val Short4Pack.s2: Short get() = z
+val Short4Pack.s3: Short get() = w
+fun short4PackOf(s0: Short, s1: Short, s2: Short, s3: Short): Short4Pack = Short4Pack(s0, s1, s2, s3)
