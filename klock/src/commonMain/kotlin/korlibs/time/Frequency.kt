83a94f991288af8d9a95bebab0c382812fa5fa69
lmvortex4k <lmvortex4k@gmail.com>
2023-03-26 22:40:23 +0200

    Adds operators to Frequency

--- klock/src/commonMain/kotlin/korlibs/time/Frequency.kt
+++ klock/src/commonMain/kotlin/korlibs/time/Frequency.kt
@@ -2,6 +2,8 @@ package korlibs.time
 
 import korlibs.time.hr.hr
 import kotlin.jvm.JvmInline
+import korlibs.time.internal.Serializable
+import korlibs.time.internal.umod
 
 val TimeSpan.hz get() = timesPerSecond
 val Int.hz get() = timesPerSecond
@@ -14,10 +16,29 @@ val Int.timesPerSecond get() = Frequency(this.toDouble())
 val Double.timesPerSecond get() = Frequency(this)
 
 @JvmInline
-value class Frequency(val hertz: Double) {
+value class Frequency(val hertz: Double) : Comparable<Frequency>, Serializable {
     companion object {
         fun from(timeSpan: TimeSpan) = timeSpan.toFrequency()
     }
+
+    override fun compareTo(other: Frequency): Int = this.hertz.compareTo(other.hertz)
+
+    operator fun unaryMinus() = Frequency(-this.hertz)
+    operator fun unaryPlus() = this
+
+    operator fun plus(other: Frequency): Frequency = Frequency(this.hertz + other.hertz)
+    operator fun minus(other: Frequency): Frequency = Frequency(this.hertz - other.hertz)
+
+    operator fun times(scale: Int): Frequency = Frequency(this.hertz * scale)
+    operator fun times(scale: Float): Frequency = Frequency(this.hertz * scale)
+    operator fun times(scale: Double): Frequency = Frequency(this.hertz * scale)
+
+    operator fun div(scale: Int): Frequency = Frequency(this.hertz / scale)
+    operator fun div(scale: Float): Frequency = Frequency(this.hertz / scale)
+    operator fun div(scale: Double): Frequency = Frequency(this.hertz / scale)
+
+    operator fun rem(other: Frequency): Frequency = Frequency(this.hertz % other.hertz)
+    infix fun umod(other: Frequency): Frequency = Frequency(this.hertz umod other.hertz)
 
     val timeSpan get() = (1.0 / this.hertz).seconds
     val hrTimeSpan get() = (1.0 / this.hertz).seconds.hr
--- klock/src/commonTest/kotlin/korlibs/time/FrequencyTest.kt
+++ klock/src/commonTest/kotlin/korlibs/time/FrequencyTest.kt
@@ -9,4 +9,16 @@ class FrequencyTest {
         assertEquals(100.milliseconds, 10.timesPerSecond.timeSpan)
         assertEquals(10.hz, 100.milliseconds.hz)
     }
+
+    @Test
+    fun `test frequency operators work as expected`() {
+        assertEquals((-60).hz, -(60.hz))
+        assertEquals((+60).hz, +(60.hz))
+        assertEquals(80.hz, 60.hz + 20.hz)
+        assertEquals(40.hz, 60.hz - 20.hz)
+        assertEquals(120.hz, 60.hz * 2)
+        assertEquals(90.hz, 60.hz * 1.5)
+        assertEquals(90.hz, 60.hz * 1.5f)
+        assertEquals(5.hz, 65.hz % 30.hz)
+    }
 }
