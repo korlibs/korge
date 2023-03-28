646c5e6439771916b961f33134f82171d03bdedd
soywiz <soywiz@gmail.com>
2023-03-29 00:20:26 +0200

    Make test work on JS too

--- korma/src/commonTest/kotlin/korlibs/math/geom/shape/NShape2DTest.kt
+++ korma/src/commonTest/kotlin/korlibs/math/geom/shape/NShape2DTest.kt
@@ -7,11 +7,11 @@ class NShape2DTest {
     @Test
     fun test() {
         val circle = Circle(Point(100, 100), radius = 10f)
-        assertEquals(314.15927f, circle.area, absoluteTolerance = 0.1f)
-        assertEquals(62.831852f, circle.perimeter, absoluteTolerance = 0.1f)
-        assertEquals(Point(100, 90), circle.projectedPoint(Point(100, 0)))
-        assertEquals(-10f, circle.distance(Point(100, 100)))
-        assertEquals(0f, circle.distance(Point(100, 90)))
-        assertEquals(+10f, circle.distance(Point(100, 80)))
+        assertEqualsFloat(314.15927f, circle.area, absoluteTolerance = 0.1)
+        assertEqualsFloat(62.831852f, circle.perimeter, absoluteTolerance = 0.1)
+        assertEqualsFloat(Point(100, 90), circle.projectedPoint(Point(100, 0)))
+        assertEqualsFloat(-10f, circle.distance(Point(100, 100)))
+        assertEqualsFloat(0f, circle.distance(Point(100, 90)))
+        assertEqualsFloat(+10f, circle.distance(Point(100, 80)))
     }
 }
