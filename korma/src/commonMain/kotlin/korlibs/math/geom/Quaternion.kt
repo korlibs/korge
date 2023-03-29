9e0375506bcdb03cfafaffd8bfbdb9a79bdcc8b1
soywiz <soywiz@gmail.com>
2023-03-29 04:49:50 +0200

    Add missing Quaternion functionality from MQuaternion

--- korma/src/commonMain/kotlin/korlibs/math/geom/Quaternion.kt
+++ korma/src/commonMain/kotlin/korlibs/math/geom/Quaternion.kt
@@ -3,6 +3,7 @@ package korlibs.math.geom
 import korlibs.memory.pack.*
 import kotlin.math.*
 
+// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 //@KormaValueApi
 inline class Quaternion private constructor(val data: Float4Pack) {
     val x: Float get() = data.f0
@@ -11,6 +12,16 @@ inline class Quaternion private constructor(val data: Float4Pack) {
     val w: Float get() = data.f3
     val vector: Vector4 get() = Vector4(data)
     val xyz: Vector3 get() = Vector3(x, y, z)
+    operator fun get(index: Int): Float = when (index) {
+        0 -> x
+        1 -> y
+        2 -> z
+        3 -> w
+        else -> Float.NaN
+    }
+
+    val lengthSquared: Float get() = (x * x) + (y * y) + (z * z) + (w * w)
+    val length: Float get() = sqrt(lengthSquared)
 
     operator fun component1(): Float = x
     operator fun component2(): Float = y
@@ -52,7 +63,59 @@ inline class Quaternion private constructor(val data: Float4Pack) {
         return Quaternion(x / length, y / length, z / length, w / length)
     }
 
+    fun inverted(): Quaternion {
+        val q = this
+        val lengthSquared = q.lengthSquared
+        return when (lengthSquared) {
+            0f -> {
+                val num = 1f / lengthSquared
+                Quaternion(q.x * -num, q.y * -num, q.z * -num, q.w * num)
+            }
+            else -> q
+        }
+    }
+
+    fun toEuler(): EulerRotation = toEuler(x, y, z, w)
+
     companion object {
+        fun dotProduct(l: Quaternion, r: Quaternion): Float = l.x * r.x + l.y * r.y + l.z * r.z + l.w * r.w
+
+        inline fun func(callback: (Int) -> Float) = Quaternion(callback(0), callback(1), callback(2), callback(3))
+        inline fun func(l: Quaternion, r: Quaternion, func: (l: Float, r: Float) -> Float) = Quaternion(
+            func(l.x, r.x),
+            func(l.y, r.y),
+            func(l.z, r.z),
+            func(l.w, r.w)
+        )
+        fun slerp(left: Quaternion, right: Quaternion, t: Float): Quaternion {
+            var tleft = left.normalized()
+            var tright = right.normalized()
+
+            var dot = Quaternion.dotProduct(tleft, right)
+
+            if (dot < 0.0f) {
+                tright = -tright
+                dot = -dot
+            }
+
+            if (dot > 0.99995f) return func(tleft, tright) { l, r -> l + t * (r - l) }
+
+            val angle0 = acos(dot)
+            val angle1 = angle0 * t
+
+            val s1 = sin(angle1) / sin(angle0)
+            val s0 = cos(angle1) - dot * s1
+
+            return func(tleft, tright) { l, r -> ((s0 * l) + (s1 * r)) }
+        }
+
+        fun nlerp(left: Quaternion, right: Quaternion, t: Double): Quaternion {
+            val sign = if (Quaternion.dotProduct(left, right) < 0) -1 else +1
+            return func { ((1f - t) * left[it] + t * right[it] * sign).toFloat() }.normalized()
+        }
+
+        fun interpolated(left: Quaternion, right: Quaternion, t: Float): Quaternion = slerp(left, right, t)
+
         fun fromRotationMatrix(m: Matrix4): Quaternion {
             m.apply {
                 val t = v00 + v11 + v22
@@ -75,6 +138,23 @@ inline class Quaternion private constructor(val data: Float4Pack) {
                     }
                 }
             }
+        }
+
+        fun fromEuler(e: EulerRotation): Quaternion = e.toQuaternion()
+
+        fun toEuler(x: Float, y: Float, z: Float, w: Float): EulerRotation {
+            val sinrCosp = +2.0 * (w * x + y * z)
+            val cosrCosp = +1.0 - 2.0 * (x * x + y * y)
+            val roll = atan2(sinrCosp, cosrCosp)
+            val sinp = +2.0 * (w * y - z * x)
+            val pitch = when {
+                abs(sinp) >= 1 -> if (sinp > 0) PI / 2 else -PI / 2
+                else -> asin(sinp)
+            }
+            val sinyCosp = +2.0 * (w * z + x * y)
+            val cosyCosp = +1.0 - 2.0 * (y * y + z * z)
+            val yaw = atan2(sinyCosp, cosyCosp)
+            return EulerRotation(roll.radians, pitch.radians, yaw.radians)
         }
     }
 }
