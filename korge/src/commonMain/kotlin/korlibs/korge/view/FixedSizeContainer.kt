5175bc89b1df22d08a330096cb26185f71326c6c
soywiz <soywiz@gmail.com>
2023-04-04 09:11:53 +0200

    Fixes CachedContainer & UIScrolalble & Adds FixedSizeCachedContainer clip & builder

--- korge/src/commonMain/kotlin/korlibs/korge/view/FixedSizeContainer.kt
+++ korge/src/commonMain/kotlin/korlibs/korge/view/FixedSizeContainer.kt
@@ -1,10 +1,11 @@
 package korlibs.korge.view
 
+import korlibs.datastructure.*
+import korlibs.io.util.*
 import korlibs.korge.internal.*
 import korlibs.korge.render.*
 import korlibs.korge.view.filter.*
 import korlibs.korge.view.property.*
-import korlibs.io.util.*
 import korlibs.math.geom.*
 
 inline fun Container.fixedSizeContainer(
@@ -43,57 +44,63 @@ open class FixedSizeContainer(
         return out
     }
 
-    private var renderingInternal = false
+    private var renderingInternalRef = Ref(false)
 
     private val tempRect = MRectangle()
 
     @OptIn(KorgeInternal::class)
     override fun renderInternal(ctx: RenderContext) {
-        if (renderingInternal) {
-            return super.renderInternal(ctx)
-        }
-        if (clip) {
-            val m = globalMatrix
-            val hasRotation = m.b != 0f || m.c != 0f
-            //val hasNegativeScale = m.a < 0.0 || m.d < 0.0
-            //if (hasRotation || hasNegativeScale) {
-            if (hasRotation) {
-            //if (true) {
-                // Use a framebuffer instead
-                val old = renderingInternal
-                try {
-                    renderingInternal = true
-                    renderFiltered(ctx, IdentityFilter)
-                } finally {
-                    renderingInternal = old
-                }
-                return
+        renderClipped(this, ctx, clip, renderingInternalRef) { super.renderInternal(ctx) }
+    }
+
+    companion object {
+        inline fun renderClipped(view: View, ctx: RenderContext, clip: Boolean, renderingInternalRef: Ref<Boolean>, superRenderInternal: (ctx: RenderContext) -> Unit) {
+            if (renderingInternalRef.value) {
+                return superRenderInternal(ctx)
             }
-            ctx.useCtx2d { c2d ->
-                // @TODO: Maybe scissor should be global and do the global to window / texture conversions in the very last moment,
-                // @TODO: so we don't propagate that complexity here
-                // If width or height are negative, because scale was negative
-                val bounds = getClippingBounds(ctx).transformed(ctx.viewMat2D).normalized()
-
-                //println("ctx.ag.isRenderingToWindow=${ctx.ag.isRenderingToWindow}, FIXED_CLIP: bounds=$bounds, ctx.viewMat2D=${ctx.viewMat2D}")
-
-                //println("FIXED_CLIP: bounds=$bounds")
-                val rect = c2d.batch.scissor.toRectOrNull()
-                var intersects = true
-                if (rect != null) {
-                    intersects = bounds.intersects(rect)
-                }
-                //println("BOUNDS2: $windowBounds, ${ctx.viewMat2D}")
-                if (intersects) {
-                    c2d.scissor(bounds) {
-                        super.renderInternal(ctx)
+            if (clip) {
+                val m = view.globalMatrix
+                val hasRotation = m.b != 0f || m.c != 0f
+                //val hasNegativeScale = m.a < 0.0 || m.d < 0.0
+                //if (hasRotation || hasNegativeScale) {
+                if (hasRotation) {
+                    //if (true) {
+                    // Use a framebuffer instead
+                    val old = renderingInternalRef.value
+                    try {
+                        renderingInternalRef.value = true
+                        view.renderFiltered(ctx, IdentityFilter)
+                    } finally {
+                        renderingInternalRef.value = old
                     }
-                } else {
-                    super.renderInternal(ctx)
+                    return
                 }
+                ctx.useCtx2d { c2d ->
+                    // @TODO: Maybe scissor should be global and do the global to window / texture conversions in the very last moment,
+                    // @TODO: so we don't propagate that complexity here
+                    // If width or height are negative, because scale was negative
+                    val bounds = view.getClippingBounds(ctx).transformed(ctx.viewMat2D).normalized()
+
+                    //println("ctx.ag.isRenderingToWindow=${ctx.ag.isRenderingToWindow}, FIXED_CLIP: bounds=$bounds, ctx.viewMat2D=${ctx.viewMat2D}")
+
+                    //println("FIXED_CLIP: bounds=$bounds")
+                    val rect = c2d.batch.scissor.toRectOrNull()
+                    var intersects = true
+                    if (rect != null) {
+                        intersects = bounds.intersects(rect)
+                    }
+                    //println("BOUNDS2: $windowBounds, ${ctx.viewMat2D}")
+                    if (intersects) {
+                        c2d.scissor(bounds) {
+                            superRenderInternal(ctx)
+                        }
+                    } else {
+                        superRenderInternal(ctx)
+                    }
+                }
+            } else {
+                superRenderInternal(ctx)
             }
-        } else {
-            super.renderInternal(ctx)
         }
     }
 }
