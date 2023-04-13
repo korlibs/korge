09e709e78654d200c883d7b589ae1f359cc4ce9c
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-04-13 12:11:17 +0200

    Remove old versions for a couple of projects (#1531)

--- korau/src/commonMain/kotlin/korlibs/audio/Korau.kt
+++
@@ -1,7 +0,0 @@
-package korlibs.audio
-
-import korlibs.audio.internal.KORAU_VERSION
-
-object Korau {
-    val VERSION = KORAU_VERSION
-}
--- korma/src/commonMain/kotlin/korlibs/math/Korma.kt
+++
@@ -1,7 +0,0 @@
-package korlibs.math
-
-import korlibs.math.internal.KORMA_VERSION
-
-object Korma {
-    val VERSION = KORMA_VERSION
-}
