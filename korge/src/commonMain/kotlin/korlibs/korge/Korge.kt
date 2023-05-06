429c9cb7394466f12d71527561ad3cd34fd63181
soywiz <soywiz@gmail.com>
2023-05-06 11:36:05 +0200

    Add KorgeDisplayMode.TOP_LEFT_NO_CLIP

--- korge/src/commonMain/kotlin/korlibs/korge/Korge.kt
+++ korge/src/commonMain/kotlin/korlibs/korge/Korge.kt
@@ -35,7 +35,9 @@ data class KorgeDisplayMode(val scaleMode: ScaleMode, val scaleAnchor: Anchor, v
     companion object {
         val DEFAULT get() = CENTER
         val CENTER = KorgeDisplayMode(ScaleMode.SHOW_ALL, Anchor.CENTER, clipBorders = true)
+        //@Deprecated("Typically TOP_LEFT_NO_CLIP is better")
         val CENTER_NO_CLIP = KorgeDisplayMode(ScaleMode.SHOW_ALL, Anchor.CENTER, clipBorders = false)
+        val TOP_LEFT_NO_CLIP = KorgeDisplayMode(ScaleMode.SHOW_ALL, Anchor.TOP_LEFT, clipBorders = false)
         val NO_SCALE = KorgeDisplayMode(ScaleMode.NO_SCALE, Anchor.TOP_LEFT, clipBorders = false)
     }
 }
