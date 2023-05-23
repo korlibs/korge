f8650867a4636f13c4f98df73cd67791131f5e62
soywiz <soywiz@gmail.com>
2023-05-23 17:58:08 +0200

    Add wasm target to KDS

---
+++ kds/src/wasmMain/kotlin/korlibs/datastructure/internal/InternalJs.kt
@@ -0,0 +1,14 @@
+package korlibs.datastructure.internal
+
+@JsFun("""(obj) => {
+    if (window.IDENTITY_HASH_CODE_SYMBOL === undefined) {
+        window.IDENTITY_HASH_CODE_SYMBOL = Symbol("KotlinIdentityHashCode");
+        window.lastIdentityHashCodeId = 0;
+    }
+    if (obj == null) return 0;
+    if (obj[window.IDENTITY_HASH_CODE_SYMBOL] === undefined) {
+        obj[window.IDENTITY_HASH_CODE_SYMBOL] = (window.lastIdentityHashCodeId = window.lastIdentityHashCodeId + 1 | 0); 
+    }
+    return obj[window.IDENTITY_HASH_CODE_SYMBOL];
+}""")
+internal actual external fun anyIdentityHashCode(obj: Any?): Int
