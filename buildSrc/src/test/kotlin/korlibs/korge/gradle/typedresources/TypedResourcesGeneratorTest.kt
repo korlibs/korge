b3f40a6ca28b72f3a18fe9dfb500da4b470fd3b1
soywiz <soywiz@gmail.com>
2023-05-25 20:36:35 +0200

    Improve generated interface

--- buildSrc/src/test/kotlin/korlibs/korge/gradle/typedresources/TypedResourcesGeneratorTest.kt
+++ buildSrc/src/test/kotlin/korlibs/korge/gradle/typedresources/TypedResourcesGeneratorTest.kt
@@ -19,53 +19,53 @@ class TypedResourcesGeneratorTest {
         println(generated)
         assertEquals(
 """
-import korlibs.audio.sound.Sound
-import korlibs.audio.sound.readSound
 import korlibs.image.atlas.Atlas
-import korlibs.image.atlas.readAtlas
 import korlibs.io.file.VfsFile
 import korlibs.io.file.std.resourcesVfs
+import korlibs.image.atlas.readAtlas
+import korlibs.audio.sound.readSound
 import korlibs.image.format.readBitmap
 
 // AUTO-GENERATED FILE! DO NOT MODIFY!
 
-inline class TypedVfsFile<T>(val file: VfsFile)
+inline class TypedVfsFile(val __file: VfsFile)
+inline class TypedVfsFileBitmap(val __file: VfsFile) { suspend fun read(): korlibs.image.bitmap.Bitmap = this.__file.readBitmap() }
+inline class TypedVfsFileSound(val __file: VfsFile) { suspend fun read(): korlibs.audio.sound.Sound = this.__file.readSound() }
 interface TypedAtlas<T>
 
-object KR {
-    val __file get() = resourcesVfs[""]
-    val `gfx` get() = KRGfx
-    val `hello` get() = TypedVfsFile<korlibs.image.bitmap.Bitmap>(resourcesVfs["hello.png"])
-    val `other` get() = KROther
-    val `sfx` get() = KRSfx
+object KR : __KR.KR
+
+object __KR {
+    
+    interface KR {
+        val __file get() = resourcesVfs[""]
+        val `gfx` get() = __KR.KRGfx
+        val `hello` get() = TypedVfsFileBitmap(resourcesVfs["hello.png"])
+        val `other` get() = __KR.KROther
+        val `sfx` get() = __KR.KRSfx
+    }
+    
+    object KRGfx {
+        val __file get() = resourcesVfs["gfx"]
+        val `demo` get() = AtlasGfxDemoAtlas.TypedAtlas(resourcesVfs["gfx/demo.atlas.json"])
+    }
+    
+    object KROther {
+        val __file get() = resourcesVfs["other"]
+        val `file` get() = TypedVfsFile(resourcesVfs["other/file.raw"])
+    }
+    
+    object KRSfx {
+        val __file get() = resourcesVfs["sfx"]
+        val `sound` get() = TypedVfsFileSound(resourcesVfs["sfx/sound.mp3"])
+    }
 }
 
-object KRGfx {
-    val __file get() = resourcesVfs["gfx"]
-    val `demo` get() = TypedVfsFile<TypedAtlas<AtlasGfxDemoAtlas>>(resourcesVfs["gfx/demo.atlas.json"])
-}
-
-object KROther {
-    val __file get() = resourcesVfs["other"]
-    val `file` get() = TypedVfsFile<korlibs.io.file.VfsFile>(resourcesVfs["other/file.raw"])
-}
-
-object KRSfx {
-    val __file get() = resourcesVfs["sfx"]
-    val `sound` get() = TypedVfsFile<korlibs.audio.sound.Sound>(resourcesVfs["sfx/sound.mp3"])
-}
-
-@kotlin.jvm.JvmName("read_TypedVfsFile_TypedAtlas_AtlasGfxDemoAtlas")
-suspend fun TypedVfsFile<TypedAtlas<AtlasGfxDemoAtlas>>.read() = AtlasGfxDemoAtlas(this.file.readAtlas())
 inline class AtlasGfxDemoAtlas(val __atlas: korlibs.image.atlas.Atlas) {
+    inline class TypedAtlas(val __file: VfsFile) { suspend fun read(): AtlasGfxDemoAtlas = AtlasGfxDemoAtlas(this.__file.readAtlas()) }
     val `hello` get() = __atlas["hello.png"]
     val `world` get() = __atlas["world.png"]
 }
-
-@kotlin.jvm.JvmName("read_TypedVfsFile_Bitmap")
-suspend fun TypedVfsFile<korlibs.image.bitmap.Bitmap>.read() = this.file.readBitmap()
-@kotlin.jvm.JvmName("read_TypedVfsFile_Sound")
-suspend fun TypedVfsFile<korlibs.audio.sound.Sound>.read() = this.file.readSound()
 """.trimIndent().trim(),
             generated.trim().replace("\t", "    ")
         )
