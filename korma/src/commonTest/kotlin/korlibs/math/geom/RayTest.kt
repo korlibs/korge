69da234c901bfd716c0d1fbce82752fc856a1bd1
soywiz <soywiz@gmail.com>
2023-05-31 14:32:25 +0200

    Fix tests in JS

--- korma/src/commonTest/kotlin/korlibs/math/geom/RayTest.kt
+++ korma/src/commonTest/kotlin/korlibs/math/geom/RayTest.kt
@@ -8,12 +8,12 @@ class RayTest {
     @Test
     fun testConstructAngle() {
         Ray(Point(1, 1), 0.degrees).also { ray ->
-            assertEquals(Point(1, 1), ray.point)
-            assertEquals(Vector2(1, 0), ray.direction)
+            assertEqualsFloat(Point(1, 1), ray.point)
+            assertEqualsFloat(Vector2(1, 0), ray.direction)
         }
-        Ray(Point(1, 1), 90.degrees).also { ray ->
-            assertEquals(Point(1, 1), ray.point)
-            assertEquals(Vector2(0, 1), ray.direction)
+        Ray(Point(1, 1), Angle.QUARTER).also { ray ->
+            assertEqualsFloat(Point(1, 1), ray.point)
+            assertEqualsFloat(Vector2(0, 1), ray.direction)
         }
     }
 
@@ -31,16 +31,16 @@ class RayTest {
     @Test
     fun testFromTwoPoints() {
         Ray.fromTwoPoints(Point(1, 0), Point(3, 0)).also { ray ->
-            assertEquals(Point(1, 0), ray.point)
-            assertEquals(Vector2(1, 0), ray.direction)
+            assertEqualsFloat(Point(1, 0), ray.point)
+            assertEqualsFloat(Vector2(1, 0), ray.direction)
         }
         Ray.fromTwoPoints(Point(10, 10), Point(10, 7)).also { ray ->
-            assertEquals(Point(10, 10), ray.point)
-            assertEquals(Vector2(0, -1), ray.direction)
+            assertEqualsFloat(Point(10, 10), ray.point)
+            assertEqualsFloat(Vector2(0, -1), ray.direction)
         }
         Ray.fromTwoPoints(Point(1, 1), Point(3, 3)).also { ray ->
-            assertEquals(Point(1, 1), ray.point)
-            assertEquals(Vector2Int(7, 7), (ray.direction * 10).toInt())
+            assertEqualsFloat(Point(1, 1), ray.point)
+            assertEqualsFloat(Vector2Int(7, 7), (ray.direction * 10).toInt())
         }
     }
 }
