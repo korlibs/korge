7a9cab0d936901c44bdc6c377c342b2c66874a15
soywiz <soywiz@gmail.com>
2023-05-31 12:46:03 +0200

    Improves Ray.direction to be normalized. Adds Ray.fromTwoPoints. Adds tests. Code + Website documentation for Ray

--- korma/src/commonMain/kotlin/korlibs/math/geom/Ray.kt
+++ korma/src/commonMain/kotlin/korlibs/math/geom/Ray.kt
@@ -2,18 +2,35 @@ package korlibs.math.geom
 
 import korlibs.memory.pack.*
 
+/** Represents an infinite [Ray] starting at [point] in the specified [direction] with an [angle] */
 inline class Ray(val data: Float4Pack) {
+    companion object {
+        /** Creates a ray starting in [start] and passing by [end] */
+        fun fromTwoPoints(start: Point, end: Point): Ray = Ray(start, end - start)
+    }
+
+    /** Starting point */
     val point: Point get() = Point(data.f0, data.f1)
+    /** Normalized direction of the ray starting at [point] */
     val direction: Vector2 get() = Vector2(data.f2, data.f3)
+    /** Angle between two points */
     val angle: Angle get() = direction.angle
 
-    constructor(point: Point, direction: Vector2) : this(float4PackOf(point.x, point.y, direction.x, direction.y))
-    constructor(point: Point, angle: Angle) : this(point, Vector2.polar(angle))
+    /** Constructs a [Ray] starting from [point] in the specified [direction] */
+    constructor(point: Point, direction: Vector2) : this(point, direction.normalized, Unit)
+    /** Constructs a [Ray] starting from [point] in the specified [angle] */
+    constructor(point: Point, angle: Angle) : this(point, Vector2.polar(angle), Unit)
 
+    private constructor(point: Point, normalizedDirection: Vector2, unit: Unit) : this(float4PackOf(point.x, point.y, normalizedDirection.x, normalizedDirection.y))
+
+    /** Checks if [this] and [other]are equals with an [epsilon] difference */
     fun isAlmostEquals(other: Ray, epsilon: Float = 0.00001f): Boolean =
         this.point.isAlmostEquals(other.point, epsilon) && this.direction.isAlmostEquals(other.direction, epsilon)
 
-    fun transformed(m: Matrix): Ray = Ray(m.transform(point), m.deltaTransform(direction))
+    /** Checks if [this] and [other]are equals with an [epsilon] tolerance */
+    fun transformed(m: Matrix): Ray = Ray(m.transform(point), m.deltaTransform(direction).normalized)
+
+    /** Converts this [Ray] into a [Line] of a specific [length] starting by [point] */
     fun toLine(length: Float = 100000f): Line = Line(point, point + direction * length)
 
     override fun toString(): String = "Ray($point, $angle)"
--- korma/src/commonTest/kotlin/korlibs/math/geom/RayTest.kt
+++ korma/src/commonTest/kotlin/korlibs/math/geom/RayTest.kt
@@ -6,6 +6,18 @@ class RayTest {
     val ray = Ray(Point(100, 100), Vector2(+1, 0))
 
     @Test
+    fun testConstructAngle() {
+        Ray(Point(1, 1), 0.degrees).also { ray ->
+            assertEquals(Point(1, 1), ray.point)
+            assertEquals(Vector2(1, 0), ray.direction)
+        }
+        Ray(Point(1, 1), 90.degrees).also { ray ->
+            assertEquals(Point(1, 1), ray.point)
+            assertEquals(Vector2(0, 1), ray.direction)
+        }
+    }
+
+    @Test
     fun testTransformed() {
         assertEqualsFloat(Ray(Point(200, 100), Vector2(+1, 0)), ray.transformed(Matrix.IDENTITY.translated(100, 0)))
         assertEqualsFloat(Ray(Point(-100, 100), Vector2(0f, +1f)), ray.transformed(Matrix.IDENTITY.rotated(90.degrees)))
@@ -14,5 +26,21 @@ class RayTest {
     @Test
     fun testToLine() {
         assertEqualsFloat(Line(Point(100, 100), Point(200f, 100f)), ray.toLine(100f))
+    }
+
+    @Test
+    fun testFromTwoPoints() {
+        Ray.fromTwoPoints(Point(1, 0), Point(3, 0)).also { ray ->
+            assertEquals(Point(1, 0), ray.point)
+            assertEquals(Vector2(1, 0), ray.direction)
+        }
+        Ray.fromTwoPoints(Point(10, 10), Point(10, 7)).also { ray ->
+            assertEquals(Point(10, 10), ray.point)
+            assertEquals(Vector2(0, -1), ray.direction)
+        }
+        Ray.fromTwoPoints(Point(1, 1), Point(3, 3)).also { ray ->
+            assertEquals(Point(1, 1), ray.point)
+            assertEquals(Vector2Int(7, 7), (ray.direction * 10).toInt())
+        }
     }
 }
