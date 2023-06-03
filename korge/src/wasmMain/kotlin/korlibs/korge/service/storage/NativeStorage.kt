3bcf98d1966fc478a68d8eb065915e211f1cadf9
soywiz <soywiz@gmail.com>
2023-06-03 03:53:31 +0200

    Some fixes

--- korge/src/wasmMain/kotlin/korlibs/korge/service/storage/NativeStorage.kt
+++ korge/src/wasmMain/kotlin/korlibs/korge/service/storage/NativeStorage.kt
@@ -1,34 +1,46 @@
 package korlibs.korge.service.storage
 
 import korlibs.korge.view.*
+import korlibs.memory.*
 import kotlinx.browser.*
 
 @JsName("Object")
 private external object JsObject {
-    fun keys(): JsArray<JsString>
+    fun keys(value: JsAny): JsArray<JsString>
 }
 
-actual class NativeStorage actual constructor(val views: Views) : IStorageWithKeys {
+actual class NativeStorage actual constructor(views: Views) : IStorageWithKeys by WasmNativeStorage(views)
+
+fun WasmNativeStorage(views: Views): IStorageWithKeys {
+    return BrowserNativeStorage(views)
+}
+
+open class BrowserNativeStorage(val views: Views) : IStorageWithKeys {
     override fun toString(): String = "NativeStorage(${toMap()})"
 
-    actual override fun keys(): List<String> {
-        val keys = JsObject.keys()
-        return (0 until keys.length).map { keys[it].toString() }
+    companion object {
+        val PREFIX = "org.korge.storage."
+        fun getKey(key: String): String = "$PREFIX$key"
     }
 
-	actual override fun set(key: String, value: String) {
-		localStorage.setItem(key, value)
-	}
+    override fun keys(): List<String> {
+        val keys = JsObject.keys(localStorage)
+        return (0 until keys.length)
+            .map { keys[it].toString() }
+            .filter { it.startsWith(PREFIX) }
+            .map { it.removePrefix(PREFIX) }
+    }
 
-	actual override fun getOrNull(key: String): String? {
-		return localStorage.getItem(key)
-	}
+    override fun set(key: String, value: String) {
+        localStorage.setItem(getKey(key), value)
+    }
 
-	actual override fun remove(key: String) {
-		localStorage.removeItem(key)
-	}
+    override fun getOrNull(key: String): String? = localStorage.getItem(getKey(key))
 
-	actual override fun removeAll() {
-		localStorage.clear()
-	}
+    override fun remove(key: String) {
+        localStorage.removeItem(getKey(key))
+    }
+
+    //actual override fun removeAll() { localStorage.clear() }
 }
+
