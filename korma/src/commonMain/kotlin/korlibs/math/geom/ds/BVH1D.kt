3d662797f117e0b1e726c3874879632b76868b4c
soywiz <soywiz@gmail.com>
2023-06-12 03:17:33 +0200

    BVH: lots of fixes and improvements

--- korma/src/commonMain/kotlin/korlibs/math/geom/ds/BVH1D.kt
+++ korma/src/commonMain/kotlin/korlibs/math/geom/ds/BVH1D.kt
@@ -4,46 +4,48 @@ import korlibs.datastructure.*
 import korlibs.datastructure.ds.*
 import korlibs.math.geom.*
 
-data class BVH1DSegment(val start: Float, val end: Float) {
-    val size: Float get() = end - start
-}
-data class BVH1DRay(val start: Float, val dir: Float)
-
-fun BVH1DSegment.toBVH(): BVHIntervals = BVHIntervals(start, size)
-fun BVH1DRay.toBVH(): BVHIntervals = BVHIntervals(start, dir)
-fun BVHIntervals.toSegment(): BVH1DSegment = BVH1DSegment(min(0), max(0))
-
 /**
  * A Bounding Volume Hierarchy implementation for 1D.
- * It uses [BVH1DSegment] to describe volumes and [Ray] for raycasting.
+ * It uses [Segment1D] to describe volumes and [Ray] for raycasting.
  */
 open class BVH1D<T>(
     val allowUpdateObjects: Boolean = true
 ) {
     val bvh = BVH<T>(dimensions = 1, allowUpdateObjects = allowUpdateObjects)
 
-    fun intersectRay(ray: BVH1DRay, rect: Rectangle? = null): BVHIntervals? = bvh.intersectRay(ray.toBVH(), rect?.toBVH())
-    fun envelope(): BVH1DSegment = bvh.envelope().toSegment()
+    fun intersectRay(ray: Ray1D, rect: Rectangle? = null): BVHRect? = bvh.intersectRay(ray.toBVH(), rect?.toBVH())
+    fun envelope(): Segment1D = bvh.envelope().toSegment1D()
 
     fun intersect(
-        ray: BVH1DRay,
+        ray: Ray1D,
         returnArray: FastArrayList<BVH.IntersectResult<T>> = fastArrayListOf(),
     ): FastArrayList<BVH.IntersectResult<T>> = bvh.intersect(ray.toBVH(), returnArray)
 
     fun search(
-        segment: BVH1DSegment,
+        segment: Segment1D,
         returnArray: FastArrayList<BVH.Node<T>> = fastArrayListOf(),
     ): FastArrayList<BVH.Node<T>> = bvh.search(intervals = segment.toBVH(), return_array = returnArray)
 
-    fun insertOrUpdate(segment: BVH1DSegment, obj: T) = bvh.insertOrUpdate(segment.toBVH(), obj)
+    fun insertOrUpdate(segment: Segment1D, obj: T) = bvh.insertOrUpdate(segment.toBVH(), obj)
 
-    fun remove(segment: BVH1DSegment, obj: T? = null): FastArrayList<BVH.Node<T>> = bvh.remove(segment.toBVH(), obj = obj)
+    fun remove(segment: Segment1D, obj: T? = null): FastArrayList<BVH.Node<T>> = bvh.remove(segment.toBVH(), obj = obj)
 
     fun remove(obj: T): Unit = bvh.remove(obj)
 
-    fun getObjectBounds(obj: T): BVH1DSegment? = bvh.getObjectBounds(obj)?.toSegment()
+    fun getObjectBounds(obj: T): Segment1D? = bvh.getObjectBoundsRect(obj)?.toSegment1D()
 
     fun debug() {
         bvh.debug()
     }
 }
+
+data class Segment1D(val start: Float, val end: Float) {
+    val size: Float get() = end - start
+}
+data class Ray1D(val start: Float, val dir: Float)
+
+fun Segment1D.toBVH(): BVHRect = BVHRect(BVHIntervals(start, size))
+fun Ray1D.toBVH(): BVHRay = BVHRay(BVHIntervals(start, dir))
+
+fun BVHRect.toSegment1D(): Segment1D = Segment1D(min(0), max(0))
+fun BVHRay.toRay1D(): Ray1D = Ray1D(pos(0), dir(0))
