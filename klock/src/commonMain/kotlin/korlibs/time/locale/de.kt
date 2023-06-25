5577c2be1a995c1b93aac3da6cc4b0b9d6025026
Bantolomeus <christian.bantel@gmail.com>
2023-06-25 14:20:26 +0200

    Better with array

--- klock/src/commonMain/kotlin/korlibs/time/locale/de.kt
+++ klock/src/commonMain/kotlin/korlibs/time/locale/de.kt
@@ -7,16 +7,51 @@ import korlibs.time.DayOfWeek
 val KlockLocale.Companion.german get() = GermanKlockLocale
 
 open class GermanKlockLocale : KlockLocale() {
-	companion object : GermanKlockLocale()
+
+    companion object {
+        private val NUM_TO_ORDINAL: Array<String> = arrayOf(
+            "index zero is invalid",
+            "erste",
+            "zweite",
+            "dritte",
+            "vierte",
+            "fünfte",
+            "sechste",
+            "siebte",
+            "achte",
+            "neunte",
+            "zehnte",
+            "elfte",
+            "zwölfte",
+            "dreizehnte",
+            "vierzehnte",
+            "fünfzehnte",
+            "sechzehnte",
+            "siebzehnte",
+            "achtzehnte",
+            "neunzehnte",
+            "zwanzigste",
+            "einundzwanzigste",
+            "zweiundzwanzigste",
+            "dreiundzwanzigste",
+            "vierundzwanzigste",
+            "fünfundzwanzigste",
+            "sechsundzwanzigste",
+            "siebenundzwanzigste",
+            "achtundzwanzigste",
+            "neunundzwanzigste",
+            "dreißigste",
+            "einunddreißigste"
+        )
+        private val ORDINAL_TO_NUM: Map<String, Int> = NUM_TO_ORDINAL.mapIndexed { index, s -> s to index }.toMap()
+    }
 
     override fun getOrdinalByDay(day: Int, context: KlockLocaleContext): String {
-        val ordinal = GermanOrdinal.values().find { it.value == day }
-        return ordinal?.getOrdinalInGerman() ?: "Invalid number"
+        return NUM_TO_ORDINAL.getOrNull(day) ?: error("Invalid numeral")
     }
 
     override fun getDayByOrdinal(ordinal: String): Int {
-        val ordinalFromEnum = GermanOrdinal.values().find { it.name.equals(ordinal, ignoreCase = true) }
-        return ordinalFromEnum?.value ?: -1
+        return ORDINAL_TO_NUM[ordinal.lowercase()] ?: -1
     }
 
 	override val ISO639_1 = "de"
