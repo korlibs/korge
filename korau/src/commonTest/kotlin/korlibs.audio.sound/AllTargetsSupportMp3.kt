ece378a393f538f4902fda60392ffc19aa0acbc9
soywiz <soywiz@gmail.com>
2023-07-01 03:57:21 +0200

    Fixes MP3 identifying non-mp3 files as such

--- korau/src/commonTest/kotlin/korlibs.audio.sound/AllTargetsSupportMp3.kt
+++ korau/src/commonTest/kotlin/korlibs.audio.sound/AllTargetsSupportMp3.kt
@@ -3,11 +3,18 @@ package korlibs.audio.sound
 import korlibs.io.async.suspendTest
 import korlibs.io.file.std.resourcesVfs
 import doIOTest
-import kotlin.test.Test
+import korlibs.audio.format.*
+import kotlin.test.*
 
 class AllTargetsSupportMp3 {
     @Test
     fun testDecode() = suspendTest({ doIOTest }) {
         val data = resourcesVfs["mp31.mp3"].readSound().decode()
+    }
+
+    @Test
+    fun testIsMp3() = suspendTest({ doIOTest }) {
+        assertNotNull(MP3.tryReadInfo(resourcesVfs["8Khz-Mono.mp3"].open()))
+        assertNull(MP3.tryReadInfo(resourcesVfs["8Khz-Mono.opus"].open()))
     }
 }
