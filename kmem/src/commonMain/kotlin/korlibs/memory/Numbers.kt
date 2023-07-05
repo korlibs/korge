034a33f1feff56c3a9618b10261968832d801ca8
soywiz <soywiz@gmail.com>
2023-07-05 11:55:44 +0200

    Add FixedLong

--- kmem/src/commonMain/kotlin/korlibs/memory/Numbers.kt
+++ kmem/src/commonMain/kotlin/korlibs/memory/Numbers.kt
@@ -29,6 +29,11 @@ public fun Float.toIntRound(): Int = round(this).toInt()
 /** Converts [this] into [Int] rounding to the nearest */
 public fun Double.toIntRound(): Int = round(this).toInt()
 
+/** Converts [this] into [Int] rounding to the nearest */
+public fun Float.toLongRound(): Long = round(this).toLong()
+/** Converts [this] into [Int] rounding to the nearest */
+public fun Double.toLongRound(): Long = round(this).toLong()
+
 /** Convert this [Long] into an [Int] but throws an [IllegalArgumentException] in the case that operation would produce an overflow */
 public fun Long.toIntSafe(): Int = if (this in Int.MIN_VALUE.toLong()..Int.MAX_VALUE.toLong()) this.toInt() else throw IllegalArgumentException("Long doesn't fit Integer")
 
