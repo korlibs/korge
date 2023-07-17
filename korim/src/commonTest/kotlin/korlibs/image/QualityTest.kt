6645bb44684e80b4713c6af2a6bdcf6890bb1dfc
soywiz <soywiz@gmail.com>
2023-07-17 13:14:52 +0200

    Fix QualityTest in JVM and JS

--- korim/src/commonTest/kotlin/korlibs/image/QualityTest.kt
+++ korim/src/commonTest/kotlin/korlibs/image/QualityTest.kt
@@ -6,17 +6,17 @@ import kotlin.test.*
 class QualityTest {
     @Test
     fun testLevels() {
-        fun Quality.result(): String = "$this: ${level.niceStr}: $isLow, $isMedium, $isHigh : ${this <= Quality.MEDIUM}, ${this >= Quality.MEDIUM}"
+        fun Quality.result(): String = "$this: ${level.niceStr(2, zeroSuffix = true)}: $isLow, $isMedium, $isHigh : ${this <= Quality.MEDIUM}, ${this >= Quality.MEDIUM}"
 
         assertEquals(
             """
-                LOWEST: 0: true, false, false : true, false
+                LOWEST: 0.0: true, false, false : true, false
                 CUSTOM1: 0.1: true, false, false : true, false
                 LOW: 0.25: true, false, false : true, false
                 MEDIUM: 0.5: false, true, false : true, true
                 HIGH: 0.75: false, false, true : false, true
                 CUSTOM9: 0.9: false, false, true : false, true
-                HIGHEST: 1: false, false, true : false, true
+                HIGHEST: 1.0: false, false, true : false, true
             """.trimIndent(),
             """
                 ${Quality.LOWEST.result()}
