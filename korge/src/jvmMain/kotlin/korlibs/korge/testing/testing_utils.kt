37e5917e6b4207635ea4e5c04aae6fb8c47ba791
Kiet <5054996+Kietyo@users.noreply.github.com>
2023-08-02 20:34:08 -0700

    Revive Korge screenshot tester with Korge UI. (#1817)
    * bring back korge screenshot tester
    
    * Korge screenshot tester with Korge UI.
    
    * Revert grayscale jpg fix for now.
    
    * Revert unnecessary gradle change.

---
+++ korge/src/jvmMain/kotlin/korlibs/korge/testing/testing_utils.kt
@@ -0,0 +1,169 @@
+package korlibs.korge.testing
+
+import korlibs.image.bitmap.*
+import korlibs.image.color.*
+import korlibs.io.async.*
+import korlibs.korge.*
+import korlibs.korge.annotations.*
+import korlibs.korge.input.*
+import korlibs.korge.ui.*
+import korlibs.korge.view.*
+import korlibs.korge.view.Container
+import korlibs.korge.view.align.*
+import korlibs.korge.view.onClick
+import korlibs.math.geom.*
+import kotlinx.coroutines.sync.*
+import java.awt.*
+
+@OptIn(KorgeExperimental::class)
+inline fun korgeScreenshotTestV2(
+    korgeConfig: Korge,
+    settings: KorgeScreenshotValidationSettings = KorgeScreenshotValidationSettings(),
+    crossinline callback: suspend Stage.(korgeScreenshotTester: KorgeScreenshotTester) -> Unit = {},
+) {
+    System.setProperty("java.awt.headless", "false")
+    val throwable = Throwable()
+    val testClassName = throwable.stackTrace[0].className
+    val testMethodName = throwable.stackTrace[0].methodName
+    val existingMain = korgeConfig.main
+    val results = KorgeScreenshotTestResults(testMethodName)
+    // Keep locked while tester is running
+    val testingLock = Mutex(locked = true)
+    val context = KorgeScreenshotTestingContext(testClassName, testMethodName)
+    val finalKorgeConfig = korgeConfig.copy(main = {
+        context.init()
+        val korgeScreenshotTester =
+            KorgeScreenshotTester(views, context, settings, testingLock, results)
+        // If there already exists a main, run that first.
+        // This allows people to test their existing modules.
+        existingMain.invoke(this)
+        callback(this, korgeScreenshotTester)
+    })
+    suspendTest {
+        try {
+            finalKorgeConfig.start( entry = finalKorgeConfig.main)
+        } catch (exception: HeadlessException) {
+            // Running in a headless environment (e.g github tests).
+            // Return to mark as passing.
+            return@suspendTest
+        }
+
+        while (testingLock.isLocked) {
+            println("Waiting for test to end...")
+        }
+
+        val resultsWithErrors = results.results.filter { result ->
+            result.hasValidationErrors
+        }
+
+        // No diffs, no need to show UI to update goldens.
+        if (resultsWithErrors.isEmpty()) return@suspendTest
+
+        val config = Korge(
+            backgroundColor = Colors.LIGHTGRAY,
+            windowSize = Size(1280, 720),
+            virtualSize = Size(700, 480),
+            main = {
+                views.gameWindow.exitProcessOnExit = false
+
+                uiScrollable(size = Size(700.0, 480.0)) { uiScrollable ->
+                    uiScrollable.backgroundColor = Colors.LIGHTGRAY
+                    var prevContainer: Container? = null
+                    resultsWithErrors.forEach { testResult ->
+                        val viewsToAlign = mutableListOf<View>()
+                        container {
+                            viewsToAlign += text("Test method name: ${results.testMethodName}")
+                            //                            val testResultSection = container {
+                            viewsToAlign += text("Golden name: ${testResult.goldenName}")
+                            viewsToAlign += container {
+                                val fn = { headerText: String, bitmap: Bitmap? ->
+                                    container {
+                                        val headerText = if (bitmap == null) {
+                                            text("$headerText")
+                                        } else {
+                                            text("$headerText (${bitmap.size.width.toInt()} x ${bitmap.size.height.toInt()})")
+                                        }
+
+                                        val rect =
+                                            solidRect(
+                                                320,
+                                                240,
+                                                color = Colors.BLACK.withAd(0.75)
+                                            ) {
+                                                alignTopToBottomOf(headerText)
+                                            }
+                                        if (bitmap == null) {
+                                            text("Deleted") {
+                                                centerOn(rect)
+                                            }
+                                        } else {
+                                            image(bitmap).apply {
+                                                scaleWhileMaintainingAspect(
+                                                    ScalingOption.ByWidthAndHeight(
+                                                        310.0,
+                                                        230.0
+                                                    )
+                                                )
+                                                centerOn(rect)
+                                            }
+                                        }
+
+                                    }
+                                }
+                                val oldImage = fn("Old Image", testResult.oldBitmap)
+                                val newImage =
+                                    fn("New Image", testResult.newBitmap).apply {
+                                        alignLeftToRightOf(oldImage, padding = 5.0)
+                                    }
+                            }
+
+                            val separator = "\n * "
+                            viewsToAlign += container {
+                                val textResultString = testResult.validationErrors.joinToString(separator=separator, prefix=separator) {
+                                    it.errorMessaage
+                                }
+                                text("Validation errors:$textResultString")
+                            }
+
+                            viewsToAlign += uiButton("Accept change?") {
+                                centerXOn(this@container)
+                                onClick {
+                                    val goldenFileNameWithExt =
+                                        context.makeGoldenFileNameWithExtension(testResult.goldenName)
+                                    if (testResult.oldBitmap != null && testResult.newBitmap != null) {
+                                        context.tempGoldensVfs[goldenFileNameWithExt].copyTo(
+                                            context.testGoldensVfs[goldenFileNameWithExt]
+                                        )
+                                    } else if (testResult.oldBitmap != null && testResult.newBitmap == null) {
+                                        // Bitmap was deleted
+                                        context.testGoldensVfs[goldenFileNameWithExt].delete()
+                                    }
+                                    disable()
+                                }
+                            }
+
+                            viewsToAlign.windowed(2) {
+                                it[1].alignTopToBottomOf(it[0])
+                            }
+
+
+                            val sectionBg =
+                                solidRect(scaledWidth, scaledHeight, Colors.DARKSLATEGRAY)
+                            sendChildToBack(sectionBg)
+
+                            centerXOnStage()
+
+                            if (prevContainer != null) {
+                                alignTopToBottomOf(prevContainer!!, padding = 10.0)
+                            }
+                            prevContainer = this
+                        }
+
+                    }
+                }
+
+            })
+        config.start()
+
+    }
+}
--- korge/src/jvmTest/kotlin/korlibs/korge/testing/KorgeScreenshotTest.kt
+++ korge/src/jvmTest/kotlin/korlibs/korge/testing/KorgeScreenshotTest.kt
@@ -2,6 +2,7 @@ package korlibs.korge.testing
 
 import korlibs.image.bitmap.*
 import korlibs.image.color.*
+import korlibs.korge.*
 import korlibs.korge.view.*
 import korlibs.math.geom.*
 import org.junit.*
@@ -131,5 +132,57 @@ class KorgeScreenshotTest {
         image(bmp2).scale(0.125).xy(16, 0)
 
         assertScreenshot(posterize = 4)
+    }
+
+    val DIFF_BY_PIXELS_SETTINGS = KorgeScreenshotValidationSettings(
+        listOf(AbsolutePixelDifferenceValidator(1000))
+    )
+
+    @Test
+    fun test12() = korgeScreenshotTestV2(
+        Korge(
+            windowSize = Size(512, 512),
+            virtualSize = Size(512, 512),
+            backgroundColor = Colors.LIGHTGRAY
+        ),
+    ) {
+        val maxDegrees = (+16).degrees
+
+        val rect1 = solidRect(100, 100, Colors.RED) {
+            rotation = maxDegrees
+            anchor(.5, .5)
+            scale = Scale(0.8)
+            position(200, 200)
+        }
+
+        it.recordGolden(this, "initial1")
+
+        val rect2 = solidRect(150, 150, Colors.YELLOW) {
+            rotation = maxDegrees
+            anchor(.5, .5)
+            scale = Scale(0.8)
+            position(350, 350)
+        }
+
+        it.recordGolden(rect2, "initial2")
+
+        val rect3 = solidRect(150, 150, Colors.GREEN) {
+            rotation = maxDegrees
+            anchor(.5, .5)
+            scale = Scale(0.8)
+            position(100, 350)
+        }
+
+        it.recordGolden(this, "initial3")
+
+        val rectContainer = container {
+            val a = 100
+            solidRect(a, a, Colors.BROWN)
+            solidRect(a / 2, a / 2, Colors.YELLOW)
+        }
+
+        it.recordGolden(rectContainer, "initial4", DIFF_BY_PIXELS_SETTINGS)
+
+        it.endTest()
     }
 }
