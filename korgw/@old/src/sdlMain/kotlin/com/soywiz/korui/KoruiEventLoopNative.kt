8f3c124db10ca562713487970c7dc5860bee01b0
Alexandre Mommers <none@ygdrasil.io>
2023-08-08 01:33:03 +0200

    remove unused code

--- korgw/@old/src/sdlMain/kotlin/com/soywiz/korui/KoruiEventLoopNative.kt
+++
@@ -1,9 +0,0 @@
-package com.soywiz.korui
-
-import korlibs.io.async.*
-import com.soywiz.korui.light.*
-import korlibs.io.*
-
-actual object KoruiEventLoop {
-	actual fun create(): EventLoop = BaseEventLoopNative()
-}
