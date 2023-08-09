1887888b5dc3050b92fe72d8d0afe9cccff52503
Kiet <5054996+Kietyo@users.noreply.github.com>
2023-08-09 00:09:03 -0700

    Convert buildSrc to using kts (#1830)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,14 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.File
+import java.io.StringReader
+import java.util.*
+
+var File.text
+    get() = this.readText();
+    set(value) {
+        this.also { it.parentFile.mkdirs() }.writeText(value)
+    }
+
 plugins {
-    id "publishing"
-    id "maven-publish"
-    id "signing"
-    id "org.jetbrains.kotlin.jvm" version libs.versions.kotlin
+    id("publishing")
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.kotlin.jvm") version libs.versions.kotlin
 }
 
-def isJava8or9 = System.getProperty("java.version").startsWith("1.8") || System.getProperty("java.version").startsWith("9")
+val isJava8or9 =
+    System.getProperty("java.version").startsWith("1.8") || System.getProperty("java.version")
+        .startsWith("9")
 
 if (isJava8or9) {
-    throw new Exception("At least Java 11 is required")
+    throw Exception("At least Java 11 is required")
 }
 
 dependencies {
@@ -23,57 +36,72 @@ dependencies {
     testImplementation(libs.junit)
 }
 
-Eval.xy(this, this, file("../gradle/repositories.settings.gradle").text)
+//Eval.xy(this, this, file("../gradle/repositories.settings.gradle").text)
+repositories {
+    mavenLocal()
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/temporary") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+}
 
-tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).all {
+tasks.withType(KotlinCompile::class).all {
     kotlinOptions.suppressWarnings = true
 }
 
-tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile.class).configureEach {
+tasks.withType(KotlinCompile::class).configureEach {
     kotlinOptions {
-        //jvmTarget = "1.8"
         jvmTarget = "11"
     }
 }
 
 java {
-    def jversion = JavaVersion.VERSION_11
+    val jversion = JavaVersion.VERSION_11
     sourceCompatibility = jversion
     targetCompatibility = jversion
-    sourceSets.main.java {
+    sourceSets.getByName("main").java {
         srcDir("src/main/kotlinGen")
     }
 }
 
+
 // Build versions
 
-String gitVersion = "unset"
-
+var gitVersion = "unset"
 try {
     gitVersion = file("../.git/ORIG_HEAD").text.trim()
-} catch (Throwable e) {
+} catch (e: Throwable) {
 }
 
 if (System.getenv("FORCED_VERSION") != null) {
     try {
-        gitVersion = Runtime.getRuntime().exec("git describe --abbrev=8 --tags --dirty", new String[0], rootDir).inputStream.text.trim()
-    } catch (Throwable e) {
+        gitVersion = Runtime.getRuntime().exec(
+            "git describe --abbrev=8 --tags --dirty",
+            arrayOf(),
+            rootDir
+        ).inputStream.readBytes().toString().trim()
+    } catch (e: Throwable) {
         System.err.println(e.message)
     }
 }
 
-Properties props = new Properties()
-props.load(new StringReader(file("../gradle.properties").text))
+val props = Properties()
+props.load(StringReader(file("../gradle.properties").text))
 
-String projectVersion = System.getenv("FORCED_VERSION")
-    ?.replaceFirst(~"^refs/tags/", "")
-    ?.replaceFirst(~"^v", "")
-    ?.replaceFirst(~"^w", "")
-    ?.replaceFirst(~"^z", "")
-    ?: props.version
+var projectVersion = System.getenv("FORCED_VERSION")
+    ?.replaceFirst("^refs/tags/", "")
+    ?.replaceFirst("^v", "")
+    ?.replaceFirst("^w", "")
+    ?.replaceFirst("^z", "")
+    ?: props.getProperty("version")
 
 if (projectVersion.contains("-only-gradle-plugin-")) {
-    def parts = projectVersion.split("-only-gradle-plugin-")
+    val parts = projectVersion.split("-only-gradle-plugin-")
     projectVersion = parts.last()
 }
 
@@ -82,9 +110,10 @@ if (System.getenv("FORCED_VERSION") != null) {
     println(":: projectVersion=$projectVersion")
 }
 
-String realKotlinVersion = System.getenv("FORCED_KOTLIN_VERSION") ?: libs.versions.kotlin.get()
+val realKotlinVersion = System.getenv("FORCED_KOTLIN_VERSION")
+    ?: libs.versions.kotlin.get()
 
-String buildVersionsString = """
+val buildVersionsString = """
 package korlibs.korge.gradle
 
 object BuildVersions {
@@ -113,14 +142,16 @@ object BuildVersions {
 }
 """.trim()
 
-String buildVersionsStringForBuildSrc = buildVersionsString
-String buildVersionsStringForPlugin = buildVersionsString.replace(
+val buildVersionsStringForBuildSrc = buildVersionsString
+val buildVersionsStringForPlugin = buildVersionsString.replace(
     "const val GIT = \"---\"",
     "const val GIT = \"${gitVersion}\""
 )
 
-File buildsVersionBuildSrcFile = file("../buildSrc/src/main/kotlinGen/korlibs/korge/gradle/BuildVersions.kt")
-File buildsVersionFilePlugin = file("../korge-gradle-plugin/build/srcgen/korlibs/korge/gradle/BuildVersions.kt")
+val buildsVersionBuildSrcFile =
+    file("../buildSrc/src/main/kotlinGen/korlibs/korge/gradle/BuildVersions.kt")
+val buildsVersionFilePlugin =
+    file("../korge-gradle-plugin/build/srcgen/korlibs/korge/gradle/BuildVersions.kt")
 
 if (!buildsVersionBuildSrcFile.exists() || buildsVersionBuildSrcFile.text != buildVersionsStringForBuildSrc) {
     buildsVersionBuildSrcFile.parentFile.mkdirs()
