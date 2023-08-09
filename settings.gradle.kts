f340febbbebdca1d4373942fb72881c766c64005
Kiet <5054996+Kietyo@users.noreply.github.com>
2023-08-09 00:09:03 -0700

    Convert buildSrc to using kts (#1830)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 pluginManagement {
-//    Eval.xy(this, it, file('./gradle/repositories.settings.gradle').text)
+    //    Eval.xy(this, it, file('./gradle/repositories.settings.gradle').text)
     repositories {
-    mavenLocal()
-    mavenCentral()
-    google()
-    gradlePluginPortal()
-    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap") }
-    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/temporary") }
-    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven") }
-    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental") }
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
-}
+        mavenLocal()
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/temporary") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    }
 }
 
 val enableMetalPlayground: String by settings
