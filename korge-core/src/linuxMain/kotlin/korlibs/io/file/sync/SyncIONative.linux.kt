16b5c4f1af0ff4d1fc0d73dbc502f633a9219f9f
soywiz <soywiz@gmail.com>
2023-09-14 19:23:34 +0200

    Try to implement syncExecNative on linux

--- korge-core/src/linuxMain/kotlin/korlibs/io/file/sync/SyncIONative.linux.kt
+++ korge-core/src/linuxMain/kotlin/korlibs/io/file/sync/SyncIONative.linux.kt
@@ -1,94 +1,101 @@
 package korlibs.io.file.sync
 
-import korlibs.io.file.std.*
+import korlibs.io.stream.*
 import kotlinx.cinterop.*
 import platform.posix.*
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual fun syncExecNative(
     commands: List<String>,
     envs: Map<String, String>,
     cwd: String
 ): SyncExecProcess {
-    TODO("Not yet implemented")
-}
+    val mem = Arena()
+    val pipeStdin = mem.allocArray<IntVar>(2)
+    val pipeStdout = mem.allocArray<IntVar>(2)
+    val pipeStderr = mem.allocArray<IntVar>(2)
 
-/*
-@OptIn(ExperimentalForeignApi::class)
-actual suspend fun posixExec(
-    path: String, cmdAndArgs: List<String>, env: Map<String, String>, handler: VfsProcessHandler
-): Int = memScoped {
-    val fd = alloc<fd_set>()
-    val timeval = alloc<timeval>()
-    timeval.tv_sec = 0
-    timeval.tv_usec = 1
-    val (f, pid) = sopen(*cmdAndArgs.toTypedArray(), cwd = path, envs = env)
-    val fn = fileno(f)
-    val bufSize = 10240
-    ByteArray(bufSize).usePinned { tmp ->
-        val bufArray = tmp.get()
-        val buf = tmp.startAddressOf
-        loop@while (true) {
-            while (true) {
-                posix_FD_SET(fn, fd.ptr)
-                select(fileno(f) + 1, fd.ptr, null, null, timeval.ptr)
-                if (posix_FD_ISSET(fn, fd.ptr) != 0) {
-                    val res = read(fn, buf, bufSize.convert()).toInt()
-                    if (res <= 0) break@loop
-                    handler.onOut(bufArray.copyOf(res))
-                } else {
-                    break // No more data available
+    // Create pipes
+    if (pipe(pipeStdin) == -1 || pipe(pipeStdout) == -1 || pipe(pipeStderr) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    val childPid = fork()
+    if (childPid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (childPid == 0) {  // Child process
+        // Redirect stdin, stdout, and stderr to the pipes
+        dup2(pipeStdin[0], STDIN_FILENO);
+        dup2(pipeStdout[1], STDOUT_FILENO);
+        dup2(pipeStderr[1], STDERR_FILENO);
+
+        // Close unused pipe ends
+        close(pipeStdin[1]);
+        close(pipeStdout[0]);
+        close(pipeStderr[0]);
+
+        // Execute desired program
+
+        execlp(commands[0], commands.getOrNull(1), *commands.drop(minOf(commands.size, 2)).toTypedArray(), null);  // For example, run 'cat'
+
+        // If execlp fails
+        perror("execlp");
+        exit(EXIT_FAILURE);
+    }
+    close(pipeStdin[0])
+    close(pipeStdout[1])
+    close(pipeStderr[1])
+
+    fun toSyncInputStream(__fd: Int): SyncInputStream {
+        return object : SyncInputStream {
+            override fun read(buffer: ByteArray, offset: Int, len: Int): Int {
+                if (len == 0) return 0
+                val read = buffer.usePinned {
+                    platform.posix.read(__fd, it.addressOf(offset), len.convert())
+                }
+                return read.toInt()
+            }
+        }
+    }
+
+    fun toSyncOutputStream(__fd: Int): SyncOutputStream {
+        return object : SyncOutputStream {
+            override fun write(buffer: ByteArray, offset: Int, len: Int) {
+                if (len == 0) return
+                buffer.usePinned {
+                    platform.posix.write(__fd, it.addressOf(offset), len.convert())
                 }
             }
-            delay(1.milliseconds)
         }
     }
-    val status = alloc<IntVar>()
-    waitpid(pid.convert(), status.ptr, 0.convert())
-    status.value
-}
-*/
 
-@OptIn(ExperimentalForeignApi::class)
-fun sopen(vararg cmds: String, cwd: String, envs: Map<String, String> = mapOf()): Pair<CPointer<FILE>?, Long> = memScoped {
-    val fds = allocArray<IntVar>(2)
-    if (socketpair(AF_UNIX, SOCK_STREAM, 0, fds) < 0) {
-        return null to 0L
-    }
-    val rcmd = ShellArgs.buildShellExecCommandLineArrayForExecl(cmds.toList())
-    //val rcmd = listOf("/bin/sh", "-c", "\"'echo' 'hello world'\"")
-    //val rcmd = listOf("/bin/sh", "-c", "'echo' 'hello world'")
+    return object : SyncExecProcess(
+        toSyncOutputStream(pipeStdin[1]),
+        toSyncInputStream(pipeStdout[0]),
+        toSyncInputStream(pipeStderr[0]),
+    ) {
+        override val exitCode: Int get() = memScoped {
+            val status = alloc<IntVar>()
+            waitpid(childPid, status.ptr, SIGTERM)
+            return status.value
+        }
 
-    //println("rcmd=$rcmd")
-    val command = rcmd.first()
-    //val args = rcmd.drop(1)
-    val args = rcmd
-    //println("rcmd=$rcmd")
-    val pid = fork()
-    when (pid) {
-        -1 -> {
-            close(fds[0])
-            close(fds[1])
-            return null to 0L
+        override fun destroy() {
+            kill(childPid, SIGTERM)
         }
-        0 -> { // child
-            //printf("CHILD!\n");
-            close(fds[0])
-            dup2(fds[0], STDIN_FILENO)
-            dup2(fds[1], STDOUT_FILENO)
-            close(fds[1])
-            chdir(cwd)
-            for ((k ,v) in envs) putenv("$k=$v".cstr)
-            memScoped {
-                val vargs = allocArray<CPointerVar<ByteVar>>(args.size + 1)
-                for (n in args.indices) vargs[n] = args[n].cstr.getPointer(this)
-                vargs[args.size] = null
-                execv(command, vargs)
-                _exit(127)
-            }
+
+        private var closed = false
+        override fun close() {
+            if (closed) return
+            closed = true
+            close(pipeStdin[1])
+            close(pipeStdout[0])
+            close(pipeStderr[0])
+            mem.clear()
         }
     }
-    //printf("GO!\n");
-    /* parent */
-    close(fds[1])
-    return fdopen(fds[0], "r+") to pid.toLong()
 }
