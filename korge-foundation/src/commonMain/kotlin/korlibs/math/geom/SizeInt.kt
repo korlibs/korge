7d681e6117d308f4795c23fcaea44c5c4dd5c78a
soywiz <soywiz@gmail.com>
2023-09-18 14:04:35 +0200

    Remove some packs from memory/pack

--- korge-foundation/src/commonMain/kotlin/korlibs/math/geom/SizeInt.kt
+++ korge-foundation/src/commonMain/kotlin/korlibs/math/geom/SizeInt.kt
@@ -1,23 +1,14 @@
 package korlibs.math.geom
 
-import korlibs.memory.pack.*
-
-inline class SizeInt internal constructor(internal val raw: Int2) {
-    operator fun component1(): Int = width
-    operator fun component2(): Int = height
-
+data class SizeInt(val width: Int, val height: Int) {
     fun avgComponent(): Int = (width + height) / 2
     fun minComponent(): Int = kotlin.math.min(width, height)
     fun maxComponent(): Int = kotlin.math.max(width, height)
-
-    val width: Int get() = raw.i0
-    val height: Int get() = raw.i1
 
     val area: Int get() = width * height
     val perimeter: Int get() = width * 2 + height * 2
 
     constructor() : this(0, 0)
-    constructor(width: Int, height: Int) : this(int2PackOf(width, height))
 
 
     operator fun unaryMinus(): SizeInt = SizeInt(-width, -height)
