f812e7fe946207b36936898bb198e7869bc4876a
soywiz <soywiz@gmail.com>
2023-09-21 17:09:21 +0200

    Add nativeThread { } function symmetric to thread { }

--- korge-foundation/src/commonMain/kotlin/korlibs/datastructure/_Datastructure_thread.kt
+++ korge-foundation/src/commonMain/kotlin/korlibs/datastructure/_Datastructure_thread.kt
@@ -7,3 +7,19 @@ expect class NativeThread(code: () -> Unit) {
     fun start(): Unit
     fun interrupt(): Unit
 }
+
+public fun nativeThread(
+    start: Boolean = true,
+    isDaemon: Boolean = false,
+    //name: String? = null,
+    //priority: Int = -1,
+    block: () -> Unit
+): NativeThread {
+    val thread = NativeThread(block)
+    if (isDaemon) thread.isDaemon = true
+    // if (priority > 0) thread.priority = priority
+    // if (name != null) thread.name = name
+    // if (contextClassLoader != null) thread.contextClassLoader = contextClassLoader
+    if (start) thread.start()
+    return thread
+}
