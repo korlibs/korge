8e1684eda2b28571861e192255bbe5a0ed08c234
soywiz <soywiz@gmail.com>
2023-09-25 20:20:49 +0200

    Skipping WEBPTest for now on iOS

--- korge-core/src/commonTest/kotlin/korlibs/image/format/WEBPTest.kt
+++ korge-core/src/commonTest/kotlin/korlibs/image/format/WEBPTest.kt
@@ -2,11 +2,16 @@ package korlibs.image.format
 
 import korlibs.io.async.*
 import korlibs.io.file.std.*
+import korlibs.platform.*
 import kotlin.test.*
 
 class WEBPTest {
     @Test
     fun test1() = suspendTest {
+        if (Platform.isIos || Platform.isTvos) {
+            println("Skipping WEBPTest for now on iOS")
+            return@suspendTest
+        }
         WEBP()
         val bmp = resourcesVfs["Exif5-2x.webp"].readBitmap(ImageDecodingProps(format = WEBP(), preferKotlinDecoder = true))
         assertEquals("256x256", "${bmp.size}")
@@ -14,6 +19,10 @@ class WEBPTest {
 
     @Test
     fun test2() = suspendTest {
+        if (Platform.isIos || Platform.isTvos) {
+            println("Skipping WEBPTest for now on iOS")
+            return@suspendTest
+        }
         WEBP()
         val bmp = WEBP().decode(resourcesVfs["Exif5-2x.webp"])
         assertEquals("256x256", "${bmp.size}")
