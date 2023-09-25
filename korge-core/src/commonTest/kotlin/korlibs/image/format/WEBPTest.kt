e2fce53dd42c1e418eff2ad890c641b06e39bb9b
soywiz <soywiz@gmail.com>
2023-09-25 11:43:43 +0200

    Try to fix decoding

--- korge-core/src/commonTest/kotlin/korlibs/image/format/WEBPTest.kt
+++ korge-core/src/commonTest/kotlin/korlibs/image/format/WEBPTest.kt
@@ -10,7 +10,7 @@ class WEBPTest {
     @Test
     fun testPremultiplied() = suspendTest {
         WEBP.initOnce(coroutineContext)
-        val bmp = resourcesVfs["Exif5-2x.webp"].readNativeImage(WEBPDecoder)
+        val bmp = resourcesVfs["Exif5-2x.webp"].readBitmap(WEBPDecoder)
         assertEquals("256x256", "${bmp.size}")
     }
 }
