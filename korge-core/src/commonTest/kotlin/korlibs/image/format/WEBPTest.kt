8928b5c10c0792069badfb87451ac58d216f62ee
soywiz <soywiz@gmail.com>
2023-09-25 11:27:51 +0200

    Add WEBP decoding

---
+++ korge-core/src/commonTest/kotlin/korlibs/image/format/WEBPTest.kt
@@ -0,0 +1,16 @@
+package korlibs.image.format
+
+import korlibs.io.async.*
+import korlibs.io.file.std.*
+import kotlin.test.*
+
+class WEBPTest {
+    val WEBPDecoder = ImageDecodingProps(format = WEBP)
+
+    @Test
+    fun testPremultiplied() = suspendTest {
+        WEBP.initOnce(coroutineContext)
+        val bmp = resourcesVfs["Exif5-2x.webp"].readNativeImage(WEBPDecoder)
+        assertEquals("256x256", "${bmp.size}")
+    }
+}
