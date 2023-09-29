1292d121136460566365d3168d9a7e29a3152f33
raV <rafalziemniewski@wp.pl>
2023-09-29 22:42:24 +0200

    used Mesh for implementation

--- korge/src/common/korlibs/korge/view/SolidTriangle.kt
+++ korge/src/common/korlibs/korge/view/SolidTriangle.kt
@@ -5,19 +5,21 @@ import korlibs.image.color.*
 import korlibs.korge.render.*
 import korlibs.math.geom.*
 import korlibs.math.geom.vector.*
+import korlibs.memory.*
 
-/** Creates a new [SolidTriangle] of [size] and color [color] and allows you to configure it via [callback]. Once created, it is added to this receiver [Container]. */
-inline fun Container.solidTriangle(size: Size, color: RGBA = Colors.WHITE, callback: @ViewDslMarker SolidTriangle.() -> Unit = {}) = SolidTriangle(size, color).addTo(this, callback)
-
-/** Creates a new [SolidTriangle] of size [width]x[height] and color [color] and allows you to configure it via [callback]. Once created, it is added to this receiver [Container]. */
-inline fun Container.solidTriangle(width: Number, height: Number, color: RGBA = Colors.WHITE, callback: @ViewDslMarker SolidTriangle.() -> Unit = {}) = SolidTriangle(Size(width, height), color).addTo(this, callback)
-
-/**
- * A Rect [TriBase] [View] of size [widthD] and [heightD] with the initial color, [color].
- */
-class SolidTriangle(size: Size, color: RGBA = Colors.WHITE) : SolidRect(size, color) {
-    // Temporary hacky solution
-    // TODO add tri() method to TexturedVertexArray ---> then we can use shortArrayOf(0, 1, 2) here
-    private val TEXTURED_ARRAY_TRI_INDICES = shortArrayOf(0, 1, 3,  0, 1, 3)
-    override val vertices = TexturedVertexArray(4, TEXTURED_ARRAY_TRI_INDICES)
+/** Creates a new [SolidTriangle] with points [[p1], [p1], [p3]] and color [color] and allows you to configure it via [callback]. Once created, it is added to this receiver [Container]. */
+inline fun Container.solidTriangle(
+  p1: Point, p2: Point, p3: Point,
+  color: RGBA = Colors.WHITE,
+  callback: @ViewDslMarker Mesh.() -> Unit = {}
+): Mesh {
+  val vertices = floatArrayOf(
+    p1.x.toFloat(), p1.y.toFloat(),
+    p2.x.toFloat(), p2.y.toFloat(),
+    p3.x.toFloat(), p3.y.toFloat(),
+  )
+  val indices = Uint16Buffer(UShortArrayInt(shortArrayOf(0, 1, 2)))
+  val uvs = Float32Buffer(12)
+  return Mesh(vertices = Float32Buffer(vertices), indices = indices, uvs = uvs)
+    .addTo(this, callback)
 }
