ef4fed8f01aa7c4d333eb470e680c04862d23af8
raV <rafalziemniewski@wp.pl>
2023-09-30 22:20:47 +0200

    inherit from mesh and expose triangle points

--- korge/src/common/korlibs/korge/view/SolidTriangle.kt
+++ korge/src/common/korlibs/korge/view/SolidTriangle.kt
@@ -1,25 +1,48 @@
 package korlibs.korge.view
 
-import korlibs.image.bitmap.*
 import korlibs.image.color.*
-import korlibs.korge.render.*
 import korlibs.math.geom.*
-import korlibs.math.geom.vector.*
 import korlibs.memory.*
+
+
+class SolidTriangle(
+  p1: Point, p2: Point, p3: Point,
+  color: RGBA = Colors.WHITE,
+) : Mesh(
+  vertices = Float32Buffer(
+    floatArrayOf(
+      p1.x.toFloat(), p1.y.toFloat(),
+      p2.x.toFloat(), p2.y.toFloat(),
+      p3.x.toFloat(), p3.y.toFloat(),
+    )
+  ),
+  indices = Uint16Buffer(UShortArrayInt(shortArrayOf(0, 1, 2))),
+  uvs = Float32Buffer(12)
+) {
+  var p1: Point = p1
+    set(value) {
+      updatedVertices()
+      field = value
+    }
+  var p2: Point = p2
+    set(value) {
+      updatedVertices()
+      field = value
+    }
+  var p3: Point = p3
+    set(value) {
+      updatedVertices()
+      field = value
+    }
+
+  init {
+    colorMul = color
+  }
+}
 
 /** Creates a new [SolidTriangle] with points [[p1], [p1], [p3]] and color [color] and allows you to configure it via [callback]. Once created, it is added to this receiver [Container]. */
 inline fun Container.solidTriangle(
   p1: Point, p2: Point, p3: Point,
   color: RGBA = Colors.WHITE,
   callback: @ViewDslMarker Mesh.() -> Unit = {}
-): Mesh {
-  val vertices = floatArrayOf(
-    p1.x.toFloat(), p1.y.toFloat(),
-    p2.x.toFloat(), p2.y.toFloat(),
-    p3.x.toFloat(), p3.y.toFloat(),
-  )
-  val indices = Uint16Buffer(UShortArrayInt(shortArrayOf(0, 1, 2)))
-  val uvs = Float32Buffer(12)
-  return Mesh(vertices = Float32Buffer(vertices), indices = indices, uvs = uvs)
-    .addTo(this, callback)
-}
+) = SolidTriangle(p1, p2, p3, color).addTo(this, callback)
