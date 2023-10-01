89f407852a32ea7bc0fdb6632952eafcadb4eaa1
raV <rafalziemniewski@wp.pl>
2023-10-02 00:07:24 +0200

    position + rotation added to test

--- korge/test/jvm/korlibs/korge/view/SolidTriangleTest.kt
+++ korge/test/jvm/korlibs/korge/view/SolidTriangleTest.kt
@@ -11,6 +11,8 @@ class SolidTriangleTest {
   fun test() = korgeScreenshotTest(Size(200, 200)) {
     addChild(
       solidTriangle(Point(10, 0), Point(0, 50), Point(110, 70), Colors.ANTIQUEWHITE)
+        .position(100, 10)
+        .rotation(30.degrees)
     )
 
     assertScreenshot(posterize = 5)
