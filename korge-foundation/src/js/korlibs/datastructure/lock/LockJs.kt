4eaf22832eae8f52402a98b47bbc2b61d88637aa
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-10-19 14:11:32 +0200

    Implement Lock.notify + Lock.wait + Lock.waitPrecise (#1950)

--- korge-foundation/src/js/korlibs/datastructure/lock/LockJs.kt
+++ korge-foundation/src/js/korlibs/datastructure/lock/LockJs.kt
@@ -1,9 +1,21 @@
 package korlibs.datastructure.lock
 
-actual class Lock actual constructor() {
+import korlibs.time.*
+
+actual class Lock actual constructor() : BaseLock {
 	actual inline operator fun <T> invoke(callback: () -> T): T = callback()
+    actual override fun notify(unit: Unit) {
+    }
+    actual override fun wait(time: TimeSpan): Boolean {
+        return false
+    }
 }
 
-actual class NonRecursiveLock actual constructor() {
+actual class NonRecursiveLock actual constructor() : BaseLock {
     actual inline operator fun <T> invoke(callback: () -> T): T = callback()
+    actual override fun notify(unit: Unit) {
+    }
+    actual override fun wait(time: TimeSpan): Boolean {
+        return false
+    }
 }
