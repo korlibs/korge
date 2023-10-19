559aa3d410c75adc7c4718dff79999df9b24960a
soywiz <soywiz@gmail.com>
2023-10-19 17:32:22 +0200

    Added some disabled test app examples using AwtAGOpenglCanvas & SyncEventLoop

---
+++ korge-foundation/test/jvm/korlibs/datastructure/event/JvmSyncEventLoopTest.kt
@@ -0,0 +1,124 @@
+package korlibs.datastructure.event
+
+import korlibs.datastructure.lock.*
+import korlibs.datastructure.thread.*
+import korlibs.time.*
+import java.awt.*
+import javax.swing.*
+import kotlin.test.*
+
+class JvmSyncEventLoopTest {
+
+    @Test
+    @Ignore
+    fun test() {
+        //val precise = false
+        val precise = true
+        val el = SyncEventLoop(precise = precise)
+        val lock = Lock()
+        var n = 0
+
+        fun updateGame() {
+            lock {
+                n++
+            }
+        }
+        fun drawGame(g: Graphics, y: Int) {
+            lock {
+                val g2d = (g as Graphics2D)
+
+                val b = g.clipBounds
+                g2d.color = Color(n and 0xFF, n and 0xFF, n and 0xFF, 0xFF)
+                g2d.clearRect(0, 0, b.width, b.height)
+                g2d.fillRect(b.x, b.y + y, 100, 100)
+                //g.dispose()
+            }
+        }
+
+        val frame = object : JFrame() {
+            init {
+                add(object : Container() {
+                    override fun paint(g: Graphics) {
+                        //super.paint(g)
+                        drawGame(g, 0)
+                    }
+                })
+            }
+        }
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.preferredSize = Dimension(200, 200)
+        frame.setLocationRelativeTo(null)
+        //frame.createBufferStrategy(2)
+        frame.pack()
+
+
+        el.setInterval(60.hz.timeSpan) {
+            //println("ITEM")
+            updateGame()
+        }
+        nativeThread {
+            el.runTasksForever()
+        }
+        nativeThread {
+            var running = true
+            while (running) {
+                val fps = frame.graphicsDevice.displayMode.refreshRate.takeIf { it > 10 } ?: 10
+                //println("fps=$fps")
+                for (n in 0 until 10) {
+                    if (!running) break
+                    frame.repaint()
+                    NativeThread.sleep(fps.hz.timeSpan, precise)
+                }
+            }
+        }
+        frame.isVisible = true
+        frame.createBufferStrategy(2)
+
+        /*
+        val bc = GraphicsEnvironment.getLocalGraphicsEnvironment()
+            .getDefaultScreenDevice()
+            .getDefaultConfiguration()
+            .getBufferCapabilities()
+        println("BC=$bc")
+        //val b = ExtendedBufferCapabilities(
+        //    BufferCapabilities(ImageCapabilities(true), ImageCapabilities(true), FlipContents.PRIOR),
+        //    ExtendedBufferCapabilities.VSyncType.VSYNC_ON
+        //)
+        //frame.createBufferStrategy(2, b)
+        frame.createBufferStrategy(3)
+        val strategy = frame.bufferStrategy
+
+        nativeThread {
+            while (true) {
+                NativeThread.sleepExact(60.hz.timeSpan)
+                val time = measureTime {
+                do {
+                        do {
+                            val g = strategy.drawGraphics
+                            // Your rendering code here
+                        } while (strategy.contentsRestored())
+                        strategy.show()
+                } while (strategy.contentsLost())
+                }
+                println("time=$time")
+            }
+        }
+
+         */
+
+        NativeThread.sleep(100.seconds)
+    }
+
+
+    private val ge by lazy { GraphicsEnvironment.getLocalGraphicsEnvironment() }
+    val JFrame.graphicsDevice: GraphicsDevice get() {
+        var currentGraphicsDevice: GraphicsDevice? = null
+        for (gd in ge.screenDevices) {
+            if (gd.defaultConfiguration.bounds.intersects(this.bounds)) {
+                currentGraphicsDevice = gd
+                break
+            }
+        }
+        return currentGraphicsDevice ?: ge.defaultScreenDevice
+    }
+}
---
+++ korge/test/jvm/korlibs/render/awt/AwtGameCanvasTest.kt
@@ -0,0 +1,113 @@
+package korlibs.render.awt
+
+import korlibs.datastructure.event.*
+import korlibs.datastructure.lock.*
+import korlibs.datastructure.thread.*
+import korlibs.image.bitmap.*
+import korlibs.image.color.*
+import korlibs.korge.render.*
+import korlibs.memory.dyn.osx.*
+import korlibs.render.osx.*
+import korlibs.render.osx.metal.*
+import korlibs.time.*
+import java.awt.*
+import javax.swing.*
+import kotlin.math.*
+import kotlin.test.*
+
+class AwtGameCanvasTest {
+    @Test
+    @Ignore
+    fun test() = korlibs.memory.dyn.osx.autoreleasePool {
+        //System.setProperty("sun.java2d.metal", "true")
+        //System.setProperty("sun.java2d.opengl", "false")
+        System.setProperty("sun.java2d.opengl", "true")
+        //System.setProperty("sun.java2d.opengl", "false")
+
+        val frame = JFrame("Title")
+        val bmp = Bitmap32(128, 128) { x, y -> Colors.BLACK.withB(x * 2).withG(y * 2) }.premultiplied()
+        val bmp2 = Bitmap32(128, 128) { x, y -> Colors.BLACK.withR(x * 2).withG(y * 2) }.premultiplied()
+
+        val el = SyncEventLoop()
+        val viewsLock = Lock()
+
+        var x = 0
+
+        el.setInterval(60.hz.timeSpan) {
+            viewsLock {
+                x++
+            }
+        }
+
+        nativeThread {
+            el.runTasksForever()
+        }
+
+        frame.contentPane.layout = GridLayout(2, 2)
+        var canvas: AwtAGOpenglCanvas? = null
+        frame.contentPane.add(AwtAGOpenglCanvas().also {
+            canvas = it
+            val renderContext = RenderContext(it.ag, it)
+            it.doRender = { ag ->
+                renderContext.doRenderNew {
+                    //println(renderContext.currentWidth)
+                    renderContext.clear(Colors.LIGHTPINK)
+                    viewsLock {
+                        renderContext.drawBitmapXY(renderContext.currentFrameBuffer, bmp, (sin(x.toFloat() / 50f) * 200 + 200).toInt(), 100)
+                        //SolidRect(100, 100, Colors.MEDIUMPURPLE).render(renderContext)
+                    }
+                }
+            }
+        })
+        frame.contentPane.add(JLabel().also { it.isOpaque = true; it.background = Color.YELLOW })
+        frame.contentPane.add(JLabel().also { it.isOpaque = true; it.background = Color.GREEN })
+        frame.contentPane.add(AwtAGOpenglCanvas().also {
+            val renderContext = RenderContext(it.ag, it)
+            it.doRender = { ag ->
+                renderContext.doRenderNew {
+                    //println(renderContext.currentWidth)
+                    renderContext.clear(Colors.MEDIUMPURPLE)
+                    renderContext.drawBitmap(renderContext.currentFrameBuffer, bmp2, -.5f, +.5f, +.5f, -.5f)
+                }
+                //SolidRect(100, 100, Colors.MEDIUMPURPLE).render(renderContext)
+            }
+        })
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.preferredSize = Dimension(600, 600)
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.isVisible = true
+
+        val dl = OSXDisplayLink {
+            canvas?.repaint()
+            //println("FRAME!")
+        }
+        dl.start()
+
+        /*
+        //println("PEER: " + AWTAccessor.getComponentAccessor().getPeer<ComponentPeer>(frame))
+        val layer = frame.getCAMetalLayer()
+        println("layer=$layer, device=${layer?.device}")
+        val dict = NSMutableDictionary()
+        dict["test"] = 9
+        println(NSNumber(dict["test"].id))
+        dict.setValue(NSNumber(9), NSString("hello"))
+        println(dict.count)
+        val tex = CoreVideoOpenGLMetalSharedTexture(512, 512)
+        println("tex=$tex")
+        //val dic = NSDictionary()
+        //println("dic=$dic : ${dic.count}")
+        println(NSNumber(9).intValue)
+        println(NSNumber(10L).longValue)
+        println(NSNumber(11.1).doubleValue)
+        //println(NSClass("NSNumber").alloc().msgSend("init"))
+         */
+
+        //CPlatformWindow
+
+        //frame.repaint()
+        //NativeThread.sleep(0.1.seconds)
+        //frame.repaint()
+        NativeThread.sleep(100.seconds)
+    }
+}
