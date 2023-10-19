c07ffeca445d51ea8917d1108e71f9c022fbee08
soywiz <soywiz@gmail.com>
2023-10-19 13:05:42 +0200

    Make korio thread globals to use NativeThread members

--- korge-core/src/common/korlibs/io/lang/Thread.kt
+++ korge-core/src/common/korlibs/io/lang/Thread.kt
@@ -1,6 +1,17 @@
 package korlibs.io.lang
 
-expect val currentThreadId: Long
-expect val currentThreadName: String?
+import korlibs.datastructure.thread.*
+import korlibs.time.*
 
-expect fun Thread_sleep(time: Long): Unit
+@Deprecated("", ReplaceWith("NativeThread.currentThreadId", "korlibs.datastructure.thread.NativeThread"))
+val currentThreadId: Long get() = NativeThread.currentThreadId
+@Deprecated("", ReplaceWith("NativeThread.currentThreadName", "korlibs.datastructure.thread.NativeThread"))
+val currentThreadName: String? get() = NativeThread.currentThreadName
+@Deprecated("", ReplaceWith("NativeThread.sleep(time.milliseconds)", "korlibs.datastructure.thread.NativeThread", "korlibs.time.milliseconds"))
+fun Thread_sleep(time: Long): Unit = NativeThread.sleep(time.milliseconds)
+//inline fun spinWhile(cond: () -> Boolean): Unit = NativeThread.spinWhile(cond)
+
+@Deprecated("", ReplaceWith("NativeThread.sleepUntil(dateTime, exact)", "korlibs.datastructure.thread.NativeThread", "korlibs.datastructure.thread.sleepUntil"))
+fun Thread_sleepUntil(dateTime: DateTime, exact: Boolean = true) {
+    NativeThread.sleepUntil(dateTime, exact)
+}
--- korge-core/src/darwin/korlibs/io/lang/ThreadDarwin.kt
+++
@@ -1,7 +0,0 @@
-package korlibs.io.lang
-
-import kotlinx.cinterop.objcPtr
-import platform.Foundation.NSThread
-
-actual val currentThreadId: Long get() = NSThread.currentThread.objcPtr().toLong()
-actual val currentThreadName: String? get() = "Thread-$currentThreadId"
