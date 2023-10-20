e0669c5b3ccd469425d05467df4e1c461853db64
soywiz <soywiz@gmail.com>
2023-10-20 05:35:08 +0200

    Added SyncEventLoopCoroutineDispatcher

---
+++ korge/test/common/korlibs/render/SyncEventLoopCoroutineDispatcherTest.kt
@@ -0,0 +1,19 @@
+package korlibs.render
+
+import korlibs.io.async.*
+import korlibs.time.*
+import kotlinx.coroutines.*
+import kotlin.test.*
+
+class SyncEventLoopCoroutineDispatcherTest {
+    @Test
+    fun test() {
+        val dispatcher = SyncEventLoopCoroutineDispatcher(precise = true, immediateRun = true)
+        launchImmediately(dispatcher) {
+            println("${DateTime.now()}: a")
+            delay(1000.milliseconds)
+            println("${DateTime.now()}: b")
+        }
+        dispatcher.loopUntilEmpty()
+    }
+}
