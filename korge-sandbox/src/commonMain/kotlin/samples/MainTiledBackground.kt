a581e445d7a95c1c9c80f306ab1f945db126fe06
soywiz <soywiz@gmail.com>
2023-10-22 03:23:55 +0200

    Optimize MainTiledBackground

--- korge-sandbox/src/commonMain/kotlin/samples/MainTiledBackground.kt
+++ korge-sandbox/src/commonMain/kotlin/samples/MainTiledBackground.kt
@@ -1,7 +1,7 @@
 package samples
 
 import korlibs.datastructure.*
-import korlibs.image.bitmap.*
+import korlibs.image.atlas.*
 import korlibs.image.format.*
 import korlibs.image.tiles.*
 import korlibs.io.file.std.*
@@ -12,9 +12,13 @@ import korlibs.korge.view.tiles.*
 class MainTiledBackground : Scene() {
     override suspend fun SContainer.sceneMain() {
 
+        val atlas = MutableAtlasUnit()
+        val korgePng = atlas.add(bitmap("korge.png").toBMP32().scaleLinear(0.5, 0.5))
+        val korimPng = atlas.add(bitmap("korim.png").toBMP32().scaleLinear(0.5, 0.5))
+
         val tileset = TileSet(
-            TileSetTileInfo(0, bitmap("korge.png").toBMP32().scaleLinear(0.5, 0.5).slice()),
-            TileSetTileInfo(1, bitmap("korim.png").toBMP32().scaleLinear(0.5, 0.5).slice()),
+            TileSetTileInfo(0, korgePng.slice),
+            TileSetTileInfo(1, korimPng.slice),
             //TileSetTileInfo(1, Bitmap32(256, 256, Colors.MEDIUMAQUAMARINE).premultipliedIfRequired().slice())
         )
         val tilemap = tileMap(IntArray2(2, 2, intArrayOf(0, 1, 1, 0)), repeatX = TileMapRepeat.REPEAT, repeatY = TileMapRepeat.REPEAT, tileset = tileset)
