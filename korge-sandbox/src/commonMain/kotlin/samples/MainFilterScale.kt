6148692cd8d953e0d3ed991142bbfc36e7163706
soywiz <soywiz@gmail.com>
2023-10-22 06:28:07 +0200

    Add DitheringFilter to the MainFilterScale sample

--- korge-sandbox/src/commonMain/kotlin/samples/MainFilterScale.kt
+++ korge-sandbox/src/commonMain/kotlin/samples/MainFilterScale.kt
@@ -10,13 +10,24 @@ import korlibs.korge.view.filter.*
 
 class MainFilterScale : Scene() {
     override suspend fun SContainer.sceneMain() {
-        val image = image(resourcesVfs["korge.png"].readBitmap()).xy(100, 100)
+        val bmp = resourcesVfs["korge.png"].readBitmap()
+        val image = image(bmp).xy(100, 100)
             .filterScale(1.0)
             .filters(Convolute3Filter(Convolute3Filter.KERNEL_EDGE_DETECTION))
+        val dithering = DitheringFilter(4.0)
+        val image2 = image(bmp).xy(600, 100)
+            .filterScale(1.0)
+            .filters(dithering)
         //.filters(BlurFilter())
         //.filters(WaveFilter())
 
-        val combo = uiSlider(value = 1.0, max = 1.0, step = 0.01).xy(400, 100).changed { image.filterScale = it  }
+        val combo = uiSlider(value = 1.0, max = 1.0, step = 0.01).xy(400, 100).changed {
+            image.filterScale = it
+            image2.filterScale = it
+        }
+        val combo2 = uiSlider(value = 4.0, min = 1.0, max = 8.0, step = 0.1).xy(400, 50).changed {
+            dithering.levels = it
+        }
         //val combo = uiComboBox(items = listOf(0.0, 0.01, 0.05, 0.075, 0.125, 0.25, 0.44, 0.5, 0.75, 0.95, 0.99, 1.0)).xy(400, 100).onSelectionUpdate { image.filterScale = it.selectedItem ?: 1.0 }
 
         // This reproduces a bug (black right and bottom border) at least on macOS with M1
