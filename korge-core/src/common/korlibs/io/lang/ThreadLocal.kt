78be1278e9b244ed3c4ff5a0179e8c6a4770e2b4
soywiz <soywiz@gmail.com>
2023-10-25 01:58:36 +0200

    Proper implementation of ThreadLocal on K/N new memory model + tests

--- korge-core/src/common/korlibs/io/lang/ThreadLocal.kt
+++ korge-core/src/common/korlibs/io/lang/ThreadLocal.kt
@@ -7,8 +7,12 @@ class threadLocal<T>(val gen: () -> T) {
 		override fun initialValue(): T = gen()
 	}
 
-	inline operator fun getValue(thisRef: Any?, property: KProperty<*>): T = local.get()
-	inline operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T): Unit = local.set(value)
+    var value: T
+        get() = getValue(null, null)
+        set(value) { setValue(null, null, value) }
+
+    inline operator fun getValue(thisRef: Any?, property: KProperty<*>?): T = local.get()
+	inline operator fun setValue(thisRef: Any?, property: KProperty<*>?, value: T): Unit = local.set(value)
 }
 
 expect abstract class NativeThreadLocal<T>() {
