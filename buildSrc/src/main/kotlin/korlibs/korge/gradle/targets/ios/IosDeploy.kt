d68f1279680ad1a37c1d05b87603026411bb8483
soywiz <soywiz@gmail.com>
2023-10-24 20:18:41 +0200

    Updated ios-deploy to the latest version

--- buildSrc/src/main/kotlin/korlibs/korge/gradle/targets/ios/IosDeploy.kt
+++ buildSrc/src/main/kotlin/korlibs/korge/gradle/targets/ios/IosDeploy.kt
@@ -1,9 +1,8 @@
 package korlibs.korge.gradle.targets.ios
 
-import org.gradle.api.Project
-import korlibs.korge.gradle.util.projectExtension
-import korlibs.korge.gradle.util.execLogger
-import java.io.File
+import korlibs.korge.gradle.util.*
+import org.gradle.api.*
+import java.io.*
 
 val Project.iosTvosDeployExt by projectExtension {
     IosDeploy(this)
@@ -12,10 +11,27 @@ val Project.iosTvosDeployExt by projectExtension {
 val korgeCacheDir get() = File(System.getProperty("user.home"), ".korge").apply { mkdirs() }
 
 class IosDeploy(val project: Project) {
-    val iosDeployDir = File(korgeCacheDir, "ios-deploy")
+    val iosDeployVersion = "1.12.2"
+    //val iosDeployRepo = "https://github.com/korlibs/ios-deploy.git"
+    val iosDeployRepo = "https://github.com/ios-control/ios-deploy.git"
+    val iosDeployDir = File(korgeCacheDir, "ios-deploy-$iosDeployVersion")
     val iosDeployCmd = File(iosDeployDir, "build/Release/ios-deploy")
 
     val isInstalled get() = iosDeployCmd.exists()
+
+    // https://github.com/ios-control/ios-deploy/issues/588
+    // no ios-deploy required anymore?
+    // xcrun devicectl list devices -j /tmp/devices.json
+    // xcrun devicectl device install app --device 00008110-001XXXXXXXXXX ./korge-sandbox/build/platforms/ios/app.xcodeproj/build/Build/Products/Debug-iphoneos/unnamed.app
+    // crun devicectl device process launch --device 00008110-001XXXXXXXXXX file:///private/var/containers/Bundle/Application/1604D2D5-35F3-4E43-8B47-1DEF5D778480/nilo.app
+
+    fun install(localAppPath: String) {
+        command("--bundle", localAppPath)
+    }
+
+    fun installAndRun(localAppPath: String) {
+        command("--noninteractive", "-d", "--bundle", localAppPath)
+    }
 
     fun command(vararg cmds: String) {
         project.execLogger {
@@ -36,7 +52,12 @@ class IosDeploy(val project: Project) {
     fun clone() {
         iosDeployDir.mkdirs()
         project.execLogger {
-            it.commandLine("git", "clone", "https://github.com/korlibs/ios-deploy.git", iosDeployDir.absolutePath)
+            it.workingDir(iosDeployDir.absolutePath)
+            it.commandLine("git", "clone", iosDeployRepo, iosDeployDir.absolutePath)
+        }
+        project.execLogger {
+            it.workingDir(iosDeployDir.absolutePath)
+            it.commandLine("git", "checkout", iosDeployVersion)
         }
     }
 
