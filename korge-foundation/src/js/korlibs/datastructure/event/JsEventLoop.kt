375318c6e22c5a5e0c4cb6bc10e0aad3e3b599ed
soywiz <soywiz@gmail.com>
2023-10-24 20:58:48 +0200

    Adds Pauseable interface + improves EventLoop

---
+++ korge-foundation/src/js/korlibs/datastructure/event/JsEventLoop.kt
@@ -0,0 +1,24 @@
+package korlibs.datastructure.event
+
+import korlibs.datastructure.closeable.*
+import korlibs.platform.*
+import korlibs.time.*
+
+object JsEventLoop : BaseEventLoop() {
+    override var paused: Boolean = false
+
+    override fun close() = Unit
+        override fun setImmediate(task: () -> Unit) {
+            jsGlobalThis.setTimeout({ task() }, 0)
+        }
+
+        override fun setTimeout(time: TimeSpan, task: () -> Unit): Closeable {
+            val id = jsGlobalThis.setTimeout({ task() }, time.millisecondsInt)
+            return Closeable { jsGlobalThis.clearTimeout(id) }
+        }
+
+        override fun setInterval(time: TimeSpan, task: () -> Unit): Closeable {
+            val id = jsGlobalThis.setInterval({ task() }, time.millisecondsInt)
+            return Closeable { jsGlobalThis.clearInterval(id) }
+        }
+}
