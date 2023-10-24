15e14e5ec2c3ccf166a7a1d80500394ea29ef318
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-10-24 22:05:27 +0200

    Lock on K/N sleep instead of spin on wait. Check that Lock.notify is in the right thread and locked + Tests (#1976)
    * Lock on K/N sleep instead of spin on wait. Check that Lock.notify is in the right thread and locked
    
    * Add missing imports

--- korge-foundation/src/js/korlibs/datastructure/lock/LockJs.kt
+++ korge-foundation/src/js/korlibs/datastructure/lock/LockJs.kt
@@ -3,10 +3,20 @@ package korlibs.datastructure.lock
 import korlibs.time.*
 
 actual class Lock actual constructor() : BaseLock {
-	actual inline operator fun <T> invoke(callback: () -> T): T = callback()
+    var locked = false
+	actual inline operator fun <T> invoke(callback: () -> T): T {
+        locked = true
+        try {
+            return callback()
+        } finally {
+            locked = false
+        }
+    }
     actual override fun notify(unit: Unit) {
+        if (!locked) error("Must lock before notifying")
     }
     actual override fun wait(time: TimeSpan): Boolean {
+        if (!locked) error("Must lock before waiting")
         return false
     }
 }
