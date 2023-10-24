e77174c725faf225ccdf444f78a23184f6a64d4e
soywiz <soywiz@gmail.com>
2023-10-24 21:21:23 +0200

    Added arraycopyStride

--- korge-foundation/test/common/korlibs/memory/ArraysTest.kt
+++ korge-foundation/test/common/korlibs/memory/ArraysTest.kt
@@ -4,6 +4,17 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ArraysTest {
+
+    @Test
+    fun testCopyStrided() {
+        val data = ByteArray(6)
+        val data0 = byteArrayOf(0, 2, 4)
+        val data1 = byteArrayOf(1, 3, 5)
+        arraycopyStride(data0, 0, 1, data, 0, 2, 3)
+        arraycopyStride(data1, 0, 1, data, 1, 2, 3)
+        assertEquals(listOf(0, 1, 2, 3, 4, 5), data.map { it.toInt() })
+    }
+
     @Test
     fun floatArrayFromIntArray() {
         val fa = IntArray(16).asFloatArray()
