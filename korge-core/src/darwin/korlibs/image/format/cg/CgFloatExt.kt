6d5580950980510ab8201352b6257c43d65f2643
soywiz <soywiz@gmail.com>
2023-10-25 11:46:01 +0200

    Provide CGRect, CGPoint and CGSize to korge conversions

--- korge-core/src/darwin/korlibs/image/format/cg/CgFloatExt.kt
+++ korge-core/src/darwin/korlibs/image/format/cg/CgFloatExt.kt
@@ -1,5 +1,6 @@
 package korlibs.image.format.cg
 
+import korlibs.math.geom.*
 import kotlinx.cinterop.*
 import platform.CoreGraphics.*
 
@@ -16,6 +17,9 @@ inline val Float.cg: CGFloat get() = this.toCgFloat()
 @OptIn(UnsafeNumber::class)
 inline val Double.cg: CGFloat get() = this.toCgFloat()
 
-fun CGRectMakeExt(x: Int, y: Int, width: Int, height: Int): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
+fun CGRect.toRectangle(): Rectangle = Rectangle(this.origin.x, this.origin.y, this.size.width, this.size.height)
+fun CGPoint.toPoint(): Point = Point(x, y)
+fun CGSize.toSize(): Size = Size(width, height)fun CGRectMakeExt(x: Int, y: Int, width: Int, height: Int): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
+
 fun CGRectMakeExt(x: Float, y: Float, width: Float, height: Float): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
 fun CGRectMakeExt(x: Double, y: Double, width: Double, height: Double): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
