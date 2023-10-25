de2d2b128b3cc635a2151315b2da20fe32be4ee3
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-10-25 17:30:00 +0200

    Update CgFloatExt.kt

--- korge-core/src/darwin/korlibs/image/format/cg/CgFloatExt.kt
+++ korge-core/src/darwin/korlibs/image/format/cg/CgFloatExt.kt
@@ -19,7 +19,8 @@ inline val Double.cg: CGFloat get() = this.toCgFloat()
 
 fun CGRect.toRectangle(): Rectangle = Rectangle(this.origin.x, this.origin.y, this.size.width, this.size.height)
 fun CGPoint.toPoint(): Point = Point(x, y)
-fun CGSize.toSize(): Size = Size(width, height)fun CGRectMakeExt(x: Int, y: Int, width: Int, height: Int): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
+fun CGSize.toSize(): Size = Size(width, height)
 
+fun CGRectMakeExt(x: Int, y: Int, width: Int, height: Int): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
 fun CGRectMakeExt(x: Float, y: Float, width: Float, height: Float): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
 fun CGRectMakeExt(x: Double, y: Double, width: Double, height: Double): CValue<CGRect> = CGRectMake(x.cg, y.cg, width.cg, height.cg)
