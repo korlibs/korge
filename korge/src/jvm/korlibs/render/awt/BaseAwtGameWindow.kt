0a55f8cf115f059475ada67609e6470b90cb8e76
soywiz <soywiz@gmail.com>
2023-10-25 03:48:51 +0200

    Uses DesktopGamepadUpdater on BaseAwtGameWindow

--- korge/src/jvm/korlibs/render/awt/BaseAwtGameWindow.kt
+++ korge/src/jvm/korlibs/render/awt/BaseAwtGameWindow.kt
@@ -223,17 +223,8 @@ abstract class BaseAwtGameWindow(
     }
 
     override fun updateGamepads() {
-        when {
-            Platform.isWindows -> xinputEventAdapter.updateGamepads(this.gamepadEmitter)
-            Platform.isLinux -> linuxJoyEventAdapter.updateGamepads(this.gamepadEmitter)
-            Platform.isMac -> macosGamepadEventAdapter.updateGamepads(this)
-            else -> Unit //println("undetected OS: ${OS.rawName}")
-        }
+        DesktopGamepadUpdater.updateGamepads(this)
     }
-
-    private val xinputEventAdapter by lazy { XInputGamepadEventAdapter() }
-    private val linuxJoyEventAdapter by lazy { LinuxJoyEventAdapter() }
-    private val macosGamepadEventAdapter by lazy { MacosGamepadEventAdapter() }
 
     val frameScaleFactor: Float get() = getDisplayScalingFactor(component)
 
