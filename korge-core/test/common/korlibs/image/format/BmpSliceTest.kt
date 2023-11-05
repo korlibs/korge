7c18ea9144171ac95e02ae2ae980722d3c48941f
soywiz <soywiz@gmail.com>
2023-11-05 23:04:09 +0100

    Verify name is preserved when using readBitmapSlice with and without mutable atlases

---
+++ korge-core/test/common/korlibs/image/format/BmpSliceTest.kt
@@ -0,0 +1,36 @@
+package korlibs.image.format
+
+import korlibs.image.atlas.*
+import korlibs.image.bitmap.*
+import korlibs.io.file.std.*
+import korlibs.math.geom.*
+import korlibs.template.*
+import kotlin.test.*
+
+class BmpSliceTest {
+    @Test
+    fun testName() = suspendTest {
+        val slice = resourcesVfs["rgba.png"].readBitmapSlice(name = "hello")
+        assertEquals("hello", slice.name)
+        assertEquals(SizeInt(4, 1), slice.bounds.size)
+    }
+
+    @Test
+    fun testPacking() = suspendTest {
+        val atlas = AtlasPacker.pack(listOf(
+            resourcesVfs["rgba.png"].readBitmapSlice(name = "hello")
+        ))
+        val slice = atlas["hello"]
+        assertEquals("hello", slice.name)
+        assertEquals(SizeInt(4, 1), slice.bounds.size)
+    }
+
+    @Test
+    fun testPackingMutable() = suspendTest {
+        val atlas = MutableAtlasUnit()
+        resourcesVfs["rgba.png"].readBitmapSlice(atlas = atlas, name = "hello")
+        val slice = atlas["hello"]
+        assertEquals("hello", slice.name)
+        assertEquals(SizeInt(4, 1), slice.bounds.size)
+    }
+}
