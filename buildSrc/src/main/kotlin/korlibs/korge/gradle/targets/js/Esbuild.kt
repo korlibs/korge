c3ac44cff47ad597648d8c1fa159021c23ac4e69
soywiz <soywiz@gmail.com>
2023-12-15 14:13:18 +0100

    Kotlin 2.0.0-Beta2

--- buildSrc/src/main/kotlin/korlibs/korge/gradle/targets/js/Esbuild.kt
+++ buildSrc/src/main/kotlin/korlibs/korge/gradle/targets/js/Esbuild.kt
@@ -42,11 +42,17 @@ fun Project.configureErrorableEsbuild() {
 
         val esbuildVersion = korge.esbuildVersion
         doFirst {
-            val npmCmd = arrayOf(
-                File(env.nodeExecutable),
-                File(env.nodeDir, "lib/node_modules/npm/bin/npm-cli.js").takeIf { it.exists() }
-                    ?: File(env.nodeDir, "node_modules/npm/bin/npm-cli.js").takeIf { it.exists() }
-                    ?: error("Can't find npm-cli.js in ${env.nodeDir} standard folders")
+
+            //val nodeDir = env.nodeBinDir
+            val nodeDir = env.dir
+            val file1: File = File(env.nodeExecutable)
+            val file2: File? = File(nodeDir, "lib/node_modules/npm/bin/npm-cli.js").takeIf { it.exists() }
+            val file3: File? = File(nodeDir, "node_modules/npm/bin/npm-cli.js").takeIf { it.exists() }
+            val npmCmd = arrayOf<File>(
+                file1,
+                file2
+                    ?: file2
+                    ?: error("Can't find npm-cli.js in ${nodeDir} standard folders")
             )
 
             environment("PATH", ENV_PATH)
