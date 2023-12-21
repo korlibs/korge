09a7d5050be752e5fa7d79a07a7432385753414f
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-12-21 12:44:54 +0100

    Add view Text.textRange Start/End (#2082)

--- korge/test/korlibs/korge/view/TextTest.kt
+++ korge/test/korlibs/korge/view/TextTest.kt
@@ -2,6 +2,8 @@ package korlibs.korge.view
 
 import assertEqualsFloat
 import korlibs.image.font.*
+import korlibs.io.async.*
+import korlibs.io.file.std.*
 import korlibs.math.geom.*
 import kotlin.test.*
 
@@ -18,5 +20,16 @@ class TextTest {
             Rectangle.fromBounds(Point(-0.8, 0), Point(28.9, 18.4)),
             text.getLocalBounds().roundDecimalPlaces(1)
         )
+    }
+
+    @Test
+    fun testTextRange() = suspendTest {
+        val text = Text("Hello", font = resourcesVfs["font/font.fnt"].readBitmapFont())
+        text.textRangeStart = 1
+        text.textRangeEnd = 3
+        text._renderInternal(null)
+        val sliceContainer = text.firstChild as Container
+        assertEquals(5, sliceContainer.numChildren)
+        assertEquals(".XX..", sliceContainer.children.joinToString("") { if (it.visible) "X" else "." })
     }
 }
