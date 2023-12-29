a8a1cca7ca1e729b6e8fd547e5dee9dd91be137b
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-12-29 09:21:07 +0100

    Remove korlibs-crypto unused dependencies (#2091)

--- korlibs-crypto/build.gradle.kts
+++ korlibs-crypto/build.gradle.kts
@@ -12,15 +12,5 @@ project.extensions.extraProperties.properties.apply {
 }
 
 dependencies {
-    //add("commonMainImplementation", "org.jetbrains.kotlinx:atomicfu:${libs.versions.kotlinx.atomicfu.get()}")
-    //add("commonMainApi", "org.jetbrains.kotlinx:atomicfu:${libs.versions.kotlinx.atomicfu.get()}")
-    //add("jvmTestApi", "org.powermock:powermock-mockito-release-full:1.6.4")
-    //add("jvmTestApi", "org.fuin:units4j:0.8.4")
-    //add("jvmTestApi", "org.ow2.asm:asm:8.0.1")
-    commonMainApi(libs.kotlinx.coroutines.core)
-    commonMainApi(libs.kotlinx.atomicfu)
-    //add("commonMainApi", libs.kotlinx.atomicfu)
-    //add("commonTestApi", project(":korge-test"))
-    commonTestApi(libs.kotlinx.coroutines.test)
     commonMainApi(project(":korlibs-time"))
 }
