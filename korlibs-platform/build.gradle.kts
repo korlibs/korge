c9812cab6554ac379787129c8b9d8ef3a9beb18b
soywiz <soywiz@gmail.com>
2024-03-02 04:43:51 +0100

    Reduce korlibs-platform dependencies

--- korlibs-platform/build.gradle.kts
+++ korlibs-platform/build.gradle.kts
@@ -11,9 +11,4 @@ project.extensions.extraProperties.properties.apply {
 }
 
 dependencies {
-    commonMainApi(libs.kotlinx.coroutines.core)
-    commonMainApi(libs.kotlinx.atomicfu)
-    commonTestApi(libs.kotlinx.coroutines.test)
-    commonMainApi(libs.kotlinx.atomicfu)
-    commonTestApi(libs.kotlinx.coroutines.test)
 }
