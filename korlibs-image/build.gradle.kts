55956d45c07b9f5e74f0cd0791144d2111bef27d
soywiz <soywiz@gmail.com>
2024-03-09 02:51:02 +0100

    [WIP] Extract korlibs-image

---
+++ korlibs-image/build.gradle.kts
@@ -0,0 +1,18 @@
+import korlibs.*
+
+description = "Korlibs Image"
+
+project.extensions.extraProperties.properties.apply {
+    applyProjectProperties(
+        "https://raw.githubusercontent.com/korlibs/korge/main/korlibs-image",
+        "MIT",
+        "https://raw.githubusercontent.com/korlibs/korge/main/korlibs-image/LICENSE"
+    )
+}
+
+dependencies {
+    commonMainApi(project(":korlibs-io"))
+    commonMainApi(project(":korlibs-ffi"))
+    //jvmMainImplementation(libs.bundles.jna)
+    commonMainApi(libs.kotlinx.atomicfu)
+}
