c4bcfdb5d9fa3ed769295ec97cb9ba296e556d64
soywiz <soywiz@gmail.com>
2024-07-19 14:24:31 +0200

    Send KeyEvent.Type.TYPE events too on iOS

--- korge-core/src@darwin/korlibs/render/DefaultGameWindowIos.kt
+++ korge-core/src@darwin/korlibs/render/DefaultGameWindowIos.kt
@@ -152,7 +152,6 @@ class ViewController(
     @Suppress("RemoveRedundantCallsOfConversionMethods")
     @OptIn(UnsafeNumber::class)
     private fun pressesHandler(type: KeyEvent.Type, presses: Set<*>, withEvent: UIPressesEvent?) {
-        super.pressesBegan(presses, withEvent)
         for (press in presses) {
             if (press !is UIPress) continue
             val uiKey = press.key ?: continue
@@ -161,17 +160,16 @@ class ViewController(
             val key = IosKeyMap.KEY_MAP[keyCode.toInt()] ?: Key.UNKNOWN
             //println("pressesHandler[$type]: ${keyCode}, ${modifierFlags}, $key, ${uiKey.charactersIgnoringModifiers}")
 
-            gameWindow.dispatchKeyEventEx(
-                type,
-                0,
-                uiKey.charactersIgnoringModifiers.firstOrNull() ?: '\u0000',
-                key,
-                keyCode.toInt(),
-                shift = modifierFlags.hasFlags(UIKeyModifierShift.toInt()),
-                ctrl = modifierFlags.hasFlags(UIKeyModifierControl.toInt()),
-                alt = modifierFlags.hasFlags(UIKeyModifierAlternate.toInt()),
-                meta = modifierFlags.hasFlags(UIKeyModifierCommand.toInt()),
-            )
+            val char = uiKey.charactersIgnoringModifiers.firstOrNull() ?: '\u0000'
+
+            val shift = modifierFlags.hasFlags(UIKeyModifierShift.toInt())
+            val ctrl = modifierFlags.hasFlags(UIKeyModifierControl.toInt())
+            val alt = modifierFlags.hasFlags(UIKeyModifierAlternate.toInt())
+            val meta = modifierFlags.hasFlags(UIKeyModifierCommand.toInt())
+            if (type == KeyEvent.Type.DOWN && char >= '\u0020') {
+                gameWindow.dispatchKeyEventEx(KeyEvent.Type.TYPE, 0, char, key, keyCode.toInt(), shift = shift, ctrl = ctrl, alt = alt, meta = meta,)
+            }
+            gameWindow.dispatchKeyEventEx(type, 0, char, key, keyCode.toInt(), shift = shift, ctrl = ctrl, alt = alt, meta = meta,)
         }
     }
 
@@ -181,7 +179,7 @@ class ViewController(
     }
 
     override fun pressesEnded(presses: Set<*>, withEvent: UIPressesEvent?) {
-        super.pressesBegan(presses, withEvent)
+        super.pressesEnded(presses, withEvent)
         pressesHandler(KeyEvent.Type.UP, presses, withEvent)
     }
 
