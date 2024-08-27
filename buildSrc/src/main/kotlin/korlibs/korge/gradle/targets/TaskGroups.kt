480538f164f575bd3bc6373602216152e5151716
soywiz <soywiz@gmail.com>
2024-08-27 12:57:38 +0200

    Change task groups to `@korge something` so they are easier to find

--- buildSrc/src/main/kotlin/korlibs/korge/gradle/targets/TaskGroups.kt
+++ buildSrc/src/main/kotlin/korlibs/korge/gradle/targets/TaskGroups.kt
@@ -1,9 +1,7 @@
 package korlibs.korge.gradle.targets
 
-const val GROUP_KORGE: String = "korge"
-const val GROUP_KORGE_RESOURCES: String = "korge-resources"
-const val GROUP_KORGE_RUN: String = "run"
-const val GROUP_KORGE_ADB: String = "adb"
-const val GROUP_KORGE_LIST: String = "list"
-const val GROUP_KORGE_PACKAGE: String = "package"
-const val GROUP_KORGE_INSTALL: String = "install"
+const val GROUP_KORGE_RUN: String = "@korge run"
+const val GROUP_KORGE_ADB: String = "@korge adb"
+const val GROUP_KORGE_LIST: String = "@korge list"
+const val GROUP_KORGE_PACKAGE: String = "@korge package"
+const val GROUP_KORGE_INSTALL: String = "@korge install"
