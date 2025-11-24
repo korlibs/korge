245fc79998a1e7d13f5004a9724f2d3adad87ae9
Marko Koschak <marko.koschak@tisno.de>
2025-11-24 18:53:29 +0100

    Implement addImageAse for all layer and tag combinations

--- korge-gradle-plugin/src/main/kotlin/korlibs/korge/gradle/korgefleks/AssetConfig.kt
+++ korge-gradle-plugin/src/main/kotlin/korlibs/korge/gradle/korgefleks/AssetConfig.kt
@@ -2,7 +2,6 @@ package korlibs.korge.gradle.korgefleks
 
 import korlibs.korge.gradle.util.ASEInfo
 import korlibs.korge.gradle.util.LocalSFile
-import korlibs.korge.gradle.util.executeSystemCommand
 import org.gradle.api.GradleException
 import java.io.File
 
@@ -17,6 +16,8 @@ class AssetsConfig(
     // Directory where game resources are located
     private val gameResourcesDir = projectDir.resolve("src/commonMain/resources")
 
+    private val imagePrefix = "img_"
+
     init {
         // Make sure the export directories exist and that they are empty
         if (exportTilesDir.exists()) exportTilesDir.deleteRecursively()
@@ -25,35 +26,145 @@ class AssetsConfig(
         exportTilesetDir.mkdirs()
     }
 
+    /** Get the Aseprite File object from the asset path
+     */
     private fun getAseFile(filename: String): File {
         val aseFile = projectDir.resolve("${assetPath}/${filename}")
         if (!aseFile.exists()) throw GradleException("Aseprite file '${aseFile}' not found!")
         return aseFile
     }
 
-    private fun checkLayersAvailable(aseFile: File, layers: List<String>) {
+    /** Check that the specified layers and tags exist in the Aseprite file
+     */
+    private fun checkLayersTagsAvailable(aseFile: File, layers: List<String>, tags: List<String>) {
         val aseInfo = ASEInfo.getAseInfo(LocalSFile(aseFile))
-
+        val availableLayers = aseInfo.layers.map { it.layerName }
+        val availableTags = aseInfo.tags.map { it.tagName }
+        for (layer in layers) {
+            if (layer !in availableLayers) {
+                throw GradleException("Layer '${layer}' not found in Aseprite file '${aseFile.name}'! " +
+                    "Available layers: ${availableLayers.joinToString(", ")}")
+            }
+        }
+        for (tag in tags) {
+            if (tag !in availableTags) {
+                throw GradleException("Tag '${tag}' not found in Aseprite file '${aseFile.name}'! " +
+                    "Available tags: ${availableTags.joinToString(", ")}")
+            }
+        }
     }
 
-    fun addImageAse(filename: String, output: String) {
+    /** Export specific layers and tags from Aseprite file
+     */
+    fun addImageAse(filename: String, layers: List<String>, tags: List<String>, output: String) {
         if (output.isBlank()) throw GradleException("No output file specified for Aseprite export of '${filename}'!")
 
         val aseFile = getAseFile(filename)
+        checkLayersTagsAvailable(aseFile, layers, tags)
 
-        val outFile = exportTilesDir.resolve("img_${output}.png")
+        println("Export image file: '${filename}', layers: '${layers}', tags: '${tags}', output: '${output}'")
 
-        val cmd = arrayOf(asepriteExe, "-b", aseFile.absolutePath, "--save-as", outFile.absolutePath)
-        println("Export image file: '${filename}', output: '${output}'")
+        if (layers.isNotEmpty()) {
+            val useLayerName = layers.size != 1  // Only use layer name in output if multiple layers are specified
 
-        //println("Executing command: ${cmd.joinToString(" ")}")
-        executeSystemCommand(cmd)
+            for (layer in layers) {
+                if (tags.isNotEmpty()) {
+                    val useTagName = tags.size != 1  // Only use tag name in output if multiple tags are specified
+
+                    for (tag in tags) {
+                        val cmd = mutableListOf(asepriteExe, "-b")
+                        cmd.add("--layer")
+                        cmd.add(layer)
+                        cmd.add("--tag")
+                        cmd.add(tag)
+                        cmd.add(aseFile.absolutePath)
+                        cmd.add("--save-as")
+                        cmd.add(exportTilesDir.resolve(
+                            if (useLayerName && useTagName) "${imagePrefix}${output}_${layer}_${tag}_{{frame0}}.png"
+                            else if (useLayerName) "${imagePrefix}${output}_${layer}_{{frame0}}.png"
+                            else if (useTagName) "${imagePrefix}${output}_${tag}_{{frame0}}.png"
+                            else "${imagePrefix}${output}_{{frame0}}.png").absolutePath)
+
+                        println("Executing command: ${cmd.joinToString(" ")}")
+//                        executeSystemCommand(cmd)
+
+                    }
+                } else {
+                    val cmd = mutableListOf(asepriteExe, "-b")
+                    cmd.add("--layer")
+                    cmd.add(layer)
+                    cmd.add(aseFile.absolutePath)
+                    cmd.add("--save-as")
+                    cmd.add(exportTilesDir.resolve(
+                        if (useLayerName) "${imagePrefix}${output}_${layer}_{{frame0}}.png"
+                        else "${imagePrefix}${output}_{{frame0}}.png").absolutePath)
+
+                    println("Executing command: ${cmd.joinToString(" ")}")
+//                    executeSystemCommand(cmd)
+                }
+            }
+        } else {
+            if (tags.isNotEmpty()) {
+                val useTagName = tags.size != 1  // Only use tag name in output if multiple tags are specified
+
+                for (tag in tags) {
+                    val cmd = mutableListOf(asepriteExe, "-b")
+                    cmd.add("--tag")
+                    cmd.add(tag)
+                    cmd.add(aseFile.absolutePath)
+                    cmd.add("--save-as")
+                    cmd.add(exportTilesDir.resolve(
+                        if (useTagName) "${imagePrefix}${output}_${tag}_{{frame0}}.png"
+                        else "${imagePrefix}${output}_{{frame0}}.png").absolutePath)
+
+                    println("Executing command: ${cmd.joinToString(" ")}")
+//                    executeSystemCommand(cmd)
+                }
+            } else {
+                val cmd = mutableListOf(asepriteExe, "-b")
+                cmd.add(aseFile.absolutePath)
+                cmd.add("--save-as")
+                cmd.add(exportTilesDir.resolve("${imagePrefix}${output}_{{frame0}}.png").absolutePath)
+
+                println("Executing command: ${cmd.joinToString(" ")}")
+//                executeSystemCommand(cmd)
+            }
+        }
     }
 
-    fun addImageAse(filename: String, layer: String, output: String) {}
-    fun addImageAse(filename: String, layer: String, tag: String, output: String) {}
-    fun addImageAse(filename: String, layer: String, tags: List<String>, output: String) {}
-    fun addImageAse(filename: String, layers: List<String>, output: String) {}
-    fun addImageAse(filename: String, layers: List<String>, tag: String, output: String) {}
-    fun addImageAse(filename: String, layers: List<String>, tags: List<String>, output: String) {}
+    /** Export full image from Aseprite file
+     */
+    fun addImageAse(filename: String, output: String) {
+        addImageAse(filename, emptyList(), emptyList(), output )
+    }
+
+    /** Export specific layer from Aseprite file
+     */
+    fun addImageAse(filename: String, layer: String, output: String) {
+        addImageAse(filename, listOf(layer), emptyList(), output)
+    }
+
+    /** Export specific layer and tag from Aseprite file
+     */
+    fun addImageAse(filename: String, layer: String, tag: String, output: String) {
+        addImageAse(filename, listOf(layer), listOf(tag), output)
+    }
+
+    /** Export specific layer and tags from Aseprite file
+     */
+    fun addImageAse(filename: String, layer: String, tags: List<String>, output: String) {
+        addImageAse(filename, listOf(layer), tags, output)
+    }
+
+    /** Export specific layers from Aseprite file
+     */
+    fun addImageAse(filename: String, layers: List<String>, output: String) {
+        addImageAse(filename, layers, emptyList(), output)
+    }
+
+    /** Export specific layers and tag from Aseprite file
+     */
+    fun addImageAse(filename: String, layers: List<String>, tag: String, output: String) {
+        addImageAse(filename, layers, listOf(tag), output)
+    }
 }
